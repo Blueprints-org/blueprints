67f51d753f267eb9ba81f6f513bc4909daa2cbe6
luuk.smulders <luuk.smulders@bam.com>
2023-11-28 14:25:33 +0100

    (#9) Few updates formula 3.1 and 3.2

--- blueprints/codes/eurocode/nen_en_1992_1_1_c2_2011/chapter_3_materials.py
+++ blueprints/codes/eurocode/nen_en_1992_1_1_c2_2011/chapter_3_materials.py
@@ -1,32 +1,29 @@
-"""This package represents the Eurocode NEN-EN 1992-1-1+C2:2011 code - Chapter 3."""
+"""This package represents the formulas in NEN-EN 1992-1-1+C2:2011 - Chapter 3."""
 import numpy as np
 
 from blueprints.codes.formula import Formula
 
+# pylint: disable=arguments-differ
 
-class Form3Dot1ApproximationConcreteCompressiveStrength(Formula):
-    """Class representing the formula 3.1 for the approximation of the concrete compressive strength after t days
-    with an average temperature of 20 degrees C [MPa]."""
+
+class Form3Dot1EstimationConcreteCompressiveStrength(Formula):
+    """Class representing formula 3.1 for the estimation of the concrete compressive strength, f_cm(t),  after t days
+    with an average temperature of 20 degrees Celsius [MPa]."""
 
     label = "3.1"
     source_document = "NEN-EN 1992-1-1+C2:2011"
 
     def __init__(self, beta_cc_t: float, f_cm: float) -> None:
-        """Calculates the approximation of the concrete compressive strength [MPa].
+        """Calculates fcm(t), the estimated concrete compressive strength [MPa].
 
-        NEN-EN 1992-1-1+C2:2011 art.3.1
+        NEN-EN 1992-1-1+C2:2011 art.3.1.2
 
         Parameters
         ----------
         beta_cc_t: float
-            Coefficient dependent of the age of concrete [-].
+            [beta_cc(t)] coefficient dependent of the age of concrete [-].
         f_cm: float
-            Average concrete compressive strength on day 28 based on table 3.1 [MPa].
-
-        Returns
-        -------
-        f_cm_t: float
-            Average concrete compressive strength at age t days
+            [fcm] average concrete compressive strength on day 28 based on table 3.1 [MPa].
         """
         super().__init__()
         self.beta_cc_t = beta_cc_t
@@ -39,33 +36,28 @@ class Form3Dot1ApproximationConcreteCompressiveStrength(Formula):
 
 
 class Form3Dot2CoefficientDependentOfConcreteAge(Formula):
-    """Class representing the formula 3.2 for the coefficient which is dependent of the age of concrete [-]."""
+    """Class representing formula 3.2 for the coefficient which is dependent of the age of concrete, beta_cc(t) [-]."""
 
     label = "3.2"
     source_document = "NEN-EN 1992-1-1+C2:2011"
 
     def __init__(self, s: float, t: int) -> None:
-        """Calculates the coefficient which is dependent of the age of concrete [-].
+        """Calculates beta_cc(t) coefficient which is dependent of the age of concrete in days [-].
 
-        NEN-EN 1992-1-1+C2:2011 art.3.2
+        NEN-EN 1992-1-1+C2:2011 art.3.1.2
 
         Parameters
         ----------
         s: float
-            Coefficient dependent on the kind of cement [-].
+            [s] coefficient dependent on the kind of cement [-].
         t: int
-            Age of concrete in days [days].
-
-        Returns
-        -------
-        beta_cc_t: float
-            Coefficient which is dependent of the age of concrete [-].
+            [t] age of concrete in days [days].
         """
         super().__init__()
         self.s = s
         self.t = t
 
     @staticmethod
-    def _evaluate(s: float, t: float) -> float:
+    def _evaluate(s: float, t: int) -> float:
         """For more detailed documentation see the class docstring."""
-        return np.exp(s * (1 - (28 / t) ** 1 / 2))
+        return np.exp(s * (1 - (28 / t) ** (1 / 2)))
