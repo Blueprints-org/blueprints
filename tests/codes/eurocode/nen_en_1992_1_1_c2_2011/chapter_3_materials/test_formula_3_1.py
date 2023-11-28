f62c058c7e57b3a1ea9125b3f31ad69ec61691df
luuk.smulders <luuk.smulders@bam.com>
2023-11-28 14:26:10 +0100

    (#9) Test formula 3.1

---
+++ tests/codes/eurocode/nen_en_1992_1_1_c2_2011/chapter_3_materials/test_formula_3_1.py
@@ -0,0 +1,32 @@
+"""Testing formula 3.1 of NEN-EN 1992-1-1+C2:2011."""
+import pytest
+
+from blueprints.codes.eurocode.nen_en_1992_1_1_c2_2011.chapter_3_materials import Form3Dot1EstimationConcreteCompressiveStrength
+
+# pylint: disable=arguments-differ
+
+
+class TestForm3Dot1EstimationConcreteCompressiveStrength:
+    """Validation for formula 3.1 from NEN-EN 1992-1-1+C2:2011."""
+
+    def test_evaluation(self) -> None:
+        """Test the evaluation of the result."""
+        # Example values
+        beta_cc_t = 1  # -
+        f_cm = 10  # MPa
+        form_3_1 = Form3Dot1EstimationConcreteCompressiveStrength(beta_cc_t=beta_cc_t, f_cm=f_cm)
+
+        # Expected result, manually calculated
+        manually_calculated_result = 10
+
+        assert form_3_1 == pytest.approx(expected=manually_calculated_result, rel=1e-4)
+
+    def test_raise_error_when_changing_value_after_initialization(self) -> None:
+        """Test that an error is raised when changing a value after initialization."""
+        # example values
+        beta_cc_t = 1  # -
+        f_cm = 10  # MPa
+        form_3_1 = Form3Dot1EstimationConcreteCompressiveStrength(beta_cc_t=beta_cc_t, f_cm=f_cm)
+
+        with pytest.raises(AttributeError):
+            form_3_1.beta_cc_t = 2
