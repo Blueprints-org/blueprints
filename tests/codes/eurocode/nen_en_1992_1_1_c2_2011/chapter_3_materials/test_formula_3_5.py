95b12249a5a3c44ae2c33f9fe177dc4ae3a51506
luuk.smulders <luuk.smulders@bam.com>
2023-11-29 13:57:51 +0100

    (#11) Update tests formula 3.5

--- tests/codes/eurocode/nen_en_1992_1_1_c2_2011/chapter_3_materials/test_formula_3_5.py
+++ tests/codes/eurocode/nen_en_1992_1_1_c2_2011/chapter_3_materials/test_formula_3_5.py
@@ -1,9 +1,8 @@
 """Testing formula 3.5 of NEN-EN 1992-1-1+C2:2011."""
+# pylint: disable=arguments-differ
 import pytest
 
 from blueprints.codes.eurocode.nen_en_1992_1_1_c2_2011.chapter_3_materials import Form3Dot5ApproximationVarianceElasticModulusOverTime
-
-# pylint: disable=arguments-differ
 
 
 class TestForm3Dot5ApproximationVarianceElasticModulusOverTime:
@@ -22,13 +21,32 @@ class TestForm3Dot5ApproximationVarianceElasticModulusOverTime:
 
         assert form_3_5 == pytest.approx(expected=manually_calculated_result, rel=1e-4)
 
-    def test_raise_error_when_changing_value_after_initialization(self) -> None:
-        """Test that an error is raised when changing a value after initialization."""
-        # example values
-        f_cm_t = 2.34  # MPa
+    def test_raise_error_when_negative_f_cm_t_is_given(self) -> None:
+        """Test the evaluation of the result."""
+        # Example values
+        f_cm_t = -2.34  # MPa
         f_cm = 3.4  # MPa
         e_cm = 2.9  # MPa
-        form_3_5 = Form3Dot5ApproximationVarianceElasticModulusOverTime(f_cm_t=f_cm_t, f_cm=f_cm, e_cm=e_cm)
 
-        with pytest.raises(AttributeError):
-            form_3_5.e_cm = 2
+        with pytest.raises(ValueError):
+            Form3Dot5ApproximationVarianceElasticModulusOverTime(f_cm_t=f_cm_t, f_cm=f_cm, e_cm=e_cm)
+
+    def test_raise_error_when_negative_f_cm_is_given(self) -> None:
+        """Test the evaluation of the result."""
+        # Example values
+        f_cm_t = 2.34  # MPa
+        f_cm = -3.4  # MPa
+        e_cm = 2.9  # MPa
+
+        with pytest.raises(ValueError):
+            Form3Dot5ApproximationVarianceElasticModulusOverTime(f_cm_t=f_cm_t, f_cm=f_cm, e_cm=e_cm)
+
+    def test_raise_error_when_negative_e_cm_is_given(self) -> None:
+        """Test the evaluation of the result."""
+        # Example values
+        f_cm_t = 2.34  # MPa
+        f_cm = 3.4  # MPa
+        e_cm = -2.9  # MPa
+
+        with pytest.raises(ValueError):
+            Form3Dot5ApproximationVarianceElasticModulusOverTime(f_cm_t=f_cm_t, f_cm=f_cm, e_cm=e_cm)
