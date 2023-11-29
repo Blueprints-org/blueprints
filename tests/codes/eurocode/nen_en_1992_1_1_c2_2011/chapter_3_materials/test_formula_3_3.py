1fc41948c451d6f0d4f9591b088333b3e4d7eb6f
luuk.smulders <luuk.smulders@bam.com>
2023-11-29 13:57:26 +0100

    (#11) Update tests formula 3.3

--- tests/codes/eurocode/nen_en_1992_1_1_c2_2011/chapter_3_materials/test_formula_3_3.py
+++ tests/codes/eurocode/nen_en_1992_1_1_c2_2011/chapter_3_materials/test_formula_3_3.py
@@ -1,9 +1,8 @@
 """Testing formula 3.3 of NEN-EN 1992-1-1+C2:2011."""
+# pylint: disable=arguments-differ
 import pytest
 
 from blueprints.codes.eurocode.nen_en_1992_1_1_c2_2011.chapter_3_materials import Form3Dot3AxialTensileStrengthFromTensileSplittingStrength
-
-# pylint: disable=arguments-differ
 
 
 class TestForm3Dot3AxialTensileStrengthFromTensileSplittingStrength:
@@ -20,11 +19,10 @@ class TestForm3Dot3AxialTensileStrengthFromTensileSplittingStrength:
 
         assert form_3_3 == pytest.approx(expected=manually_calculated_result, rel=1e-4)
 
-    def test_raise_error_when_changing_value_after_initialization(self) -> None:
-        """Test that an error is raised when changing a value after initialization."""
-        # example values
-        f_ct_sp = 3.4  # MPa
-        form_3_3 = Form3Dot3AxialTensileStrengthFromTensileSplittingStrength(f_ct_sp=f_ct_sp)
+    def test_raise_error_when_negative_f_ct_sp_is_given(self) -> None:
+        """Test the evaluation of the result."""
+        # Example values
+        f_ct_sp = -3.4  # MPa
 
-        with pytest.raises(AttributeError):
-            form_3_3.f_ct_sp = 2
+        with pytest.raises(ValueError):
+            Form3Dot3AxialTensileStrengthFromTensileSplittingStrength(f_ct_sp=f_ct_sp)
