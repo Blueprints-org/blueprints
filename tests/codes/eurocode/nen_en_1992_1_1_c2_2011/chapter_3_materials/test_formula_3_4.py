fb73537d31616887807acfad0908e0e6d1601aeb
luuk.smulders <luuk.smulders@bam.com>
2023-11-29 13:57:39 +0100

    (#11) Update tests formula 3.4

--- tests/codes/eurocode/nen_en_1992_1_1_c2_2011/chapter_3_materials/test_formula_3_4.py
+++ tests/codes/eurocode/nen_en_1992_1_1_c2_2011/chapter_3_materials/test_formula_3_4.py
@@ -1,9 +1,8 @@
 """Testing formula 3.4 of NEN-EN 1992-1-1+C2:2011."""
+# pylint: disable=arguments-differ
 import pytest
 
 from blueprints.codes.eurocode.nen_en_1992_1_1_c2_2011.chapter_3_materials import Form3Dot4DevelopmentTensileStrength
-
-# pylint: disable=arguments-differ
 
 
 class TestForm3Dot4DevelopmentTensileStrength:
@@ -25,20 +24,29 @@ class TestForm3Dot4DevelopmentTensileStrength:
     def test_raise_error_when_alpha_is_unequal_to_1_or_2d3(self) -> None:
         """Test that an error is raised when alpha is unequal to 1 or 2/3."""
         # Example values
-        beta_cc_t = 0.32  # -
-        alpha = 1 / 3  # - -> unequal to 1 or 2/3
-        f_ctm = 3.45
+        beta_cc_t = 0.32  # - -> Positive
+        alpha = 1 / 3  # - -> Unequal to 1 or 2/3
+        f_ctm = 3.45  # MPa -> Positive
 
         with pytest.raises(ValueError):
             Form3Dot4DevelopmentTensileStrength(beta_cc_t=beta_cc_t, alpha=alpha, f_ctm=f_ctm)
 
-    def test_raise_error_when_changing_value_after_initialization(self) -> None:
-        """Test that an error is raised when changing a value after initialization."""
-        # example values
-        beta_cc_t = 0.32  # -
-        alpha = 2 / 3
-        f_ctm = 3.45
-        form_3_4 = Form3Dot4DevelopmentTensileStrength(beta_cc_t=beta_cc_t, alpha=alpha, f_ctm=f_ctm)
+    def test_raise_error_when_negative_beta_cc_t_is_given(self) -> None:
+        """Test the evaluation of the result."""
+        # Example values
+        beta_cc_t = -0.32  # - -> Negative
+        alpha = 2 / 3  # - -> Equal to 1 or 2/3
+        f_ctm = 3.45  # MPa -> Positive
 
-        with pytest.raises(AttributeError):
-            form_3_4.f_ctm = 2
+        with pytest.raises(ValueError):
+            Form3Dot4DevelopmentTensileStrength(beta_cc_t=beta_cc_t, alpha=alpha, f_ctm=f_ctm)
+
+    def test_raise_error_when_negative_f_ctm_is_given(self) -> None:
+        """Test the evaluation of the result."""
+        # Example values
+        beta_cc_t = 0.32  # - -> Positive
+        alpha = 2 / 3  # - -> unequal to 1 or 2/3
+        f_ctm = -3.45  # MPa -> Negative
+
+        with pytest.raises(ValueError):
+            Form3Dot4DevelopmentTensileStrength(beta_cc_t=beta_cc_t, alpha=alpha, f_ctm=f_ctm)
