0797d19201839655cd99899a550d39b574e30a8e
luuk.smulders <luuk.smulders@bam.com>
2023-11-30 16:48:25 +0100

    (#30) Added test formula 3.28

---
+++ tests/codes/eurocode/nen_en_1992_1_1_c2_2011/chapter_3_materials/test_formula_3_28.py
@@ -0,0 +1,43 @@
+"""Testing formula 3.28 of NEN-EN 1992-1-1+C2:2011."""
+# pylint: disable=arguments-differ
+import pytest
+
+from blueprints.codes.eurocode.nen_en_1992_1_1_c2_2011.chapter_3_materials import Form3Dot28RatioLossOfPreStressClass1
+
+
+class TestForm3Dot28RatioLossOfPreStressClass1:
+    """Validation for formula 3.28 from NEN-EN 1992-1-1+C2:2011."""
+
+    def test_evaluation(self) -> None:
+        """Test the evaluation of the result."""
+        # Example values
+        rho_1000 = 35.2  # %
+        mu = 0.28  # -
+        t = 7.4  # hours
+
+        form_3_28 = Form3Dot28RatioLossOfPreStressClass1(rho_1000=rho_1000, mu=mu, t=t)
+
+        # Expected result, manually calculated
+        manually_calculated_result = 8.754937 * 10**-4
+
+        assert form_3_28 == pytest.approx(expected=manually_calculated_result, rel=1e-4)
+
+    def test_raise_error_when_negative_rho_1000_is_given(self) -> None:
+        """Test a negative value."""
+        # Example values
+        rho_1000 = -35.2  # %
+        mu = 0.28  # -
+        t = 7.4  # hours
+
+        with pytest.raises(ValueError):
+            Form3Dot28RatioLossOfPreStressClass1(rho_1000=rho_1000, mu=mu, t=t)
+
+    def test_raise_error_when_negative_t_is_given(self) -> None:
+        """Test a negative value."""
+        # Example values
+        rho_1000 = 35.2  # %
+        mu = 0.28  # -
+        t = -7.4  # hours
+
+        with pytest.raises(ValueError):
+            Form3Dot28RatioLossOfPreStressClass1(rho_1000=rho_1000, mu=mu, t=t)
