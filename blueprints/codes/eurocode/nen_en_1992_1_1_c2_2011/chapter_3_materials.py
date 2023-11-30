76fa98f81ff1054cf359959232941463ec9c671a
luuk.smulders <luuk.smulders@bam.com>
2023-11-30 15:24:26 +0100

    (#28) Added formula 3.25

--- blueprints/codes/eurocode/nen_en_1992_1_1_c2_2011/chapter_3_materials.py
+++ blueprints/codes/eurocode/nen_en_1992_1_1_c2_2011/chapter_3_materials.py
@@ -153,6 +153,7 @@ class Form3Dot24IncreasedCharacteristicCompressiveStrength(Formula):
             [fck] Characteristic compressive strength [MPa]
         sigma_2 : MPA
             [σ2] Effective compressive stress in transverse direction [MPa]
+            <= 0.05 * fck
         """
         super().__init__()
         self.f_ck = f_ck
@@ -169,3 +170,43 @@ class Form3Dot24IncreasedCharacteristicCompressiveStrength(Formula):
         if sigma_2 > 0.05 * f_ck:
             raise ValueError(f"Invalid sigma_2: {sigma_2}. Try using Form3Dot25IncreasedCharacteristicCompressiveStrength class.")
         return f_ck * (1.000 + 5.0 * sigma_2 / f_ck)
+
+
+class Form3Dot25IncreasedCharacteristicCompressiveStrength(Formula):
+    """Class representing formula 3.25 for the calculation of the increased characteristic compressive strength due to enclosed concrete."""
+
+    label = "3.25"
+    source_document = NEN_EN_1992_1_1_C2_2011
+
+    def __init__(
+        self,
+        f_ck: MPA,
+        sigma_2: MPA,
+    ) -> None:
+        """[fck,c] Increased characteristic compressive strength due to enclosed concrete [MPa].
+
+        NEN-EN 1992-1-1+C2:2011 art.3.1.9(2) - Formula (3.25)
+
+        Parameters
+        ----------
+        f_ck : MPA
+            [fck] Characteristic compressive strength [MPa]
+        sigma_2 : MPA
+            [σ2] Effective compressive stress in transverse direction [MPa]
+            > 0.05 * fck
+        """
+        super().__init__()
+        self.f_ck = f_ck
+        self.sigma_2 = sigma_2
+
+    @staticmethod
+    def _evaluate(
+        f_ck: MPA,
+        sigma_2: MPA,
+    ) -> float:
+        """Evaluates the formula, for more information see the __init__ method"""
+        if f_ck < 0:
+            raise ValueError(f"Invalid f_ck: {f_ck}. f_ck cannot be negative")
+        if sigma_2 <= 0.05 * f_ck:
+            raise ValueError(f"Invalid sigma_2: {sigma_2}. Try using Form3Dot24IncreasedCharacteristicCompressiveStrength class.")
+        return f_ck * (1.125 + 2.5 * sigma_2 / f_ck)
