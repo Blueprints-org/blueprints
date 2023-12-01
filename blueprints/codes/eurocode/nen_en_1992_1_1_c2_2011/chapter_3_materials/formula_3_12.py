f820604631e7262e39c17f80db4bf992fa0e320b
luuk.smulders <luuk.smulders@bam.com>
2023-12-01 13:36:02 +0100

    (#14) Refactor formulas to their own files

---
+++ blueprints/codes/eurocode/nen_en_1992_1_1_c2_2011/chapter_3_materials/formula_3_12.py
@@ -0,0 +1,37 @@
+"""Formula 3.12 from NEN-EN 1992-1-1+C2:2011: Chapter 3 - Materials."""
+# pylint: disable=arguments-differ
+from blueprints.codes.eurocode.nen_en_1992_1_1_c2_2011 import NEN_EN_1992_1_1_C2_2011
+from blueprints.codes.formula import Formula
+from blueprints.type_alias import MPA
+
+
+class Form3Dot12AutogeneShrinkageInfinity(Formula):
+    """Class representing formula 3.12, which calculates the autogene shrinkage at infinity"""
+
+    source_document = NEN_EN_1992_1_1_C2_2011
+    label = "3.12"
+
+    def __init__(
+        self,
+        f_ck: MPA,
+    ) -> None:
+        """[εca(∞)] Autogene shrinkage at infinity [-].
+
+        NEN-EN 1992-1-1+C2:2011 art.3.1.4(6) - Formula (3.12)
+
+        Parameters
+        ----------
+        f_ck : MPA
+            [fck] Compressive strength concrete [MPa].
+        """
+        super().__init__()
+        self.f_ck = f_ck
+
+    @staticmethod
+    def _evaluate(
+        f_ck: MPA,
+    ) -> float:
+        """Evaluates the formula, for more information see the __init__ method"""
+        if f_ck < 0:
+            raise ValueError(f"Invalid f_ck: {f_ck}. f_ck cannot be negative")
+        return 2.5 * (f_ck - 10) * 10**-6
