b8a709d1517e19baa2974c585d37fe882afc79e5
Enrique García <enrique.garcia@bam.com>
2023-12-01 17:01:46 +0100

    (#28) Missing 3_, rename files

--- tests/codes/eurocode/nen_en_1992_1_1_c2_2011/chapter_3_materials/test_formula_3_25.py
+++ tests/codes/eurocode/nen_en_1992_1_1_c2_2011/chapter_3_materials/test_formula_3_25.py
@@ -2,7 +2,7 @@
 # pylint: disable=arguments-differ
 import pytest
 
-from blueprints.codes.eurocode.nen_en_1992_1_1_c2_2011.chapter_3_materials.formula_25 import Form3Dot25IncreasedCharacteristicCompressiveStrength
+from blueprints.codes.eurocode.nen_en_1992_1_1_c2_2011.chapter_3_materials.formula_3_25 import Form3Dot25IncreasedCharacteristicCompressiveStrength
 
 
 class TestForm3Dot25IncreasedCharacteristicCompressiveStrength:
