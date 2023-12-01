ba64b3c63580e935e983bf76e2ef13b6aaff9dba
luuk.smulders <luuk.smulders@bam.com>
2023-12-01 13:01:34 +0100

    (#32) Updating test files due to refactor

--- tests/codes/eurocode/nen_en_1992_1_1_c2_2011/chapter_3_materials/test_formula_3_9.py
+++ tests/codes/eurocode/nen_en_1992_1_1_c2_2011/chapter_3_materials/test_formula_3_9.py
@@ -2,7 +2,7 @@
 # pylint: disable=arguments-differ
 import pytest
 
-from blueprints.codes.eurocode.nen_en_1992_1_1_c2_2011.chapter_3_materials import Form3Dot9DryingShrinkage
+from blueprints.codes.eurocode.nen_en_1992_1_1_c2_2011.chapter_3_materials.formula_3_9 import Form3Dot9DryingShrinkage
 
 
 class TestForm3Dot9DryingShrinkage:
