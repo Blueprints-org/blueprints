a9474bc5c95a8470bcdbd2ac4f21fb2a872e7652
egarciamendez <enriquegarcia@live.nl>
2023-12-03 20:38:48 +0100

    (#15) Added formula 5.1

---
+++ blueprints/codes/eurocode/nen_en_1992_1_1_c2_2011/chapter_5_structural_analysis/formula_5_1.py
@@ -0,0 +1,142 @@
+"""Formula 5.1 from NEN-EN 1992-1-1+C2:2011: Chapter Structural Analysis."""
+# pylint: disable=arguments-differ
+import numpy as np
+
+from blueprints.codes.eurocode.nen_en_1992_1_1_c2_2011 import NEN_EN_1992_1_1_C2_2011
+from blueprints.codes.formula import Formula
+from blueprints.type_alias import M
+
+
+class Form5Dot1Imperfections(Formula):
+    """Class representing formula 5.1 for the calculation of initial inclination imperfections, Θi."""
+
+    label = "5.1"
+    source_document = NEN_EN_1992_1_1_C2_2011
+
+    def __init__(
+        self,
+        theta_0: float,
+        alpha_h: float,
+        alpha_m: float,
+    ) -> None:
+        """[Θi] Initial inclination imperfections, Θi, is a ratio between height and inclination of the member [-].
+
+        NEN-EN 1992-1-1+C2:2011 art.5.2(5) - Formula (5.1)
+
+        Parameters
+        ----------
+        theta_0 : float
+            [Θ0] Basic value [-].
+            Note: The value of Θ0 for use in a Country may be found in its National Annex.
+            The recommended value is 1/200
+        alpha_h : float
+            [αh] Reduction factor for length or heigh [-].
+            Use your own implementation of this value or use the SubForm5Dot1ReductionFactorLengthOrHeight class.
+        alpha_m : float
+            [αm] Reduction factor for number of members [-].
+            Use your own implementation of this value or use the SubForm5Dot1ReductionFactorNumberOfMembers class.
+
+        Returns
+        -------
+        None
+        """
+        super().__init__()
+        self.theta_0 = theta_0
+        self.alpha_h = alpha_h
+        self.alpha_m = alpha_m
+
+    @staticmethod
+    def _evaluate(
+        theta_0: float,
+        alpha_h: float,
+        alpha_m: float,
+    ) -> float:
+        """Evaluates the formula, for more information see the __init__ method"""
+        if theta_0 < 0:
+            raise ValueError(f"Negative theta_0: {theta_0}. theta_0 cannot be negative")
+        if alpha_h < 0:
+            raise ValueError(f"Negative alpha_h: {alpha_h}. alpha_h cannot be negative")
+        if alpha_m < 0:
+            raise ValueError(f"Negative alpha_m: {alpha_m}. alpha_m cannot be negative")
+        return theta_0 * alpha_h * alpha_m
+
+
+class SubForm5Dot1ReductionFactorLengthOrHeight(Formula):
+    """Class representing sub-formula 5.1 for the calculation of the reduction factor for length or height, αh."""
+
+    label = "5.1"
+    source_document = NEN_EN_1992_1_1_C2_2011
+
+    def __init__(
+        self,
+        l: M,
+    ) -> None:
+        """[αh] Reduction factor for length or height [-].
+
+        The calculated value of αh is between 2/3 and 1.0.
+
+        NEN-EN 1992-1-1+C2:2011 art.5.2(5) - Formula (5.1)
+
+        Parameters
+        ----------
+        l : M
+            [l] Length or height, see art.5.2(6) [m].
+
+        Returns
+        -------
+        None
+        """
+        super().__init__()
+        self.l = l
+
+    @staticmethod
+    def _evaluate(
+        l: M,
+    ) -> float:
+        """Evaluates the formula, for more information see the __init__ method"""
+        if l < 0:
+            raise ValueError(f"Negative l: {l}. l cannot be negative")
+        # the value of alpha_h is between 2/3 and 1.0
+        alpha_h = 2 / np.sqrt(l)
+        if alpha_h < 2 / 3:
+            return 2 / 3
+        if alpha_h > 1:
+            return 1
+        return alpha_h
+
+
+class SubForm5Dot1ReductionFactorNumberOfMembers(Formula):
+    """Class representing sub-formula 5.1 for the calculation of the reduction factor for number of members, αm."""
+
+    label = "5.1"
+    source_document = NEN_EN_1992_1_1_C2_2011
+
+    def __init__(
+        self,
+        m: int,
+    ) -> None:
+        """[αm] Reduction factor for number of members [-].
+
+        NEN-EN 1992-1-1+C2:2011 art.5.2(5) - Formula (5.1)
+
+        Parameters
+        ----------
+        m : int
+            [m] Number of vertical members contributing to the total effect [-].
+
+        Returns
+        -------
+        None
+        """
+        super().__init__()
+        self.m = m
+
+    @staticmethod
+    def _evaluate(
+        m: int,
+    ) -> float:
+        """Evaluates the formula, for more information see the __init__ method"""
+        if m < 0:
+            raise ValueError(f"Negative m: {m}. m cannot be negative")
+        # the value of alpha_m is between 1.0 and 1.5
+        return np.sqrt(0.5 * (1 + 1 / m))
