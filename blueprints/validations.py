6272ffdf3e8fcd77ee68e61943300534200fa7fb
Enrique García <enrique.garcia@bam.com>
2023-12-06 16:16:18 +0100

    (#38) validations ready to take off!

--- blueprints/validations.py
+++ blueprints/validations.py
@@ -1,49 +1,55 @@
 """Module for validation actions inside of Blueprints."""
 
 
-class NonPositiveValueError(Exception):
+class LessOrEqualToZeroError(Exception):
     """Raised when a value is less than or equal to zero."""
+
+    def __init__(self, value_name: str, value: float) -> None:
+        message = f"Invalid value for '{value_name}': {value}. Values for '{value_name}' must be greater than zero."
+        super().__init__(message)
 
 
 class NegativeValueError(Exception):
     """Raised when a value is negative."""
 
+    def __init__(self, value_name: str, value: float) -> None:
+        message = f"Invalid value for '{value_name}': {value}. Values for '{value_name}' cannot be negative."
+        super().__init__(message)
 
-def raise_if_less_or_equal_to_zero(**kwargs) -> None:
-    """
-    Raise a ValueError if any of the given keyword arguments are less than or equal to zero.
+
+def raise_if_less_or_equal_to_zero(**kwargs: float) -> None:
+    """Raise a LessOrEqualToZeroError if any of the given keyword arguments are less than or equal to zero.
 
     Parameters
     ----------
-    **kwargs
+    **kwargs : dict[str, float]
         A dictionary of keyword arguments where keys are parameter names, and values are the values to validate.
 
     Raises
     ------
-    NonPositiveValueError
-        If any value is greater than zero.
+    LessOrEqualToZeroError
+        If any value is less than or equal to zero.
 
     """
     for key, value in kwargs.items():
         if value <= 0:
-            raise NonPositiveValueError(f"Invalid value for '{key}': {value}. Values for '{key}' must be greater than zero.")
+            raise LessOrEqualToZeroError(value_name=key, value=value)
 
 
-def raise_if_negative(**kwargs) -> None:
-    """
-    Raise a ValueError if any of the given keyword arguments are negative.
+def raise_if_negative(**kwargs: float) -> None:
+    """Raise a NegativeValueError if any of the given keyword arguments are negative.
 
     Parameters
     ----------
-    **kwargs
+    **kwargs : dict[str, float]
         A dictionary of keyword arguments where keys are parameter names, and values are the values to validate.
 
     Raises
     ------
     NegativeValueError
-        If any value is greater than or equal to zero.
+        If any value is negative.
 
     """
     for key, value in kwargs.items():
         if value < 0:
-            raise NegativeValueError(f"Invalid value for '{key}': {value}. Values for '{key}' cannot be negative.")
+            raise NegativeValueError(value_name=key, value=value)
