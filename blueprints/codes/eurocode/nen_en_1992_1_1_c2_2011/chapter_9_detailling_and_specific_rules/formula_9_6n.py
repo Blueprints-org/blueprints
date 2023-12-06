20a22faea2c103e769310f66d39622efb9ca796f
PetervWestrienen <petervanwestrienen@gmail.com>
2023-12-06 15:19:04 +0100

    (#35) add formula 9.6N and test

---
+++ blueprints/codes/eurocode/nen_en_1992_1_1_c2_2011/chapter_9_detailling_and_specific_rules/formula_9_6n.py
@@ -0,0 +1,42 @@
+"""This package represents the Eurocode NEN-EN 1992-1-1+C2:2011 code - Chapter 9 - formula (9.6N)."""
+# pylint: disable=arguments-differ
+
+import numpy as np
+
+from blueprints.codes.eurocode.nen_en_1992_1_1_c2_2011 import NEN_EN_1992_1_1_C2_2011
+from blueprints.codes.formula import Formula
+from blueprints.type_alias import DEG, MM
+
+
+class Form9Dot6NMaximumDistanceShearReinforcement(Formula):
+    """Class representing the formula 9.6N for the calculation of the maximum distance between shear reinforcement in longitudinal direction"""
+
+    label = "9.6N"
+    source_document = NEN_EN_1992_1_1_C2_2011
+
+    def __init__(
+        self,
+        d: MM,
+        alpha: DEG,
+    ) -> None:
+        """[sl,max] Maximum distance between shear reinforcement in longitudinal direction [mm].
+
+        NEN-EN 1992-1-1+C2:2011 art.9.2.2(6) - Formula (9.6N)
+
+        Parameters
+        ----------
+        d: MM
+            [d] Effective height of the cross-section [mm].
+        alpha: DEG
+            [α] The angle between the shear reinforcement and the longitudinal axis of the beam (see 9.2.2(1)) [deg].
+        """
+        super().__init__()
+        self.d = d
+        self.alpha = alpha
+
+    @staticmethod
+    def _evaluate(d: MM, alpha: DEG) -> float:
+        """For more detailed documentation see the class docstring."""
+        if d < 0:
+            raise ValueError(f"Negative d: {d}. d cannot be negative")
+        return 0.75 * d * (1 + (1 / np.tan(alpha * np.pi / 180)))
---
+++ tests/codes/eurocode/nen_en_1992_1_1_c2_2011/chapter_9_detailling_and_specific_rules/test_formula_9_6n.py
@@ -0,0 +1,31 @@
+"""Testing formula 9.3 of NEN-EN 1992-1-1+C2:2011."""
+# pylint: disable=arguments-differ
+import pytest
+
+from blueprints.codes.eurocode.nen_en_1992_1_1_c2_2011.chapter_9_detailling_and_specific_rules.formula_9_6n import (
+    Form9Dot6NMaximumDistanceShearReinforcement,
+)
+
+
+class TestForm9Dot6NMaximumDistanceShearReinforcement:
+    """Validation for formula 9.6N from NEN-EN 1992-1-1+C2:2011."""
+
+    def test_evaluation(self) -> None:
+        """Test the evaluation of the result."""
+        # Example values
+        d = 100  # mm
+        alpha = 85  # deg
+        form_9_6n = Form9Dot6NMaximumDistanceShearReinforcement(d=d, alpha=alpha)
+
+        # Expected result, manually calculated
+        manually_calculated_result = 81.56164976
+
+        assert form_9_6n == pytest.approx(expected=manually_calculated_result, rel=1e-4)
+
+    def test_raise_error_when_negative_d_is_given(self) -> None:
+        """Test the evaluation of the result."""
+        d = -100  # mm
+        alpha = 85  # deg
+
+        with pytest.raises(ValueError):
+            Form9Dot6NMaximumDistanceShearReinforcement(d=d, alpha=alpha)
