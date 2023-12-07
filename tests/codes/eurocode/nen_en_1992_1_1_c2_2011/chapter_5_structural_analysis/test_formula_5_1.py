a01a28a25f3a3c38784e5cc5b24bad420e133f91
Enrique García <enrique.garcia@bam.com>
2023-12-07 11:23:17 +0100

    (#15) Integration tests with sub-formulas added

--- tests/codes/eurocode/nen_en_1992_1_1_c2_2011/chapter_5_structural_analysis/test_formula_5_1.py
+++ tests/codes/eurocode/nen_en_1992_1_1_c2_2011/chapter_5_structural_analysis/test_formula_5_1.py
@@ -2,7 +2,11 @@
 # pylint: disable=arguments-differ
 import pytest
 
-from blueprints.codes.eurocode.nen_en_1992_1_1_c2_2011.chapter_5_structural_analysis.formula_5_1 import Form5Dot1Imperfections
+from blueprints.codes.eurocode.nen_en_1992_1_1_c2_2011.chapter_5_structural_analysis.formula_5_1 import (
+    Form5Dot1Imperfections,
+    SubForm5Dot1ReductionFactorLengthOrHeight,
+    SubForm5Dot1ReductionFactorNumberOfMembers,
+)
 from blueprints.validations import NegativeValueError
 
 
@@ -53,3 +57,33 @@ class TestForm5Dot1Imperfections:
 
         with pytest.raises(NegativeValueError):
             Form5Dot1Imperfections(theta_0=theta_0, alpha_h=alpha_h, alpha_m=alpha_m)
+
+    def test_integration_with_sub_formula_5_1_reduction_factor_length_or_height(self) -> None:
+        """Test the integration with sub-formula 5.1 for the reduction factor for length or height, αh."""
+        # Example values
+        theta_0 = 0.005
+        alpha_h = SubForm5Dot1ReductionFactorLengthOrHeight(l=5.3)
+        alpha_m = 0.9
+
+        # Object to test
+        form_5_1 = Form5Dot1Imperfections(theta_0=theta_0, alpha_h=alpha_h, alpha_m=alpha_m)
+
+        # Expected result, manually calculated
+        manually_calculated_result = 0.003909
+
+        assert form_5_1 == pytest.approx(expected=manually_calculated_result, rel=1e-4)
+
+    def test_integration_with_sub_formula_5_1_reduction_factor_number_of_members(self) -> None:
+        """Test the integration with sub-formula 5.1 for the reduction factor for number of members, αm."""
+        # Example values
+        theta_0 = 0.005
+        alpha_h = 0.8
+        alpha_m = SubForm5Dot1ReductionFactorNumberOfMembers(m=3)
+
+        # Object to test
+        form_5_1 = Form5Dot1Imperfections(theta_0=theta_0, alpha_h=alpha_h, alpha_m=alpha_m)
+
+        # Expected result, manually calculated
+        manually_calculated_result = 0.00326598
+
+        assert form_5_1 == pytest.approx(expected=manually_calculated_result, rel=1e-4)
