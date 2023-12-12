f1c178f33c2bbdb50275baba27ccaa946bd6cc9a
PetervWestrienen <petervanwestrienen@gmail.com>
2023-12-12 09:48:16 +0100

    (#64) test formula 8_8n

--- tests/codes/eurocode/nen_en_1992_1_1_c2_2011/chapter_8_detailing_of_reinforcement_and_prestressing_tendons/test_formula_8_8n.py
+++ tests/codes/eurocode/nen_en_1992_1_1_c2_2011/chapter_8_detailing_of_reinforcement_and_prestressing_tendons/test_formula_8_8n.py
@@ -5,6 +5,8 @@ import pytest
 
 from blueprints.codes.eurocode.nen_en_1992_1_1_c2_2011.chapter_8_detailing_of_reinforcement_and_prestressing_tendons.formula_8_8n import (
     Form8Dot8NAnchorageCapacityWeldedTransverseBar,
+    SubForm8Dot8NConcreteStress,
+    SubForm8Dot8NDesignLengthOfTransverseBar,
 )
 from blueprints.validations import NegativeValueError
 
@@ -113,3 +115,61 @@ class TestForm8Dot8NForm8Dot8NAnchorageCapacityWeldedTransverseBar:
                 sigma_td=sigma_td,
                 f_wd=f_wd,
             )
+
+    def test_integration_with_sub_formula_8_8_design_length_of_transverse_bar(self) -> None:
+        """Test the integration with sub-formula 8.8 for calculating design length of transverse bar ltd."""
+        # Example values
+        phi_t = 16  # mm
+        sigma_td = 60  # MPa
+        f_wd = 150  # kN
+        f_yd = 500  # MPa
+        l_t = 100  # mm
+        l_td = SubForm8Dot8NDesignLengthOfTransverseBar(
+            phi_t=phi_t,
+            f_yd=f_yd,
+            sigma_td=sigma_td,
+            l_t=l_t,
+        )
+
+        # Object to test
+        form_8_8n = Form8Dot8NAnchorageCapacityWeldedTransverseBar(
+            l_td=l_td,
+            phi_t=phi_t,
+            sigma_td=sigma_td,
+            f_wd=f_wd,
+        )
+
+        # Expected result, manually calculated
+        manually_calculated_result = 51.434980
+
+        assert form_8_8n == pytest.approx(expected=manually_calculated_result, rel=1e-4)
+
+    def test_integration_with_sub_formula_8_8_concrete_stress(self) -> None:
+        """Test the integration with sub-formula 8.8 for calculating concrete stress σtd."""
+        # Example values
+        phi_t = 16  # mm
+        f_wd = 150  # kN
+        l_td = 80  # mm
+        f_ctd = 3  # MPa
+        sigma_cm = 15  # MPa
+        y = 0.5  # -
+        f_cd = 25  # MPa
+        sigma_td = SubForm8Dot8NConcreteStress(
+            f_ctd=f_ctd,
+            sigma_cm=sigma_cm,
+            y=y,
+            f_cd=f_cd,
+        )
+
+        # Object to test
+        form_8_8n = Form8Dot8NAnchorageCapacityWeldedTransverseBar(
+            l_td=l_td,
+            phi_t=phi_t,
+            sigma_td=sigma_td,
+            f_wd=f_wd,
+        )
+
+        # Expected result, manually calculated
+        manually_calculated_result = 46.080000
+
+        assert form_8_8n == pytest.approx(expected=manually_calculated_result, rel=1e-4)
