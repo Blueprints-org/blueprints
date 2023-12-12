b01f501efd7e7d113368a66bed405c1d53162092
PetervWestrienen <petervanwestrienen@gmail.com>
2023-12-12 10:17:46 +0100

    (#64) added integration test

--- tests/codes/eurocode/nen_en_1992_1_1_c2_2011/chapter_8_detailing_of_reinforcement_and_prestressing_tendons/test_sub_formula_8_8n_1.py
+++ tests/codes/eurocode/nen_en_1992_1_1_c2_2011/chapter_8_detailing_of_reinforcement_and_prestressing_tendons/test_sub_formula_8_8n_1.py
@@ -3,6 +3,7 @@
 import pytest
 
 from blueprints.codes.eurocode.nen_en_1992_1_1_c2_2011.chapter_8_detailing_of_reinforcement_and_prestressing_tendons.formula_8_8n import (
+    SubForm8Dot8NConcreteStress,
     SubForm8Dot8NDesignLengthOfTransverseBar,
 )
 from blueprints.validations import LessOrEqualToZeroError, NegativeValueError
@@ -122,3 +123,32 @@ class TestSubForm8Dot8NDesignLengthOfTransverseBar:
                 sigma_td=sigma_td,
                 l_t=l_t,
             )
+
+    def test_integration_with_sub_form_8_8n_concrete_stress(self) -> None:
+        """Test the integration with sub-formula 8.8 for calculating concrete stress."""
+        # Example values
+        phi_t = 16  # mm
+        f_yd = 500  # MPa
+        l_t = 100  # mm
+        f_ctd = 2.6  # MPa
+        sigma_cm = 15  # MPa
+        y = 0.5  # -
+        f_cd = 25  # MPa
+        sigma_td = SubForm8Dot8NConcreteStress(
+            f_ctd=f_ctd,
+            sigma_cm=sigma_cm,
+            y=y,
+            f_cd=f_cd,
+        )
+
+        # Object to test
+        form_8_8n = SubForm8Dot8NDesignLengthOfTransverseBar(
+            phi_t=phi_t,
+            f_yd=f_yd,
+            sigma_td=sigma_td,
+            l_t=l_t,
+        )
+
+        manually_calculated_result = 69.950631942
+
+        assert form_8_8n == pytest.approx(expected=manually_calculated_result, rel=1e-4)
