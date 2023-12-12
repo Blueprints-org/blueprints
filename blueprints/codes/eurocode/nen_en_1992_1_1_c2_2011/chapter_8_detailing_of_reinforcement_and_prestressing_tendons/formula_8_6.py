edf2e17a98098d02e91ad229e3a27a5f23d2ce21
PetervWestrienen <petervanwestrienen@gmail.com>
2023-12-12 11:58:53 +0100

    (#68) Added formula 8.6 + test

---
+++ blueprints/codes/eurocode/nen_en_1992_1_1_c2_2011/chapter_8_detailing_of_reinforcement_and_prestressing_tendons/formula_8_6.py
@@ -0,0 +1,44 @@
+"""Formula 8.6 from NEN-EN 1992-1-1+C2:2011: Chapter 8: Detailing of reinforcement and prestressing tendons"""
+# pylint: disable=arguments-differ
+# pylint: disable=duplicate-code
+
+from blueprints.codes.eurocode.nen_en_1992_1_1_c2_2011 import NEN_EN_1992_1_1_C2_2011
+from blueprints.codes.formula import Formula
+from blueprints.type_alias import MM
+from blueprints.validations import raise_if_negative
+
+
+class Form8Dot6MinimumTensionAnchorage(Formula):
+    """Class representing formula 8.6 for the calculation of the minimum anchorage length if no other limitation is applied for anchorage in
+    tension."""
+
+    label = "8.6"
+    source_document = NEN_EN_1992_1_1_C2_2011
+
+    def __init__(
+        self,
+        l_b_rqd: MM,
+        phi: MM,
+    ) -> None:
+        """[lb,min] Minimum anchorage length if no other limitation is applied for anchorage in tension. [mm].
+
+        NEN-EN 1992-1-1+C2:2011 art.8.4.4(1) - Formula (8.6)
+
+        Parameters
+        ----------
+        l_b_rqd: MM
+            [lb,rqd] Basic required anchorage length, for anchoring the force As*σsd in a straight bar assuming constant bond stress fbd. [mm].
+            = (Φ/4) * (σsd/fbd)
+            Use your own implementation for this value or use the Form8Dot3RequiredAnchorageLength class.
+        phi: MM
+            [Φ] Diameter of the bar [mm].
+        """
+        super().__init__()
+        self.l_b_rqd = l_b_rqd
+        self.phi = phi
+
+    @staticmethod
+    def _evaluate(l_b_rqd: MM, phi: MM) -> MM:
+        """Evaluates the formula, for more information see the __init__ method"""
+        raise_if_negative(phi=phi, l_b_rqd=l_b_rqd)
+        return max(0.3 * l_b_rqd, 10 * phi, 100)
---
+++ tests/codes/eurocode/nen_en_1992_1_1_c2_2011/chapter_8_detailing_of_reinforcement_and_prestressing_tendons/test_formula_8_6.py
@@ -0,0 +1,83 @@
+"""Testing formula 8.6 of NEN-EN 1992-1-1+C2:2011."""
+# pylint: disable=arguments-differ
+# pylint: disable=duplicate-code
+
+import pytest
+
+from blueprints.codes.eurocode.nen_en_1992_1_1_c2_2011.chapter_8_detailing_of_reinforcement_and_prestressing_tendons.formula_8_3 import (
+    Form8Dot3RequiredAnchorageLength,
+)
+from blueprints.codes.eurocode.nen_en_1992_1_1_c2_2011.chapter_8_detailing_of_reinforcement_and_prestressing_tendons.formula_8_6 import (
+    Form8Dot6MinimumTensionAnchorage,
+)
+from blueprints.validations import NegativeValueError
+
+
+class TestForm8Dot6MinimumTensionAnchorage:
+    """Validation for formula 8.6 from NEN-EN 1992-1-1+C2:2011."""
+
+    def test_evaluation_first_term_decisive(self) -> None:
+        """Test the evaluation of the result if the first term is decisive."""
+        # example values
+        l_b_rqd = 500  # mm
+        phi = 8  # mm
+        form_8_7 = Form8Dot6MinimumTensionAnchorage(l_b_rqd=l_b_rqd, phi=phi)
+
+        manually_calculated_result = 150  # mm
+
+        assert form_8_7 == pytest.approx(expected=manually_calculated_result, rel=1e-4)
+
+    def test_evaluation_second_term_decisive(self) -> None:
+        """Test the evaluation of the result if the second term is decisive."""
+        # example values
+        l_b_rqd = 200  # mm
+        phi = 16  # mm
+        form_8_7 = Form8Dot6MinimumTensionAnchorage(l_b_rqd=l_b_rqd, phi=phi)
+
+        manually_calculated_result = 160  # mm
+
+        assert form_8_7 == pytest.approx(expected=manually_calculated_result, rel=1e-4)
+
+    def test_evaluation_third_term_decisive(self) -> None:
+        """Test the evaluation of the result if the third term is decisive."""
+        # example values
+        l_b_rqd = 150  # mm
+        phi = 8  # mm
+
+        form_8_7 = Form8Dot6MinimumTensionAnchorage(l_b_rqd=l_b_rqd, phi=phi)
+
+        manually_calculated_result = 100  # mm
+
+        assert form_8_7 == pytest.approx(expected=manually_calculated_result, rel=1e-4)
+
+    def test_evaluation_negative_l_b_rqd(self) -> None:
+        """Test if NegativeValueError is raised for negative l_b_rqd."""
+        # example values
+        l_b_rqd = -500  # mm
+        phi = 8  # mm
+
+        with pytest.raises(NegativeValueError):
+            Form8Dot6MinimumTensionAnchorage(l_b_rqd=l_b_rqd, phi=phi)
+
+    def test_evaluation_negative_phi(self) -> None:
+        """Test if NegativeValueError is raised for negative phi."""
+        # example values
+        l_b_rqd = 500  # mm
+        phi = -8  # mm
+
+        with pytest.raises(NegativeValueError):
+            Form8Dot6MinimumTensionAnchorage(l_b_rqd=l_b_rqd, phi=phi)
+
+    def test_integration_with_form_8_3(self) -> None:
+        """Test the integration with Form8Dot3RequiredAnchorageLength."""
+        # example values
+        phi = 12  # mm
+        sigma_sd = 435  # MPA
+        f_bd = 2.9  # MPA
+        l_b_rqd = Form8Dot3RequiredAnchorageLength(phi=phi, sigma_sd=sigma_sd, f_bd=f_bd)
+
+        form_8_7 = Form8Dot6MinimumTensionAnchorage(l_b_rqd=l_b_rqd, phi=phi)
+
+        manually_calculated_result = 135  # mm
+
+        assert form_8_7 == pytest.approx(expected=manually_calculated_result, rel=1e-4)
