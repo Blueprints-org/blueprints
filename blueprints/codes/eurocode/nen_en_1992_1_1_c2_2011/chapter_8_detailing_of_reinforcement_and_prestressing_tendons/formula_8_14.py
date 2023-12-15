69e1dd9c0f36f737d164ae09643637a2d9376879
PetervWestrienen <petervanwestrienen@gmail.com>
2023-12-15 15:48:40 +0100

    (#83) Added formula 8.14 + test

---
+++ blueprints/codes/eurocode/nen_en_1992_1_1_c2_2011/chapter_8_detailing_of_reinforcement_and_prestressing_tendons/formula_8_14.py
@@ -0,0 +1,46 @@
+"""Formula 8.14 from NEN-EN 1992-1-1+C2:2011: Chapter 8: Detailing of reinforcement and prestressing tendons"""
+import numpy as np
+
+# pylint: disable=arguments-differ
+from blueprints.codes.eurocode.nen_en_1992_1_1_c2_2011 import NEN_EN_1992_1_1_C2_2011
+from blueprints.codes.formula import Formula
+from blueprints.type_alias import DIMENSIONLESS, MM
+from blueprints.validations import raise_if_negative
+
+
+class Form8Dot14EquivalentDiameterBundledBars(Formula):
+    """Class representing formula 8.14 for the calculation of the equivalent diameter of bundled bars, :math:`Ø_n`."""
+
+    label = "8.14"
+    source_document = NEN_EN_1992_1_1_C2_2011
+
+    def __init__(
+        self,
+        diameter: MM,
+        n_b: DIMENSIONLESS,
+    ) -> None:
+        """[:math:`Ø_n`] Equivalent diameter of bundled bars [mm].
+
+        NEN-EN 1992-1-1+C2:2011 art.8.9.1(2) - Formula (8.14)
+
+        Parameters
+        ----------
+        diameter : MM
+            [:math:`Ø`] Diameter of the bars [mm].
+        n_b : DIMENSIONLESS
+            [:math:`n_b`] Number of bars in the bundle [-].
+            ≤ 4 for vertical bars in compression and for bars in a lapped joint.
+            ≤ 3 for all other cases.
+        """
+        super().__init__()
+        self.diameter = diameter
+        self.n_b = n_b
+
+    @staticmethod
+    def _evaluate(diameter: MM, n_b: DIMENSIONLESS) -> MM:
+        """Evaluates the formula, for more information see the __init__ method"""
+        raise_if_negative(
+            diameter=diameter,
+            n_b=n_b,
+        )
+        return min(diameter * np.sqrt(n_b), 55)
---
+++ tests/codes/eurocode/nen_en_1992_1_1_c2_2011/chapter_8_detailing_of_reinforcement_and_prestressing_tendons/test_formula_8_14.py
@@ -0,0 +1,56 @@
+"""Testing formula 8.14 of NEN-EN 1992-1-1+C2:2011."""
+# pylint: disable=arguments-differ, duplicate-code
+
+
+import pytest
+
+from blueprints.codes.eurocode.nen_en_1992_1_1_c2_2011.chapter_8_detailing_of_reinforcement_and_prestressing_tendons.formula_8_14 import (
+    Form8Dot14EquivalentDiameterBundledBars,
+)
+from blueprints.validations import NegativeValueError
+
+
+class TestForm8Dot14EquivalentDiameterBundledBars:
+    """Validation for formula 8.14 from NEN-EN 1992-1-1+C2:2011."""
+
+    def test_evaluation(self) -> None:
+        """Test the evaluation of the result."""
+        # example values
+        diameter = 16  # mm
+        n_b = 4  # [-]
+        form_8_14 = Form8Dot14EquivalentDiameterBundledBars(diameter=diameter, n_b=n_b)
+
+        # manually calculated result
+        manually_calculated_result = 32  # mm
+
+        assert form_8_14 == pytest.approx(expected=manually_calculated_result, rel=1e-4)
+
+    def test_evaluation_maximum(self) -> None:
+        """Test the evaluation of the result if the maximum is reached."""
+        # example values
+        diameter = 32  # mm
+        n_b = 4  # [-]
+        form_8_14 = Form8Dot14EquivalentDiameterBundledBars(diameter=diameter, n_b=n_b)
+
+        # manually calculated result
+        manually_calculated_result = 55  # mm
+
+        assert form_8_14 == pytest.approx(expected=manually_calculated_result, rel=1e-4)
+
+    def test_raise_error_if_diameter_is_negative(self) -> None:
+        """Test that a NegativeValueError is raised if diameter is negative."""
+        # example values
+        diameter = 32  # mm
+        n_b = 4  # [-]
+
+        with pytest.raises(NegativeValueError):
+            Form8Dot14EquivalentDiameterBundledBars(diameter=diameter, n_b=n_b)
+
+    def test_raise_error_if_n_b_is_negative(self) -> None:
+        """Test that a NegativeValueError is raised if n_b is negative."""
+        # example values
+        diameter = 32
+        n_b = -4
+
+        with pytest.raises(NegativeValueError):
+            Form8Dot14EquivalentDiameterBundledBars(diameter=diameter, n_b=n_b)
