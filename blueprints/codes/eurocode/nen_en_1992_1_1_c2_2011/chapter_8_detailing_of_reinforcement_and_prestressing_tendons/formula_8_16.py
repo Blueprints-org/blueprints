44a7079e52f0a190ae19f9265b2cff9df94602e5
Enrique García <enrique.garcia@bam.com>
2023-12-19 17:08:19 +0100

    (#87) Adding sub-formula SubForm8Dot16Alpha2 from NEN-EN 1992-1-1+C2:2011

--- blueprints/codes/eurocode/nen_en_1992_1_1_c2_2011/chapter_8_detailing_of_reinforcement_and_prestressing_tendons/formula_8_16.py
+++ blueprints/codes/eurocode/nen_en_1992_1_1_c2_2011/chapter_8_detailing_of_reinforcement_and_prestressing_tendons/formula_8_16.py
@@ -106,3 +106,41 @@ class SubForm8Dot16Alpha1(Formula):
                 return 1.25
             case _:
                 raise ValueError(f"Invalid release type: {release_type}. Valid values are 'gradual' or 'sudden'.")
+
+
+class SubForm8Dot16Alpha2(Formula):
+    """Class representing sub-formula 8.16 for the calculation of the coefficient :math:`α_{2}`."""
+
+    label = "8.16"
+    source_document = NEN_EN_1992_1_1_C2_2011
+
+    def __init__(
+        self,
+        type_of_wire: str,
+    ) -> None:
+        """[:math:`α_{2}`] Coefficient that takes into account the type of wires in the cross-section [-].
+
+        NEN-EN 1992-1-1+C2:2011 art.8.10.2.2(2) - Formula (8.16)
+
+        Parameters
+        ----------
+        type_of_wire : str
+            Type of wire.
+
+            = 'circular' for circular cross-sections;
+
+            = '3_7_wire_strands' for 3 and 7-wire strands;
+        """
+        super().__init__()
+        self.type_of_wire = type_of_wire
+
+    @staticmethod
+    def _evaluate(type_of_wire: str) -> DIMENSIONLESS:
+        """Evaluates the formula, for more information see the __init__ method"""
+        match type_of_wire.lower():
+            case "circular":
+                return 0.25
+            case "3_7_wire_strands":
+                return 0.19
+            case _:
+                raise ValueError(f"Invalid type of wire: {type_of_wire}. Valid values are 'circular' or '3_7_wire_strands'.")
