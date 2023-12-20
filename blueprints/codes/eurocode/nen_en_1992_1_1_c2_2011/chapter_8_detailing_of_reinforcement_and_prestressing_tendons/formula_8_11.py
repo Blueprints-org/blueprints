c4076f97c416e04707bb6e7681f04d768a0c43c9
Enrique García <enrique.garcia@bam.com>
2023-12-20 17:27:05 +0100

    (#87) typehint fix

--- blueprints/codes/eurocode/nen_en_1992_1_1_c2_2011/chapter_8_detailing_of_reinforcement_and_prestressing_tendons/formula_8_11.py
+++ blueprints/codes/eurocode/nen_en_1992_1_1_c2_2011/chapter_8_detailing_of_reinforcement_and_prestressing_tendons/formula_8_11.py
@@ -18,7 +18,7 @@ class Form8Dot11MinimumDesignLapLength(Formula):
         l_b_rqd: MM,
         diameter: MM,
     ) -> None:
-        """[:math:`l0,min`] Design minimum lap length [mm].
+        """[:math:`l_{0,min}`] Design minimum lap length [:math:`mm`].
 
         NEN-EN 1992-1-1+C2:2011 art.8.7.3(1) - Formula (8.11)
 
@@ -26,15 +26,18 @@ class Form8Dot11MinimumDesignLapLength(Formula):
         ----------
         alpha_6 : DIMENSIONLESS
             [:math:`α_6`] Coefficient for the effect of reinforcement ratio [-].
-            = :math:`(\frac{ρ_l}{25})^{0.5} ≤ 1.5` with a minimum of :math:`1.0`.
-            Where: :math:`ρ_l` = reinforcement percentage lapped within :math:`0,65 l_0` from the centre of the lap length
+
+            :math:`= (ρ_l/25)^{0.5} <= 1.5` with a minimum of 1.0.
+
+            Where: :math:`ρ_l` = reinforcement percentage lapped within :math:`0.65 ⋅ l_0` from the centre of the lap length
             considered (see figure 8.8) [-].
         l_b_rqd: MM
-            [:math:`l{b,rqd}`] Basic required anchorage length, for anchoring the force :math:`A_s σ_{sd}` in a straight bar assuming constant
-            bond stress (formula 8.3) [mm].
-            Use your own implementation for this value or use the Form8Dot3RequiredAnchorageLength class.
+            [:math:`l_{b,rqd}`] Basic required anchorage length, for anchoring the force :math:`A_s ⋅ σ_{sd}` in a straight bar assuming constant
+            bond stress (formula 8.3) [:math:`mm`].
+
+            Use your own implementation for this value or use the :class:`Form8Dot3RequiredAnchorageLength` class.
         diameter : MM
-            [:math:`Ø`] Diameter of the bar [mm].
+            [:math:`Ø`] Diameter of the bar [:math:`mm`].
         """
         super().__init__()
         self.alpha_6 = alpha_6
