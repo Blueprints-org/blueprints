3f4a16b8bee4295be5de3831fc5e97de960c19b4
Enrique García Méndez <44614728+egarciamendez@users.noreply.github.com>
2023-12-27 23:20:34 +0100

    adding missing docstrings

---
+++ blueprints/checks/rectangular_concrete_section/__init__.py
@@ -0,0 +1,1 @@
+"""Rectangular concrete section package."""
---
+++ tests/codes/eurocode/nen_en_1992_1_1_c2_2011/chapter_11_lightweight_aggregate_concrete_structures/__init__.py
@@ -0,0 +1,1 @@
+"""Package containing all tests for formulas from NEN-EN 1992-1-1+C2:2011: Chapter 11 - Lightweight aggregate concrete structures."""
