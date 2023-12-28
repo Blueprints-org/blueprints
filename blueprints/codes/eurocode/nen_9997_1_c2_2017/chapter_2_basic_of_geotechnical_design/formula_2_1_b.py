3db50e5c98910ebb9ed04de37b5318447d6b8729
PetervWestrienen <petervanwestrienen@gmail.com>
2023-12-28 10:26:04 +0100

    (#100) added 2.1b

---
+++ blueprints/codes/eurocode/nen_9997_1_c2_2017/chapter_2_basic_of_geotechnical_design/formula_2_1_b.py
@@ -0,0 +1,36 @@
+""""Formula 2.1b from NEN 9997-1+C2:2017: Chapter 2: Basis of geotechnical design."""
+from blueprints.codes.formula import Formula
+from blueprints.type_alias import DIMENSIONLESS
+from blueprints.validations import raise_if_negative
+
+
+class Form2Dot1BRepresentativeValue(Formula):
+    """Class representing formula 2.1b for the calculation of the representative value :math:`F_{rep}` of actions."""
+
+    label = "2.1b"
+    source_document = "NEN 9997-1+C2:2017"
+
+    def __init__(self, psi: DIMENSIONLESS, f_k: float) -> None:
+        """[:math:`F_{rep}`] Representative value of actions.
+
+        NEN 9997-1+C2:2017 art.2.4.6.1(2) - Formula (2.1b)
+
+        Parameters
+        ----------
+        psi : DIMENSIONLESS
+            [:math:`Ψ`] factor for converting the characteristic value to the representative value [-].
+        f_k : float
+            [:math:`F_{k}`] Characteristic value of actions.
+        """
+        super().__init__()
+        self.psi = psi
+        self.f_k = f_k
+
+    @staticmethod
+    def _evaluate(
+        psi: DIMENSIONLESS,
+        f_k: float,
+    ) -> float:
+        """Evaluates the formula, for more information see the __init__ method."""
+        raise_if_negative(psi=psi, f_k=f_k)
+        return psi * f_k
