2a0052b7246f5ee113c243464d60ab8a24f831bb
PetervWestrienen <petervanwestrienen@gmail.com>
2024-01-05 13:18:36 +0100

    (#109) fixed issue with merges of different branches

---
+++ blueprints/codes/eurocode/nen_9997_1_c2_2017/chapter_2_basic_of_geotechnical_design/__init__.py
@@ -0,0 +1,1 @@
+"""Package contains the formulas from chapter 2: Basic of geotechnical design of NEN 9997-1+C2:2017."""
---
+++ blueprints/codes/eurocode/nen_en_1993_1_1_c2_a1_2016/chapter_2_basic_of_design/__init__.py
@@ -0,0 +1,1 @@
+"""Package contains the formulas of chapter 2: 'Basic of design' of NEN-EN 1993-1-1+C2+A1:2016."""
