e463c42e9ace04bab9eb11b84ce259eee4a00eaa
Klaas <k.j.sicking@student.tudelft.nl>
2024-03-18 15:03:39 +0100

    (#189) added file structure for formula

--- blueprints/codes/eurocode/nen_en_1993_5_2008/chapter_5_ultimate_limit_states/formula_5_2.py
+++ blueprints/codes/eurocode/nen_en_1993_5_2008/chapter_5_ultimate_limit_states/formula_5_2.py
@@ -1,13 +1,16 @@
 """Formula 5.2 from NEN-EN 1993-5:2008 Chapter 5 - Ultimate limit state."""
+from blueprints.codes.eurocode import NEN_EN_1993_5_2008
 from blueprints.codes.formula import Formula
 from blueprints.type_alias import DIMENSIONLESS, KNM, MM3, MPA
 from blueprints.unit_conversion import N_TO_KN
+from blueprints.validations import raise_if_less_or_equal_to_zero
 
 
 class Form5Dot2DesignMomentResistanceClass1Or2(Formula):
     """Class representing a formula 5.2 for design moment resistance for Class 1 or 2 cross-sections."""
 
     label = "5.2"
+    source_document = NEN_EN_1993_5_2008
 
     def __init__(
         self,
@@ -36,6 +39,13 @@ class Form5Dot2DesignMomentResistanceClass1Or2(Formula):
         self.f_y = f_y
         self.gamma_m_0 = gamma_m_0
 
-    def evaluate(self) -> KNM:
+    @staticmethod
+    def _evaluate(
+        beta_b: DIMENSIONLESS,
+        w_pl: MM3,
+        f_y: MPA,
+        gamma_m_0: DIMENSIONLESS,
+    ) -> KNM:
         """Evaluates the formula, for more information see the __init__ method."""
-        return (self.beta_b * self.w_pl * self.f_y / self.gamma_m_0) * N_TO_KN
+        raise_if_less_or_equal_to_zero(beta_b=beta_b, w_pl=w_pl, f_y=f_y, gamma_m_0=gamma_m_0)
+        return (beta_b * w_pl * f_y / gamma_m_0) * N_TO_KN
