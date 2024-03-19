ec087a190d231a925d7024804fc018aea941d6f3
Klaas <k.j.sicking@student.tudelft.nl>
2024-03-19 12:21:57 +0100

    (#192) used function 5.2, changed w_pl into w_el to get function 5.3

---
+++ tests/codes/eurocode/nen_en_1993_5_2008/chapter_5_ultimate_limit_states/test_formula_5_3.py
@@ -0,0 +1,54 @@
+"""Testing formula 5.2 of NEN-EN 1993-5:2008."""
+
+import pytest
+
+from blueprints.codes.eurocode.nen_en_1993_5_2008.chapter_5_ultimate_limit_states.formula_5_3 import Form5Dot3DesignMomentResistanceClass3
+from blueprints.validations import LessOrEqualToZeroError
+
+
+class TestForm5Dot3DesignMomentResistanceClass3:
+    """Validation for formula 5.2 from NEN-EN 1993-5:2008."""
+
+    def test_evaluation(self) -> None:
+        """Test the evaluation of the result."""
+        # Example values
+        beta_b = 0.5  # Dimensionless
+        w_el = 20  # MM3
+        f_y = 200  # MPA
+        gamma_m_0 = 0.8  # Dimensionless
+        form = Form5Dot3DesignMomentResistanceClass3(beta_b=beta_b, w_el=w_el, f_y=f_y, gamma_m_0=gamma_m_0)
+
+        # Expected result, manually calculated
+        expected = 2.5
+
+        assert form == pytest.approx(expected)
+
+    @pytest.mark.parametrize(
+        ("beta_b", "w_el", "f_y", "gamma_m_0"),
+        [
+            (-0.5, 1, 1, 1),  # beta_b is negative
+            (0, 1, 1, 1),  # beta_b is zero
+            (1, -0.5, 1, 1),  # w_el is negative
+            (1, 0, 1, 1),  # w_el is zero
+            (1, 1, -0.5, 1),  # f_y is negative
+            (1, 1, 0, 1),  # f_y is zero
+            (1, 1, 1, -0.5),  # gamma_m_0 is negative
+            (1, 1, 1, 0),  # gamma_m_0 is zero
+        ],
+    )
+    def test_raise_error_when_negative_or_zero_n_t_rd_is_given(self, beta_b: float, w_el: float, f_y: float, gamma_m_0: float) -> None:
+        """Test a zero value for n_t_rd."""
+        with pytest.raises(LessOrEqualToZeroError):
+            Form5Dot3DesignMomentResistanceClass3(beta_b=beta_b, w_el=w_el, f_y=f_y, gamma_m_0=gamma_m_0)
+
+    def test_latex_output(self) -> None:
+        """Test the latex implementation."""
+        beta_b = 0.5  # Dimensionless
+        w_el = 20  # MM3
+        f_y = 200  # MPA
+        gamma_m_0 = 0.8  # Dimensionless
+
+        form = Form5Dot3DesignMomentResistanceClass3(beta_b=beta_b, w_el=w_el, f_y=f_y, gamma_m_0=gamma_m_0)
+        assert form.latex().complete == r"M_{c,Rd} = \beta_B W_{el} f_y / \gamma_{M0} = 0.5 \cdot 20 \cdot 200 / 0.8 / 1000 = 2.5"
+        assert form.latex().short == r"M_{c,Rd} = 2.5"
+        assert str(form.latex()) == r"M_{c,Rd} = \beta_B W_{el} f_y / \gamma_{M0} = 0.5 \cdot 20 \cdot 200 / 0.8 / 1000 = 2.5"
