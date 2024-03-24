d7b9b3b2c16d1d2cbdb54b130e2a6651250bcd88
Enrique García Méndez <44614728+egarciamendez@users.noreply.github.com>
2024-03-24 19:15:03 +0100

    (#200) fixing tests

--- tests/codes/eurocode/nen_en_1993_5_2008/chapter_5_ultimate_limit_states/test_formula_5_8.py
+++ tests/codes/eurocode/nen_en_1993_5_2008/chapter_5_ultimate_limit_states/test_formula_5_8.py
@@ -2,11 +2,11 @@
 
 import pytest
 
-from blueprints.codes.eurocode.nen_en_1993_5_2008.chapter_5_ultimate_limit_states.formula_5_8 import Form5Dot8RelativeSlenderness
+from blueprints.codes.eurocode.nen_en_1993_5_2008.chapter_5_ultimate_limit_states.formula_5_8 import Form5Dot8RelativeWebSlenderness
 from blueprints.validations import LessOrEqualToZeroError
 
 
-class TestForm5Dot8RelativeSlenderness:
+class TestForm5Dot8RelativeWebSlenderness:
     """Validation for formula 5.8 from NEN-EN 1993-5:2008."""
 
     def test_evaluation(self) -> None:
@@ -17,7 +17,7 @@ class TestForm5Dot8RelativeSlenderness:
         f_y = 250  # MPA
         e = 200000  # MPA
 
-        form = Form5Dot8RelativeSlenderness(c=c, t_w=t_w, f_y=f_y, e=e)
+        form = Form5Dot8RelativeWebSlenderness(c=c, t_w=t_w, f_y=f_y, e=e)
 
         # Expected result, manually calculated
         expected = 0.611647366
@@ -27,35 +27,50 @@ class TestForm5Dot8RelativeSlenderness:
     @pytest.mark.parametrize(
         ("c", "t_w", "f_y", "e"),
         [
-            (500, -10, 250, 200000),  # t_w is negative
+            (0, 10, 250, 200000),  # c is zero
+            (-500, 10, 250, 200000),  # c is negative
             (500, 0, 250, 200000),  # t_w is zero
-            (500, 10, -250, 200000),  # f_y is negative
+            (500, -10, 250, 200000),  # t_w is negative
             (500, 10, 0, 200000),  # f_y is zero
-            (500, 10, 250, -200000),  # E is negative
+            (500, 10, -250, 200000),  # f_y is negative
             (500, 10, 250, 0),  # E is zero
+            (500, 10, 250, -200000),  # E is negative
         ],
     )
     def test_raise_error_when_negative_or_zero_values_are_given(self, c: float, t_w: float, f_y: float, e: float) -> None:
         """Test a zero and negative value for parameters c, t_w, f_y, and e."""
         with pytest.raises(LessOrEqualToZeroError):
-            Form5Dot8RelativeSlenderness(c=c, t_w=t_w, f_y=f_y, e=e)
+            Form5Dot8RelativeWebSlenderness(c=c, t_w=t_w, f_y=f_y, e=e)
 
-    def test_latex_output(self) -> None:
+    @pytest.mark.parametrize(
+        ("representation", "expected"),
+        [
+            (
+                "complete",
+                r"\overline{\lambda} = 0.346 \cdot \frac{c}{t_w} \sqrt{\frac{f_y}{E}} = "
+                r"0.346 \cdot \frac{500}{10} \sqrt{\frac{250}{200000}} = 0.6116473657263635",
+            ),
+            ("short", r"\overline{\lambda} = 0.6116473657263635"),
+            (
+                "string",
+                r"\overline{\lambda} = 0.346 \cdot \frac{c}{t_w} \sqrt{\frac{f_y}{E}} = "
+                r"0.346 \cdot \frac{500}{10} \sqrt{\frac{250}{200000}} = 0.6116473657263635",
+            ),
+        ],
+    )
+    def test_latex_output(self, representation: str, expected: str) -> None:
         """Test the latex implementation."""
         c = 500  # MM
         t_w = 10  # MM
         f_y = 250  # MPA
         e = 200000  # MPA
 
-        form = Form5Dot8RelativeSlenderness(c=c, t_w=t_w, f_y=f_y, e=e)
-        assert (
-            form.latex().complete
-            == r"\overline{\lambda} = 0.346 \cdot \frac{c}{t_w} \sqrt{\frac{f_y}{E}} = 0.346 \cdot \frac{500}{10} \sqrt{\frac{250}{200000}} = "
-            + str(form)
-        )
-        assert form.latex().short == r"\overline{\lambda} = " + str(form)
-        assert str(
-            form.latex()
-        ) == r"\overline{\lambda} = 0.346 \cdot \frac{c}{t_w} \sqrt{\frac{f_y}{E}} = 0.346 \cdot \frac{500}{10} \sqrt{\frac{250}{200000}} = " + str(
-            form
-        )
+        form = Form5Dot8RelativeWebSlenderness(c=c, t_w=t_w, f_y=f_y, e=e).latex()
+
+        actual = {
+            "complete": form.complete,
+            "short": form.short,
+            "string": str(form),
+        }
+
+        assert actual[representation] == expected, f"{representation} representation failed."
