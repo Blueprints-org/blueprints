08a5d732f1be98dc758da41d95e9e91ee51a97b8
Enrique García Méndez <44614728+egarciamendez@users.noreply.github.com>
2024-03-24 18:53:11 +0100

    Update formula_5_7.py

--- blueprints/codes/eurocode/nen_en_1993_5_2008/chapter_5_ultimate_limit_states/formula_5_7.py
+++ blueprints/codes/eurocode/nen_en_1993_5_2008/chapter_5_ultimate_limit_states/formula_5_7.py
@@ -38,7 +38,7 @@ class Form5Dot7ShearBucklingResistance(Formula):
             [:math:`f_{bv}`] Shear buckling strength according to Table 6-1 of EN 1993-1-3 for a web without stiffening
             at the support and for a relative web slenderness [:math:`MPa`]
 
-            Use your own implementation for this value or use :class:`Form5Dot8RelativeSlenderness` class.
+            Use your own implementation for this value or use :class:`Form5Dot8RelativeWebSlenderness` class.
         gamma_m_0 : float
             [:math:`γ_{M0}`] Partial factor for material properties [-].
         """
