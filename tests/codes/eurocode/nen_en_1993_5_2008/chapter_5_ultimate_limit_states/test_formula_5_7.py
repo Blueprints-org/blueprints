0b39fc1e32bdef9ae092053e8bd3a326f205cdc5
Enrique García Méndez <44614728+egarciamendez@users.noreply.github.com>
2024-03-24 18:39:58 +0100

    fixing tests

--- tests/codes/eurocode/nen_en_1993_5_2008/chapter_5_ultimate_limit_states/test_formula_5_7.py
+++ tests/codes/eurocode/nen_en_1993_5_2008/chapter_5_ultimate_limit_states/test_formula_5_7.py
@@ -13,46 +13,76 @@ class TestForm5Dot7ShearBucklingResistance:
         """Test the evaluation of the result."""
         # Example values
         h = 500  # MM
-        tf = 20  # MM
-        tw = 10  # MM
+        t_f = 20  # MM
+        t_w = 10  # MM
         f_bv = 300  # kN
         gamma_m_0 = 1.1
 
-        form = Form5Dot7ShearBucklingResistance(h=h, t_f=tf, t_w=tw, f_bv=f_bv, gamma_m_0=gamma_m_0)
+        form = Form5Dot7ShearBucklingResistance(
+            h=h,
+            t_f=t_f,
+            t_w=t_w,
+            f_bv=f_bv,
+            gamma_m_0=gamma_m_0,
+        )
 
         # Expected result, manually calculated
-        expected = 1309090.909
+        expected = 1309.09
 
         assert form == pytest.approx(expected)
 
     @pytest.mark.parametrize(
-        ("h", "tf", "tw", "f_bv", "gamma_m_0"),
+        ("h", "t_f", "t_w", "f_bv", "gamma_m_0"),
         [
-            (500, -20, 10, 300, 1.1),  # tf is negative
-            (500, 0, 10, 300, 1.1),  # tf is zero
-            (500, 20, -10, 300, 1.1),  # tw is negative
-            (500, 20, 0, 300, 1.1),  # tw is zero
+            (0, 20, 10, 300, 1.1),  # h is zero
+            (-500, 20, 10, 300, 1.1),  # h is negative
+            (500, -20, 10, 300, 1.1),  # t_f is negative
+            (500, 0, 10, 300, 1.1),  # t_f is zero
+            (500, 20, -10, 300, 1.1),  # t_w is negative
+            (500, 20, 0, 300, 1.1),  # t_w is zero
             (500, 20, 10, 300, 0),  # gamma_m_0 is zero
+            (500, 20, 10, 300, -1),  # gamma_m_0 is negative
         ],
     )
-    def test_raise_error_when_negative_or_zero_values_are_given(self, h: float, tf: float, tw: float, f_bv: float, gamma_m_0: float) -> None:
-        """Test a zero and negative value for parameters h, tf, tw, f_bv, and gamma_m_0."""
+    def test_raise_error_when_negative_or_zero_values_are_given(self, h: float, t_f: float, t_w: float, f_bv: float, gamma_m_0: float) -> None:
+        """Test a zero and negative value for parameters h, t_f, t_w, f_bv, and gamma_m_0."""
         with pytest.raises(LessOrEqualToZeroError):
-            Form5Dot7ShearBucklingResistance(h=h, t_f=tf, t_w=tw, f_bv=f_bv, gamma_m_0=gamma_m_0)
+            Form5Dot7ShearBucklingResistance(h=h, t_f=t_f, t_w=t_w, f_bv=f_bv, gamma_m_0=gamma_m_0)
 
-    def test_latex_output(self) -> None:
+    @pytest.mark.parametrize(
+        ("representation", "expected"),
+        [
+            (
+                "complete",
+                r"V_{b,Rd} = \frac{\left(h - t_f \right) t_w f_{bv}}{\gamma_{M0}} = \frac{(500 - 20) \cdot 10 \cdot 300}{1.1} = 1309.090909090909",
+            ),
+            ("short", r"V_{b,Rd} = 1309.090909090909"),
+            (
+                "string",
+                r"V_{b,Rd} = \frac{\left(h - t_f \right) t_w f_{bv}}{\gamma_{M0}} = \frac{(500 - 20) \cdot 10 \cdot 300}{1.1} = 1309.090909090909",
+            ),
+        ],
+    )
+    def test_latex_output(self, representation: str, expected: str) -> None:
         """Test the latex implementation."""
         h = 500  # MM
-        tf = 20  # MM
-        tw = 10  # MM
+        t_f = 20  # MM
+        t_w = 10  # MM
         f_bv = 300  # kN
         gamma_m_0 = 1.1
 
-        form = Form5Dot7ShearBucklingResistance(h=h, t_f=tf, t_w=tw, f_bv=f_bv, gamma_m_0=gamma_m_0)
-        assert form.latex().complete == (
-            r"V_{b,Rd} = \frac{\left(h - t_f \right) t_w f_{bv}}{\gamma_{M0}} = \frac{(500 - 20) \cdot 10 \cdot 300}{1.1} = "
-        ) + str(form)
-        assert form.latex().short == r"V_{b,Rd} = " + str(form)
-        assert str(form.latex()) == (
-            r"V_{b,Rd} = \frac{\left(h - t_f \right) t_w f_{bv}}{\gamma_{M0}} = \frac{(500 - 20) \cdot 10 \cdot 300}{1.1} = "
-        ) + str(form)
+        form = Form5Dot7ShearBucklingResistance(
+            h=h,
+            t_f=t_f,
+            t_w=t_w,
+            f_bv=f_bv,
+            gamma_m_0=gamma_m_0,
+        ).latex()
+
+        actual = {
+            "complete": form.complete,
+            "short": form.short,
+            "string": str(form),
+        }
+
+        assert actual[representation] == expected, f"{representation} representation failed."
