bbf7036511c2f3db31a095247533e7bea3666a94
Enrique García Méndez <44614728+egarciamendez@users.noreply.github.com>
2024-03-24 23:39:46 +0100

    (#103) ruff

--- blueprints/codes/eurocode/nen_9997_1_c2_2017/chapter_1_general_rules/__init__.py
+++ blueprints/codes/eurocode/nen_9997_1_c2_2017/chapter_1_general_rules/__init__.py
@@ -1,1 +1,1 @@
-""""Package representing the formulas chapter 1 from NEN-EN 1997-1:2017."""
+"""Package representing the formulas chapter 1 from NEN-EN 1997-1:2017."""
--- tests/codes/eurocode/nen_9997_1_c2_2017/chapter_1_general_rules/__init__.py
+++ tests/codes/eurocode/nen_9997_1_c2_2017/chapter_1_general_rules/__init__.py
@@ -1,1 +1,1 @@
-""""Package for testing the formulas from chapter 1 from NEN-EN 1997-1:2017."""
+"""Package for testing the formulas from chapter 1 from NEN-EN 1997-1:2017."""
