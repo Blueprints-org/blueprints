d2a0dea7457cb81a6faa30915a8b7c343e679a6e
Enrique García Méndez <44614728+egarciamendez@users.noreply.github.com>
2024-03-24 19:53:38 +0100

    (#202) fixing tests

--- tests/codes/eurocode/nen_en_1993_5_2008/chapter_5_ultimate_limit_states/test_formula_5_10.py
+++ tests/codes/eurocode/nen_en_1993_5_2008/chapter_5_ultimate_limit_states/test_formula_5_10.py
@@ -25,10 +25,10 @@ class TestForm5Dot10ReductionFactorShear:
     @pytest.mark.parametrize(
         ("v_ed", "v_pl_rd"),
         [
-            (-100, 200),  # v_ed is negative
-            (100, -200),  # v_pl_rd is negative
             (0, 200),  # v_ed is zero
+            (-100, 200),  # v_ed is negative
             (100, 0),  # v_pl_rd is zero
+            (100, -200),  # v_pl_rd is negative
         ],
     )
     def test_raise_error_when_negative_or_zero_values_are_given(self, v_ed: float, v_pl_rd: float) -> None:
@@ -36,17 +36,32 @@ class TestForm5Dot10ReductionFactorShear:
         with pytest.raises(LessOrEqualToZeroError):
             Form5Dot10ReductionFactorShear(v_ed=v_ed, v_pl_rd=v_pl_rd)
 
-    def test_latex_output(self) -> None:
+    @pytest.mark.parametrize(
+        ("representation", "expected"),
+        [
+            (
+                "complete",
+                r"\rho = \left(2 \cdot \frac{V_{Ed}}{V_{pl,Rd}} - 1\right)^2 = \left(2 \cdot \frac{100}{400} - 1\right)^2 = 0.25",
+            ),
+            ("short", r"\rho = 0.25"),
+            (
+                "string",
+                r"\rho = \left(2 \cdot \frac{V_{Ed}}{V_{pl,Rd}} - 1\right)^2 = \left(2 \cdot \frac{100}{400} - 1\right)^2 = 0.25",
+            ),
+        ],
+    )
+    def test_latex_output(self, representation: str, expected: str) -> None:
         """Test the latex implementation."""
+        # Example values
         v_ed = 100  # kN
-        v_pl_rd = 200  # kN
+        v_pl_rd = 400  # kN
 
-        form = Form5Dot10ReductionFactorShear(v_ed=v_ed, v_pl_rd=v_pl_rd)
-        assert (
-            form.latex().complete
-            == r"\rho = \left(2 \cdot \frac{V_{Ed}}{V_{pl,Rd}} - 1\right)^2 = \left(2 \cdot \frac{100}{200} - 1\right)^2 = " + str(form)
-        )
-        assert form.latex().short == r"\rho = " + str(form)
-        assert str(
-            form.latex()
-        ) == r"\rho = \left(2 \cdot \frac{V_{Ed}}{V_{pl,Rd}} - 1\right)^2 = \left(2 \cdot \frac{100}{200} - 1\right)^2 = " + str(form)
+        form = Form5Dot10ReductionFactorShear(v_ed=v_ed, v_pl_rd=v_pl_rd).latex()
+
+        actual = {
+            "complete": form.complete,
+            "short": form.short,
+            "string": str(form),
+        }
+
+        assert actual[representation] == expected, f"{representation} representation failed."
