e3019652b64b4a219270c247f7355d433a3ad58f
Enrique García Méndez <44614728+egarciamendez@users.noreply.github.com>
2024-03-25 14:23:26 +0100

    Update blueprints/codes/eurocode/nen_en_1993_5_2008/chapter_5_ultimate_limit_states/formula_5_7.py
    Co-authored-by: Wichard Bron <102153534+bro-wi@users.noreply.github.com>

--- blueprints/codes/eurocode/nen_en_1993_5_2008/chapter_5_ultimate_limit_states/formula_5_7.py
+++ blueprints/codes/eurocode/nen_en_1993_5_2008/chapter_5_ultimate_limit_states/formula_5_7.py
@@ -71,10 +71,10 @@ class Form5Dot7ShearBucklingResistance(Formula):
         """Returns LatexFormula object for formula 5.7."""
         return LatexFormula(
             return_symbol=r"V_{b,Rd}",
-            result=str(self),
+            result=f"{self:.3f}",,
             equation=latex_fraction(numerator=r"\left(h - t_f \right) t_w f_{bv}", denominator=r"\gamma_{M0}"),
             numeric_equation=latex_fraction(
-                numerator=rf"({self.h} - {self.t_f}) \cdot {self.t_w} \cdot {self.f_bv}",
+                numerator=rf"({self.h:.2f} - {self.t_f:.2f}) \cdot {self.t_w} \cdot {self.f_bv:.3f}",
                 denominator=self.gamma_m_0,
             ),
             comparison_operator_label="=",
