e87a6203c2724464b5914787237de468876272fe
Enrique García Méndez <44614728+egarciamendez@users.noreply.github.com>
2024-04-23 11:37:42 +0200

    Update tests/materials/test_concrete.py
    Co-authored-by: Sina Zel taat <111974143+SZeltaat@users.noreply.github.com>

--- tests/materials/test_concrete.py
+++ tests/materials/test_concrete.py
@@ -59,6 +59,15 @@ class TestConcreteMaterial:
         """Tests the f_ck_cube property."""
         assert fixture_concrete_material_c30_37.f_ck_cube == 37
 
+    def test_f_ck_cube_raises_value_error(self) -> None:
+        """Tests the f_ck_cube property raises a ValueError when the ConcreteStrengthClass is invalid."""
+        invalid_concrete_strength_class = type(
+            "InvalidConcreteStrengthClass", (), {"value": "invalid_value"}
+        )()
+        invalid_concrete_material = ConcreteMaterial(invalid_concrete_strength_class)
+        with pytest.raises(ValueError):
+            invalid_concrete_material.f_ck_cube
+
     def test_f_cd(self, fixture_concrete_material_c30_37: ConcreteMaterial) -> None:
         """Tests the f_cd property."""
         assert fixture_concrete_material_c30_37.f_cd == 20.0
