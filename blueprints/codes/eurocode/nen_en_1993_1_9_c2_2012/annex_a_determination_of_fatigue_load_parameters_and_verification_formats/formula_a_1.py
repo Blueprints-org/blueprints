93320d745a253636ca87ffddf1e94717264a4dea
gerjan.dorgelo <gerjan.dorgelo@anteagroup.nl>
2024-05-21 16:07:20 +0200

    added .md files and LaTeX

--- blueprints/codes/eurocode/nen_en_1993_1_9_c2_2012/annex_a_determination_of_fatigue_load_parameters_and_verification_formats/formula_a_1.py
+++ blueprints/codes/eurocode/nen_en_1993_1_9_c2_2012/annex_a_determination_of_fatigue_load_parameters_and_verification_formats/formula_a_1.py
@@ -2,12 +2,13 @@
 
 from blueprints.codes.eurocode.nen_en_1993_1_9_c2_2012 import NEN_EN_1993_1_9_C2_2012
 from blueprints.codes.formula import Formula
+from blueprints.codes.latex_formula import LatexFormula
 from blueprints.type_alias import DIMENSIONLESS
 from blueprints.validations import raise_if_negative, raise_if_less_or_equal_to_zero
 from blueprints.validations import raise_if_lists_differ_in_length, raise_if_list_is_empty
 
 class FormADot1DamageDuringDesignLife(Formula):
-    """Class representing formula A.1 for the calculation of the damage during the design life, D_d"""
+    """Class representing formula A.1 for the calculation of the damage during the design life, :math:`D_d`"""
 
     label = "A.1"
     source_document = NEN_EN_1993_1_9_C2_2012
@@ -18,16 +19,16 @@ class FormADot1DamageDuringDesignLife(Formula):
         N_R: list[DIMENSIONLESS]
 
     ) -> None:
-        """[fcm(t)] The calculation of the damage during the design life [-].
+        """[:math:`D_d`] The calculation of the damage during the design life [:math:`-`].
 
         NEN-EN 1993-1-9+C2:2012 art.A.5 - Formula (A.1)
 
         Parameters
         ----------
         n_E : list[DIMENSIONLESS]
-            [n_E] Contains number of cycles associated with the stress range γ_Ff·Δδ_i for each band i in the factored spectrum [-].
+            [:math:`n_E`] Contains number of cycles associated with the stress range γ_Ff·Δδ_i for each band i in the factored spectrum [:math:`-`].
         N_R : list[DIMENSIONLESS]
-            [N_R] Contains the endurance (in cycles) obtained from the factored Δδ_C/γ_Mf-N_r curve for each stress range of γ_Ff·Δδ_i [-]
+            [:math:`N_R`] Contains the endurance (in cycles) obtained from the factored Δδ_C/γ_Mf-N_r curve for each stress range of γ_Ff·Δδ_i [:math:`-`]
 
         Returns
         -------
@@ -48,3 +49,13 @@ class FormADot1DamageDuringDesignLife(Formula):
         raise_if_negative(n_E_min=min(n_E))
         raise_if_less_or_equal_to_zero(N_R_min=min(N_R))
         return sum([n_E[i] / N_R[i] for i in range(len(n_E))])
+
+    def latex(self) -> LatexFormula:
+        """Returns LatexFormula object for formula A.1."""
+        return LatexFormula(
+            return_symbol=r"D_d",
+            result=f"{self:.3f}",
+            equation=r"\sum_{i}^{n} \frac{n_{Ei}}{N_Ri}",
+            numeric_equation="".join(rf"\frac{{{self.n_E[i]:.3f}}}{{{self.N_R[i]:.3f}}} + " for i in range(len(self.N_R)))[:-3],
+            comparison_operator_label="=",
+        )
--- tests/codes/eurocode/nen_en_1993_1_9_c2_2012/annex_a_determination_of_fatigue_load_parameters_and_verification_formats/test_formula_a_1.py
+++ tests/codes/eurocode/nen_en_1993_1_9_c2_2012/annex_a_determination_of_fatigue_load_parameters_and_verification_formats/test_formula_a_1.py
@@ -12,53 +12,79 @@ class TestFormADot1DamageDuringDesignLife:
     def test_evaluation(self) -> None:
         """Test the evaluation of the result."""
         # example values
-        n_E = [5, 4, 3]
-        N_R = [10, 20, 30]
+        n_E = [5, 4, 3]  # [-]
+        N_R = [10, 20, 30]  # [-]
 
         form_a_1 = FormADot1DamageDuringDesignLife(n_E=n_E, N_R=N_R)
         # manually calculated result
-        manually_calculated_result = 5 / 10 + 4 / 20 + 3 / 30
+        manually_calculated_result = 5 / 10 + 4 / 20 + 3 / 30  # -
 
         assert form_a_1 == pytest.approx(expected=manually_calculated_result, rel=1e-9)
 
     def test_raise_error_if_negative_n_E(self) -> None:
         """Test that a NegativeValueError is raised when a negative value is passed for n_E."""
-        n_E = [-5, 4, 3]
-        N_R = [10, 20, 30]
+        n_E = [-5, 4, 3]  # [-]
+        N_R = [10, 20, 30]  # [-]
         with pytest.raises(NegativeValueError):
             FormADot1DamageDuringDesignLife(n_E=n_E, N_R=N_R)
 
     def test_raise_error_if_negative_N_R(self) -> None:
         """Test that a LessOrEqualToZeroError is raised when a negative value is passed for N_R."""
-        n_E = [5, 4, 3]
-        N_R = [10, -20, 30]
+        n_E = [5, 4, 3]  # [-]
+        N_R = [10, -20, 30]  # [-]
         with pytest.raises(LessOrEqualToZeroError):
             FormADot1DamageDuringDesignLife(n_E=n_E, N_R=N_R)
 
     def test_raise_error_if_zero_N_R(self) -> None:
         """Test that a LessOrEqualToZeroError is raised when a zero value is passed for N_R."""
-        n_E = [5, 4, 3]
-        N_R = [10, 20, 0]
+        n_E = [5, 4, 3]  # [-]
+        N_R = [10, 20, 0]  # [-]
         with pytest.raises(LessOrEqualToZeroError):
             FormADot1DamageDuringDesignLife(n_E=n_E, N_R=N_R)
 
     def test_raise_error_if_n_E_is_empty(self) -> None:
         """Test that a EmptyListError is raised when an empty list is passed for n_E."""
-        n_E = []
-        N_R = [10, 20, 30]
+        n_E = []  # [-]
+        N_R = [10, 20, 30]  # [-]
         with pytest.raises(EmptyListError):
             FormADot1DamageDuringDesignLife(n_E=n_E, N_R=N_R)
 
     def test_raise_error_if_N_R_is_empty(self) -> None:
         """Test that a EmptyListError is raised when an empty list is passed for N_R."""
-        n_E = [5, 4, 3]
-        N_R = []
+        n_E = [5, 4, 3]  # [-]
+        N_R = []  # [-]
         with pytest.raises(EmptyListError):
             FormADot1DamageDuringDesignLife(n_E=n_E, N_R=N_R)
 
     def test_raise_error_if_n_E_and_N_R_are_different_length(self) -> None:
         """Test that a ListsNotSameLengthError is raised when n_E and N_R are not the same length."""
-        n_E = [5, 4]
-        N_R = [10, 20, 30]
+        n_E = [5, 4]  # [-]
+        N_R = [10, 20, 30]  # [-]
         with pytest.raises(ListsNotSameLengthError):
             FormADot1DamageDuringDesignLife(n_E=n_E, N_R=N_R)
+
+    @pytest.mark.parametrize(
+        ("representation", "expected"),
+        [
+            (
+                "complete",
+                r"D_d = \sum_{i}^{n} \frac{n_{Ei}}{N_Ri} = \frac{5.000}{10.000} + \frac{4.000}{20.000} + \frac{3.000}{30.000} = 0.800",
+            ),
+            ("short", r"D_d = 0.800"),
+        ],
+    )
+    def test_latex(self, representation: str, expected: str) -> None:
+        """Test the latex representation of the formula."""
+        # Example values
+        n_E = [5, 4, 3]  # [-]
+        N_R = [10, 20, 30]  # [-]
+
+        # Object to test
+        form_5_14_latex = FormADot1DamageDuringDesignLife(n_E=n_E, N_R=N_R).latex()
+
+        actual = {
+            "complete": form_5_14_latex.complete,
+            "short": form_5_14_latex.short,
+        }
+
+        assert actual[representation] == expected, f"{representation} representation failed."
