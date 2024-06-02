3641ece7ca224802c69f2d0ab1eb93390343b30c
Enrique García Méndez <44614728+egarciamendez@users.noreply.github.com>
2024-06-03 00:22:59 +0200

    Update test_formula_5_16.py

--- tests/codes/eurocode/nen_en_1992_1_1_c2_2011/chapter_5_structural_analysis/test_formula_5_16.py
+++ tests/codes/eurocode/nen_en_1992_1_1_c2_2011/chapter_5_structural_analysis/test_formula_5_16.py
@@ -3,10 +3,10 @@
 import pytest
 
 from blueprints.codes.eurocode.nen_en_1992_1_1_c2_2011.chapter_5_structural_analysis.formula_5_16 import Form5Dot16EffectiveLengthUnbraced
-from blueprints.validations import NegativeValueError
+from blueprints.validations import LessOrEqualToZeroError
 
 
-class TestForm5Dot4TransverseForceEffectBracingSystem:
+class TestForm5Dot16EffectiveLengthUnbraced:
     """Validation for formula 5.16 from NEN-EN 1992-1-1+C2:2011, where left side of max statement is active."""
 
     def test_evaluation_left(self) -> None:
@@ -49,7 +49,7 @@ class TestForm5Dot4TransverseForceEffectBracingSystem:
     )
     def test_raise_error_when_negative_theta_i_is_given(self, k_1: float, k_2: float, height: float) -> None:
         """Test negative values for theta_i, n_a and n_b."""
-        with pytest.raises(NegativeValueError):
+        with pytest.raises(LessOrEqualToZeroError):
             Form5Dot16EffectiveLengthUnbraced(k_1=k_1, k_2=k_2, height=height)
 
     @pytest.mark.parametrize(
@@ -59,27 +59,15 @@ class TestForm5Dot4TransverseForceEffectBracingSystem:
                 "complete",
                 r"l_0 = l \cdot max\left\{"
                 r"\sqrt{1+10 \cdot \frac{k_1 \cdot k_2}{k_1+k_2}}; "
-                r"\left(1+\frac{k_1}{1 \cdot k_1}\right) \cdot \left(1 + \frac{k_2}{1 \cdot k_2}\right) \right\}"
+                r"\left(1+\frac{k_1}{1 + k_1}\right) \cdot \left(1 + \frac{k_2}{1 + k_2}\right) \right\}"
                 r" = "
                 r"4.000 \cdot max\left\{"
                 r"\sqrt{1+10 \cdot \frac{2.000 \cdot 3.000}{2.000+3.000}}; "
-                r"\left(1+\frac{2.000}{1 \cdot 2.000}\right) \cdot "
-                r"\left(1 + \frac{3.000}{1 \cdot 3.000}\right) \right\}"
+                r"\left(1+\frac{2.000}{1 + 2.000}\right) \cdot "
+                r"\left(1 + \frac{3.000}{1 + 3.000}\right) \right\}"
                 r" = 14.422",
             ),
             ("short", r"l_0 = 14.422"),
-            (
-                "string",
-                r"l_0 = l \cdot max\left\{"
-                r"\sqrt{1+10 \cdot \frac{k_1 \cdot k_2}{k_1+k_2}}; "
-                r"\left(1+\frac{k_1}{1 \cdot k_1}\right) \cdot \left(1 + \frac{k_2}{1 \cdot k_2}\right) \right\}"
-                r" = "
-                r"4.000 \cdot max\left\{"
-                r"\sqrt{1+10 \cdot \frac{2.000 \cdot 3.000}{2.000+3.000}}; "
-                r"\left(1+\frac{2.000}{1 \cdot 2.000}\right) \cdot "
-                r"\left(1 + \frac{3.000}{1 \cdot 3.000}\right) \right\}"
-                r" = 14.422",
-            ),
         ],
     )
     def test_latex(self, representation: str, expected: str) -> None:
@@ -95,7 +83,6 @@ class TestForm5Dot4TransverseForceEffectBracingSystem:
         actual = {
             "complete": form_5_16_latex.complete,
             "short": form_5_16_latex.short,
-            "string": str(form_5_16_latex),
         }
 
         assert actual[representation] == expected, f"{representation} representation failed."
