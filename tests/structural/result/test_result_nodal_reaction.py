b454277ad545f3733a092a95d6f73d90c5a381e9
Rick Goeij, de <rick.de.goeij@bam.com>
2024-06-18 14:32:36 +0200

    added nodal reaction tests

--- tests/structural/result/test_result_nodal_reaction.py
+++ tests/structural/result/test_result_nodal_reaction.py
@@ -1,13 +1,13 @@
 import pytest
 
-from blueprints.structural.result.result_internal_forces_1d import ResultInternalForces1D
+from blueprints.structural.result.result_nodal_reaction import ResultNodalReaction
 
 
 class TestResultInternalForces2D:
     """Test class for the ResultInternalForce1D class."""
 
     @pytest.mark.parametrize(
-        "n, v_y, v_z, m_x, m_y, m_z", 
+        "r_x, r_y, r_z, m_x, m_y, m_z", 
         [
             (10, 20, 30, 40, 50, 60),
             (-10, -20, -30, -40, -50, -60),
@@ -15,19 +15,19 @@ class TestResultInternalForces2D:
             (1.5, 2.5, 3.5, 4.5, 5.5, 6.5),
         ]
     )
-    def test_constructor(self, n, v_y, v_z, m_x, m_y, m_z):
+    def test_constructor(self, r_x, r_y, r_z, m_x, m_y, m_z):
         """ test constructor returns expected"""
-        result = ResultInternalForces1D(
-            n=n,
-            v_y=v_y,
-            v_z=v_z,
+        result = ResultNodalReaction(
+            r_x=r_x,
+            r_y=r_y,
+            r_z=r_z,
             m_x=m_x,
             m_y=m_y,
             m_z=m_z
         )
-        assert result.n == n
-        assert result.v_y == v_y
-        assert result.v_z == v_z
+        assert result.r_x == r_x
+        assert result.r_y == r_y
+        assert result.r_z == r_z
         assert result.m_x == m_x
         assert result.m_y == m_y
         assert result.m_z == m_z
