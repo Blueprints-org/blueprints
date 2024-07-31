17945cd08aec0200e4702acd8278b27def424844
Enrique García Méndez <44614728+egarciamendez@users.noreply.github.com>
2024-07-31 10:04:52 +0200

    m to mm unit conversion

--- blueprints/unit_conversion.py
+++ blueprints/unit_conversion.py
@@ -22,6 +22,10 @@ NMM_TO_KNM = 1e-6
 MM2_TO_M2 = 1e-6
 # </editor-fold>
 
+# <editor-fold desc="Length conversion">
+M_TO_MM = 1e3
+# </editor-fold>
+
 # <editor-fold desc="ROTATIONAL CONVERSION">
 RAD_TO_MRAD = 1e3
 MRAD_TO_RAD = 1e-3
