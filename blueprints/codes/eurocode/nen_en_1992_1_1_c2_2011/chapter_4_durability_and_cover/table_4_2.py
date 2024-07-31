1d52164ca1acc1cd626e15ee9e3cd1f1d5e6bd94
Sina Zel taat <sina.zeltaat@wsp.com>
2024-07-31 13:06:39 +0200

    Add formula for table 4.2 from NEN-EN 1992-1-1+C2:2011

---
+++ blueprints/codes/eurocode/nen_en_1992_1_1_c2_2011/chapter_4_durability_and_cover/table_4_2.py
@@ -0,0 +1,55 @@
+"""Table 4.2 from NEN-EN 1992-1-1+C2:2011: Chapter 4 - Durability and cover to reinforcement."""
+
+from blueprints.codes.eurocode.nen_en_1992_1_1_c2_2011 import NEN_EN_1992_1_1_C2_2011
+from blueprints.codes.formula import Formula
+from blueprints.codes.latex_formula import LatexFormula
+from blueprints.type_alias import MM, MM2
+from blueprints.validations import raise_if_negative
+
+
+class TAble4Dot2MinimumCoverWithRegardToBond(Formula):
+    """Class representing the table 4.2 for the calculation of the minimum cover :math:`c_{min,b}` [:math:`mm`] requirements with regard to bond."""
+
+    label = "4.2"
+    source_document = NEN_EN_1992_1_1_C2_2011
+
+    def __init__(
+        self,
+        diameter: MM2,
+        nominal_max_aggregate_size_greater_than_32_mm: bool,
+    ) -> None:
+        """[:math:`c_{nom}`] Calculates the minimum concrete cover with regard to bond [:math:`mm`].
+
+        NEN-EN 1992-1-1+C2:2011 art.4.4.1.2 (3) - Table (4.2)
+
+        Parameters
+        ----------
+        diameter: MM2
+            Diameter of the reinforcement [:math:`mm2`].
+            In case of bundled bars, the equivalent diameter [:math:`phi_{d}`] should be used. See 8.9.1.
+        nominal_max_aggregate_size_greater_than_32_mm: bool
+            Is the nominal maximum aggregate size greater than 32 [:math:`mm`]?
+        """
+        super().__init__()
+        self.diameter = diameter
+        self.nominal_max_aggregate_size_greater_than_32_mm = nominal_max_aggregate_size_greater_than_32_mm
+
+    @staticmethod
+    def _evaluate(
+        diameter: MM2,
+        nominal_max_aggregate_size_greater_than_32_mm: bool,
+    ) -> MM:
+        """For more detailed documentation see the class docstring."""
+        raise_if_negative(diameter=diameter)
+        return diameter + 5 * nominal_max_aggregate_size_greater_than_32_mm
+
+    def latex(self) -> LatexFormula:
+        """Returns LatexFormula object for table 4.2."""
+        suffix = " + 5" if self.nominal_max_aggregate_size_greater_than_32_mm else ""
+        return LatexFormula(
+            return_symbol=r"c_{min,b}",
+            result=str(self),
+            equation=r"(equivalent) diameter" + suffix,
+            numeric_equation=f"{self.diameter}" + suffix,
+            comparison_operator_label="=",
+        )
