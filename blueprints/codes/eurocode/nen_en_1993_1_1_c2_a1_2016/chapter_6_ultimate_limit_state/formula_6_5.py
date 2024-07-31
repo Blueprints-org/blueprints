7da1f865328c230f259fe75c2338642b701d9f5f
gerjan.dorgelo <gerjan.dorgelo@anteagroup.nl>
2024-07-31 13:50:04 +0200

    add last LaTex to 1993

--- blueprints/codes/eurocode/nen_en_1993_1_1_c2_a1_2016/chapter_6_ultimate_limit_state/formula_6_5.py
+++ blueprints/codes/eurocode/nen_en_1993_1_1_c2_a1_2016/chapter_6_ultimate_limit_state/formula_6_5.py
@@ -3,6 +3,7 @@
 # pylint: disable=arguments-differ
 from blueprints.codes.eurocode.nen_en_1993_1_1_c2_a1_2016 import NEN_EN_1993_1_1_C2_A1_2016
 from blueprints.codes.formula import Formula
+from blueprints.codes.latex_formula import LatexFormula
 from blueprints.type_alias import KN, RATIO
 from blueprints.validations import raise_if_less_or_equal_to_zero, raise_if_negative
 
@@ -42,3 +43,13 @@ class Form6Dot5UnityCheckTensileStrength(Formula):
         raise_if_less_or_equal_to_zero(n_t_rd=n_t_rd)
         raise_if_negative(n_ed=n_ed)
         return n_ed / n_t_rd
+
+    def latex(self) -> LatexFormula:
+        """Returns LatexFormula object for formula 6.5."""
+        return LatexFormula(
+            return_symbol=r"N_{Ed}/N_{t,Rd}",
+            result=f"{self:.3f}",
+            equation=r"N_{Ed} / N_{t,Rd}",
+            numeric_equation=rf"{self.n_ed:.3f} / {self.n_t_rd:.3f}",
+            comparison_operator_label="=",
+        )
