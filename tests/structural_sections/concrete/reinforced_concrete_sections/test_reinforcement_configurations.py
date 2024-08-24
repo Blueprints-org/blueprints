316359e87acfb62a28674349ee6ff80988b0c5be
Enrique García Méndez <44614728+egarciamendez@users.noreply.github.com>
2024-08-24 22:02:57 +0200

    fixing tests

---
+++ tests/structural_sections/concrete/reinforced_concrete_sections/test_reinforcement_configurations.py
@@ -0,0 +1,91 @@
+"""Tests reinforcement configurations."""
+
+from typing import Type
+
+import pytest
+
+from blueprints.materials.reinforcement_steel import ReinforcementSteelMaterial
+from blueprints.structural_sections.concrete.reinforced_concrete_sections.reinforcement_configurations import (
+    ReinforcementByDistance,
+    ReinforcementByQuantity,
+)
+
+
+class TestReinforcementByDistance:
+    """Tests for the reinforcement by distance configuration."""
+
+    @pytest.fixture()
+    def reinforcement_by_distance(self) -> ReinforcementByDistance:
+        """Creates a reinforcement by distance configuration."""
+        return ReinforcementByDistance(
+            diameter=12,
+            center_to_center=100,
+            material=ReinforcementSteelMaterial(),
+        )
+
+    def test_n_rebars_per_meter(self, reinforcement_by_distance: ReinforcementByDistance) -> None:
+        """Test the number of rebars per meter."""
+        n_rebars_per_meter = reinforcement_by_distance.n_rebars_per_meter
+        assert n_rebars_per_meter == 10
+
+    def test_area(self, reinforcement_by_distance: ReinforcementByDistance) -> None:
+        """Test the area of the reinforcement."""
+        area = reinforcement_by_distance.area
+        assert area == pytest.approx(expected=1130.9733, rel=1e-4)
+
+    def test__repr__(self, reinforcement_by_distance: ReinforcementByDistance) -> None:
+        """Test the representation of the reinforcement."""
+        representation = repr(reinforcement_by_distance)
+        assert representation == "ReinforcementByDistance|⌀12-100|1131 mm²/m"
+
+    def test_wrong_ctc(self) -> None:
+        """Test the wrong center-to-center distance."""
+        with pytest.raises(ValueError):
+            ReinforcementByDistance(
+                diameter=12,
+                center_to_center=0,
+                material=ReinforcementSteelMaterial(),
+            )
+
+
+class TestReinforcementByQuantity:
+    """Tests for the reinforcement by quantity configuration."""
+
+    @pytest.fixture()
+    def reinforcement_by_quantity(self) -> ReinforcementByQuantity:
+        """Creates a reinforcement by quantity configuration."""
+        return ReinforcementByQuantity(
+            diameter=12,
+            material=ReinforcementSteelMaterial(),
+            n=10,
+        )
+
+    def test_area(self, reinforcement_by_quantity: ReinforcementByQuantity) -> None:
+        """Test the area of the reinforcement."""
+        area = reinforcement_by_quantity.area
+        assert area == pytest.approx(expected=1130.9733, rel=1e-4)
+
+    @pytest.mark.parametrize("diameter", [0, -1])
+    def test_wrong_diameter(self, diameter: int) -> None:
+        """Test the wrong diameter."""
+        with pytest.raises(ValueError):
+            ReinforcementByQuantity(
+                diameter=diameter,
+                material=ReinforcementSteelMaterial(),
+                n=10,
+            )
+
+    @pytest.mark.parametrize(("wrong_n", "expected_error"), [(0, ValueError), (-1, ValueError), (1.5, TypeError)])
+    def test_wrong_n_float(self, wrong_n: int, expected_error: Type[BaseException]) -> None:
+        """Test the wrong number of rebars."""
+        with pytest.raises(expected_error):
+            ReinforcementByQuantity(
+                diameter=12,
+                material=ReinforcementSteelMaterial(),
+                n=wrong_n,
+            )
+
+    def test__repr__(self, reinforcement_by_quantity: ReinforcementByQuantity) -> None:
+        """Test the representation of the reinforcement."""
+        representation = repr(reinforcement_by_quantity)
+        assert representation == "ReinforcementByQuantity|10⌀12|1131 mm²"
