28115ba5d84a1c3fb444b9868e3ecba722b4f1af
Linden <t.van.der.linden@amsterdam.nl>
2024-08-26 16:41:23 +0200

    auto corrections from ruff

--- blueprints/codes/eurocode/nen_9997_1_c2_2017/chapter_2_basic_of_geotechnical_design/formula_2_4.py
+++ blueprints/codes/eurocode/nen_9997_1_c2_2017/chapter_2_basic_of_geotechnical_design/formula_2_4.py
@@ -8,17 +8,18 @@ from blueprints.validations import raise_if_negative
 
 
 class Form2Dot4DesignValueGeotechnicalParameter(Formula):
-    """Class representing formula 2.4 for the check of 
-    the destabilizing load effect 
-    against the stabilizing load effect and friction resistance 
-    :math:`E_dst;d leq E_stb;d + T_d`."""
+    """Class representing formula 2.4 for the check of
+    the destabilizing load effect
+    against the stabilizing load effect and friction resistance
+    :math:`E_dst;d leq E_stb;d + T_d`.
+    """
 
     label = "2.4"
     source_document = NEN_9997_1_C2_2017
 
     def __init__(self, e_dst_d: N, e_stb_d: N, t_d: N) -> None:
-        """Check of the destabilizing load effect 
-        against the stabilizing load effect and friction resistance 
+        """Check of the destabilizing load effect
+        against the stabilizing load effect and friction resistance
         [:math:`E_dst;d leq E_stb;d + T_d`].
 
         NEN 9997-1+C2:2017 art.2.4.7.2(1) - Formula (2.4)
@@ -38,11 +39,7 @@ class Form2Dot4DesignValueGeotechnicalParameter(Formula):
         self.t_d = t_d
 
     @staticmethod
-    def _evaluate(
-        e_dst_d: N,
-        e_stb_d: N,
-        t_d: N
-    ) -> bool:
+    def _evaluate(e_dst_d: N, e_stb_d: N, t_d: N) -> bool:
         """Evaluates the formula, for more information see the __init__ method."""
         raise_if_negative(e_dst_d=e_dst_d, e_stb_d=e_stb_d)
         return e_dst_d <= e_stb_d + t_d
@@ -54,6 +51,6 @@ class Form2Dot4DesignValueGeotechnicalParameter(Formula):
             equation="E_{dst;d} \\leq E_{stb;d} + T_d",
             result="OK" if self.__bool__() else "\\text{Not OK}",
             numeric_equation=f"{self.e_dst_d:.2f} \\leq {self.e_stb_d:.2f} + {self.t_d:.2f} \\to {self.e_dst_d:.2f} \\leq "
-                             f"{self.e_stb_d + self.t_d:.2f}",
+            f"{self.e_stb_d + self.t_d:.2f}",
             comparison_operator_label="\\to",
         )
