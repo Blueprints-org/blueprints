2d97721a85d539be886219f909547ad7269175ef
Rami Evans <rami.evans@rhdhv.com>
2024-09-04 14:27:06 +0200

    Included formula 5.18

---
+++ blueprints/codes/eurocode/nen_en_1992_1_1_c2_2011/chapter_5_structural_analysis/formula_5_18.py
@@ -0,0 +1,92 @@
+"""Formula 5.18 from NEN-EN 1992-1-1+C2:2011: Chapter 5 - Structural Analysis."""
+
+from blueprints.codes.eurocode.nen_en_1992_1_1_c2_2011 import NEN_EN_1992_1_1_C2_2011
+from blueprints.codes.latex_formula import LatexFormula
+from blueprints.type_alias import DIMENSIONLESS, KN, M4, MPA, M
+from blueprints.validations import raise_if_less_or_equal_to_zero
+
+
+class Form5Dot18ComparisonGeneralSecondOrderEffects:
+    """Class representing comparison 5.18 for general second-order effects in buildings."""
+
+    label = "5.18"
+    source_document = NEN_EN_1992_1_1_C2_2011
+
+    def __init__(self, f_ved: KN, k_1: DIMENSIONLESS, n_s: DIMENSIONLESS, length: M, e_cd: MPA, i_c: M4) -> None:
+        """[:math:`CHECK`] Criteria met, based on damage accumulation.
+
+        NEN-EN 1993-1-1+C2:2011 art.5.8.3.3 - Formula (5.18)
+
+        Parameters
+        ----------
+        f_ved : KN
+            [:math: F_{v,ed}`] ... [:math:`kN`].
+        k_1 : DIMENSIONLESS
+            [:math: K_1`] ... [:math:`-`].
+        n_s : DIMENSIONLESS
+            [:math: `n_s`] is the total build-layers [:math:`-`].
+        length : M
+            [:math: `L`] is the total height of the building above the level of constraint. [:math:`m`].
+        e_cd : MPa
+            [:math: `E_{cd}`] is the calculated value of the elastic modulus of concrete. [:math:`MPa`].
+        i_c : M4
+            [:math: `l_c`] is the quadratic surface-moment of the tearing element. [:math:`m^4`].
+
+
+        Returns
+        -------
+        None
+        """
+        self.f_ved = f_ved
+        self.k_1 = k_1
+        self.n_s = n_s
+        self.length = length
+        self.e_cd = e_cd
+        self.i_c = i_c
+
+    @property
+    def left_hand_side(self) -> KN:
+        """Calculate the left hand side of the comparison.
+
+        Returns
+        -------
+            KN: Left hand side
+        """
+        return self.f_ved
+
+    @property
+    def right_hand_side(self) -> KN:
+        """Calculate the left hand side of the comparison.
+
+        Returns
+        -------
+            KN: Right hand side
+        """
+        raise_if_less_or_equal_to_zero(n=(self.n_s + 1.6))
+        return self.k_1 * (self.n_s / (self.n_s + 1.6)) * ((self.e_cd * self.i_c) / self.length**2)
+
+    @property
+    def ratio(self) -> DIMENSIONLESS:
+        """Ratio between left hand side and right hand side of the comparison, commonly referred to as unity check."""
+        return self.left_hand_side / self.right_hand_side
+
+    def __bool__(self) -> bool:
+        """Evaluates the comparison, for more information see the __init__ method."""
+        return self.left_hand_side <= self.right_hand_side
+
+    def __str__(self) -> str:
+        """Return the result of the comparison."""
+        return self.latex().complete
+
+    def latex(self) -> LatexFormula:
+        """Returns LatexFormula object for formula 5.18."""
+        return LatexFormula(
+            return_symbol=r"CHECK",
+            result="OK" if self.__bool__() else "\\text{Not OK}",
+            equation=r"\F_{V,Ed} \leq \frac{n_s}{n_s + 1.6} \cdot \frac{\sum E_{cd} \cdot I_c}{L^2}",
+            numeric_equation=(
+                rf"{self.f_ved:.3f}"
+                rf"\leq \frac{{{self.n_s}}}{{{self.n_s + 1.6}}} \cdot \frac{{\sum {self.e_cd} \cdot {self.i_c}}}{{{self.length ** 2}}}"
+            ),
+            comparison_operator_label=r"\rightarrow",
+        )
---
+++ tests/codes/eurocode/nen_en_1992_1_1_c2_2011/chapter_5_structural_analysis/test_formula_5_18.py
@@ -0,0 +1,72 @@
+"""Testing formula 5.18 from NEN-EN 1993-1-9+C2:2012."""
+
+import pytest
+
+from blueprints.codes.eurocode.nen_en_1992_1_1_c2_2011.chapter_5_structural_analysis.formula_5_18 import Form5Dot18ComparisonGeneralSecondOrderEffects
+from blueprints.validations import LessOrEqualToZeroError
+
+
+class TestForm5Dot18ComparisonGeneralSecondOrderEffects:
+    """Testing formula 5.18 from NEN-EN 1993-1-9+C2:2012."""
+
+    @pytest.mark.parametrize(
+        ("f_ved", "k_1", "n_s", "length", "e_cd", "i_c", "result_manual"),
+        [
+            (0.0533, 0.8, 2, 5, 30_000, 0.0001, True),
+            (0.6667, 1.2, 4, 3, 35_000, 0.0002, False),
+        ],
+    )
+    def test_comparison(self, f_ved: float, k_1: float, n_s: float, length: float, e_cd: float, i_c: float, result_manual: bool) -> None:
+        """Test the evaluation of the comparison."""
+        form_5_18 = bool(Form5Dot18ComparisonGeneralSecondOrderEffects(f_ved=f_ved, k_1=k_1, n_s=n_s, length=length, e_cd=e_cd, i_c=i_c))
+        assert form_5_18 == pytest.approx(expected=result_manual)
+
+    @pytest.mark.parametrize(
+        ("f_ved", "k_1", "n_s", "length", "e_cd", "i_c"),
+        [
+            (0.0533, 0.8, 2, 5, 30_000, 0.0001),
+            (0.6667, 1.2, 4, 3, 35_000, 0.0002),
+        ],
+    )
+    def test_evaluation(self, f_ved: float, k_1: float, n_s: float, length: float, e_cd: float, i_c: float) -> None:
+        """Test the right-hand side evaluation of the result."""
+        form_5_18 = Form5Dot18ComparisonGeneralSecondOrderEffects(f_ved=f_ved, k_1=k_1, n_s=n_s, length=length, e_cd=e_cd, i_c=i_c)
+        assert form_5_18.right_hand_side == pytest.approx(expected=f_ved, abs=1e-3)
+
+    def test_raise_error_if_negative_n_e(self) -> None:
+        """Test that a NegativeValueError is raised when a negative value is passed for n_s."""
+        n_s = -1.6  # -
+        def_test = 10
+        with pytest.raises(LessOrEqualToZeroError):
+            bool(Form5Dot18ComparisonGeneralSecondOrderEffects(f_ved=def_test, k_1=def_test, n_s=n_s, length=def_test, e_cd=def_test, i_c=def_test))
+
+    @pytest.mark.parametrize(
+        ("representation", "expected"),
+        [
+            (
+                "complete",
+                r"CHECK \rightarrow \F_{V,Ed} \leq \frac{n_s}{n_s + 1.6} \cdot \frac{\sum E_{cd} \cdot I_c}{L^2} "
+                r"\rightarrow 0.053\leq \frac{2}{3.6} \cdot \frac{\sum 30000 \cdot 0.0001}{25} \rightarrow OK",
+            ),
+            ("short", r"CHECK \rightarrow OK"),
+        ],
+    )
+    def test_latex(self, representation: str, expected: str) -> None:
+        """Test the latex representation of the comparison."""
+        # Example values
+        f_ved = 0.0533
+        k_1 = 0.8
+        n_s = 2
+        length = 5
+        e_cd = 30_000
+        i_c = 0.0001
+
+        # Object to test
+        form_5_18_latex = Form5Dot18ComparisonGeneralSecondOrderEffects(f_ved=f_ved, k_1=k_1, n_s=n_s, length=length, e_cd=e_cd, i_c=i_c).latex()
+
+        actual = {
+            "complete": form_5_18_latex.complete,
+            "short": form_5_18_latex.short,
+        }
+
+        assert actual[representation] == expected, f"{representation} representation failed."
