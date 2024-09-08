908680657bbab58c92b752fc81a80ba42f7a8b02
Enrique García Méndez <44614728+egarciamendez@users.noreply.github.com>
2024-09-08 20:17:54 +0200

    use dataclass

--- blueprints/materials/soil.py
+++ blueprints/materials/soil.py
@@ -1,5 +1,6 @@
 """Module containing definitions of soil types and soil materials."""
 
+from dataclasses import dataclass
 from enum import Enum
 
 
@@ -59,92 +60,37 @@ class SoilConsistencies(Enum):
     VERY_DENSE = "Very dense"
 
 
+@dataclass
 class SoilType:
-    """Class representing a soil type."""
+    """Class representing a soil type.
 
-    def __init__(
-        self,
-        main_type: SoilMainTypes,
-        minor_composition: SoilMinorCompositions | None = None,
-        consolidation_state: SoilConsolidationStates | None = None,
-        consistency: SoilConsistencies | None = None,
-        description: str | None = None,
-    ) -> None:
-        """Initializes the soil type."""
-        self.main_type = main_type
-        self.minor_composition = minor_composition
-        self.consolidation_state = consolidation_state
-        self.consistency = consistency
-        self.description = description
+    Parameters
+    ----------
+    main_type : SoilMainTypes
+        The soil type of the main portion of the soil mix.
+    minor_composition : SoilMinorCompositions, optional
+        The composition of the minor portion of the soil mix, by default None.
+    consolidation_state : SoilConsolidationStates, optional
+        The consolidation state of the soil, by default None.
+    consistency : SoilConsistencies, optional
+        The consistency of the soil, by default None.
+    description : str, optional
+        The custom description of the soil, by default None.
+    """
 
-    @property
-    def main_type(self) -> SoilMainTypes:
-        """The soil type of the main portion of the soil mix."""
-        return self._main_type
-
-    @main_type.setter
-    def main_type(self, main_type: SoilMainTypes) -> None:
-        if not isinstance(main_type, SoilMainTypes):
-            raise TypeError(f"Expected `main_type` to be of type {SoilMainTypes.__name__}, but got {type(main_type)}")
-
-        self._main_type = main_type
-
-    @property
-    def minor_composition(self) -> SoilMinorCompositions | None:
-        """The composition of the minor portion of the soil mix."""
-        return self._minor_composition
-
-    @minor_composition.setter
-    def minor_composition(self, minor_composition: SoilMinorCompositions | None) -> None:
-        if minor_composition is not None and not isinstance(minor_composition, SoilMinorCompositions):
-            raise TypeError(f"Expected `minor_composition` to be None or of type {SoilMinorCompositions.__name__}, but got {type(minor_composition)}")
-
-        self._minor_composition = minor_composition
-
-    @property
-    def consolidation_state(self) -> SoilConsolidationStates | None:
-        """The consolidation state of the soil."""
-        return self._consolidation_state
-
-    @consolidation_state.setter
-    def consolidation_state(self, consolidation_state: SoilConsolidationStates | None) -> None:
-        if consolidation_state is not None and not isinstance(consolidation_state, SoilConsolidationStates):
-            raise TypeError(
-                f"Expected `consolidation_state` to be None or of type {SoilConsolidationStates.__name__}, but got {type(consolidation_state)}"
-            )
-
-        self._consolidation_state = consolidation_state
-
-    @property
-    def consistency(self) -> SoilConsistencies | None:
-        """The consistency of the soil."""
-        return self._consistency
-
-    @consistency.setter
-    def consistency(self, consistency: SoilConsistencies | None) -> None:
-        if consistency is not None and not isinstance(consistency, SoilConsistencies):
-            raise TypeError(f"Expected `consistency` to be None or of type {SoilConsistencies.__name__}, but got {type(consistency)}")
-
-        self._consistency = consistency
-
-    @property
-    def description(self) -> str | None:
-        """The custom description of the soil."""
-        return self._description
-
-    @description.setter
-    def description(self, description: str | None) -> None:
-        if description is not None and not isinstance(description, str):
-            raise TypeError(f"Expected `description` to be None or of type {str.__name__}, but got {type(description)}")
-
-        self._description = description
+    main_type: SoilMainTypes
+    minor_composition: SoilMinorCompositions | None = None
+    consolidation_state: SoilConsolidationStates | None = None
+    consistency: SoilConsistencies | None = None
+    description: str | None = None
 
     @property
     def default_description(self) -> str:
         """The default description of the soil type based on the `main_type`, `minor_composition`, `consolidation_state`, and `consistency`."""
-        s = self.main_type.value
+        default_description = self.main_type.value
 
         for prop in [self.minor_composition, self.consolidation_state, self.consistency]:
             if prop is not None:
-                s += ", " + prop.value
-        return s
+                assert isinstance(prop, Enum)
+                default_description += ", " + prop.value
+        return default_description
