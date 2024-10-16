a6b64d0a668c4351f0f95f9a85657d1b27fb0b5b
Enrique García Méndez <44614728+egarciamendez@users.noreply.github.com>
2024-10-16 17:09:05 +0200

    kiss

--- docs/source/examples/rectangular_reinforced_cross_section.py
+++ docs/source/examples/rectangular_reinforced_cross_section.py
@@ -1,13 +1,9 @@
 """Reinforced concrete cross-section example."""
 
-from shapely import LineString
-
 from blueprints.materials.concrete import ConcreteMaterial, ConcreteStrengthClass
 from blueprints.materials.reinforcement_steel import ReinforcementSteelMaterial, ReinforcementSteelQuality
 from blueprints.structural_sections.concrete.covers import CoversRectangular
-from blueprints.structural_sections.concrete.rebar import Rebar
 from blueprints.structural_sections.concrete.reinforced_concrete_sections.rectangular import RectangularReinforcedCrossSection
-from blueprints.structural_sections.concrete.reinforced_concrete_sections.reinforcement_configurations import ReinforcementByQuantity
 
 # Define a concrete material
 concrete = ConcreteMaterial(concrete_class=ConcreteStrengthClass.C35_45)
@@ -37,21 +33,6 @@ cs.add_longitudinal_reinforcement_by_quantity(
     material=steel,
 )
 
-# add a second layer of reinforcement to the cross-section
-cs.add_longitudinal_reinforcement_by_quantity(
-    n=3,
-    diameter=16,
-    edge="upper",
-    material=steel,
-    cover=100,
-)
-
-# add a free reinforcement configuration to the cross-section
-cs.add_reinforcement_configuration(
-    line=LineString([(50, -50), (200, 200)]),
-    configuration=ReinforcementByQuantity(diameter=20, n=4, material=steel),
-)
-
 # Add stirrups to the cross-section
 cs.add_stirrup_along_edges(
     diameter=8,
@@ -63,16 +44,6 @@ cs.add_stirrup_along_edges(
     diameter=12,
     distance=300,
     material=steel,
-)
-
-# Add a longitudinal rebar to the cross-section
-cs.add_longitudinal_rebar(
-    rebar=Rebar(
-        diameter=12,
-        x=-250,
-        y=-100,
-        material=steel,
-    )
 )
 
 cs.plot(show=True)
