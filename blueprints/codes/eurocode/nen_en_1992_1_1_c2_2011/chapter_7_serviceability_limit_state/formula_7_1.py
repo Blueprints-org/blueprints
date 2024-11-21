a0b8160b9636c0dac052d28d1938ca2a2f0f5e42
Jesse Vermeulen <jesse.vermeulen@anteagroup.nl>
2024-11-21 10:19:19 +0100

    Changed back 2005 to 2011

--- blueprints/codes/eurocode/nen_en_1992_1_1_c2_2011/chapter_7_serviceability_limit_state/formula_7_1.py
+++ blueprints/codes/eurocode/nen_en_1992_1_1_c2_2011/chapter_7_serviceability_limit_state/formula_7_1.py
@@ -1,6 +1,6 @@
-"""Formula 7.1 from NEN-EN 1992-1-1+C2:2005: Chapter 7 - Serviceability limit state (SLS)."""
+"""Formula 7.3 from NEN-EN 1992-1-1+C2:2011: Chapter 7 - Serviceability limit state (SLS)."""
 
-from blueprints.codes.eurocode.nen_en_1992_1_1_c2_2005 import NEN_EN_1992_1_1_C2_2005
+from blueprints.codes.eurocode.nen_en_1992_1_1_c2_2011 import NEN_EN_1992_1_1_C2_2011
 from blueprints.codes.formula import Formula
 from blueprints.codes.latex_formula import LatexFormula
 from blueprints.type_alias import KN, MM2, MPA
@@ -11,7 +11,7 @@ class Form7Dot3CoefficientKc(Formula):
     """Class representing the formula 7.3 for the coefficient kc for flanges of tubular cross-sections and T-sections."""
 
     label = "7.3"
-    source_document = NEN_EN_1992_1_1_C2_2005
+    source_document = NEN_EN_1992_1_1_C2_2011
 
     def __init__(
         self,
