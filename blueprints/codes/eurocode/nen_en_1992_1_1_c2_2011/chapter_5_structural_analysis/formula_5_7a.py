8194cd09e6b07ae8e06a5113137a7c236ee90c52
Enrique García Méndez <44614728+egarciamendez@users.noreply.github.com>
2024-11-23 16:04:54 +0100

    restore 5.7a and 5.7b

---
+++ blueprints/codes/eurocode/nen_en_1992_1_1_c2_2011/chapter_5_structural_analysis/formula_5_7a.py
@@ -0,0 +1,17 @@
+"""Formula 5.7a from NEN-EN 1992-1-1+C2:2011: Chapter 5 - Structural Analysis."""
+
+from blueprints.codes.eurocode.nen_en_1992_1_1_c2_2011 import NEN_EN_1992_1_1_C2_2011
+from blueprints.codes.eurocode.nen_en_1992_1_1_c2_2011.chapter_5_structural_analysis.formula_5_7ab import Form5Dot7abFlangeEffectiveFlangeWidth
+
+
+class Form5Dot7aFlangeEffectiveFlangeWidth(Form5Dot7abFlangeEffectiveFlangeWidth):
+    """Class representing formula 5.7a for the calculation of effective flange width of the i-th flange [:math:`b_{eff,i}`].
+    See Figure 5.3.
+
+    Notes
+    -----
+    This formula uses the combination of formula 5.7a and 5.7b. Formula 5.7a and 5.7b cannot be used independently.
+    """
+
+    label = "5.7a"
+    source_document = NEN_EN_1992_1_1_C2_2011
