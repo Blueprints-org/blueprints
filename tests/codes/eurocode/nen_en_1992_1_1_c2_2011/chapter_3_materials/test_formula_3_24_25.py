238fc2b1b95af86d0c10de1e51640ecdd4ff64b2
GerjanDorgelo <gerjan.dorgelo@anteagroup.nl>
2024-12-24 09:23:41 +0100

    Update minimum code coverage workflow and comment out test cases for formula 3.24 and 3.25 to see if the former worked

--- tests/codes/eurocode/nen_en_1992_1_1_c2_2011/chapter_3_materials/test_formula_3_24_25.py
+++ tests/codes/eurocode/nen_en_1992_1_1_c2_2011/chapter_3_materials/test_formula_3_24_25.py
@@ -11,19 +11,6 @@ from blueprints.validations import NegativeValueError
 class TestForm3Dot24And25IncreasedCharacteristicCompressiveStrength:
     """Validation for formula 3.24 and 3.25 from NEN-EN 1992-1-1+C2:2011."""
 
-    def test_evaluation_small_sigma_2(self) -> None:
-        """Test the evaluation of the result."""
-        # Example values
-        f_ck = 12.2  # MPa
-        sigma_2 = 0.03 * f_ck  # MPa
-
-        form_3_24 = Form3Dot24And25IncreasedCharacteristicCompressiveStrength(f_ck=f_ck, sigma_2=sigma_2)
-
-        # Expected result, manually calculated
-        manually_calculated_result = 14.03
-
-        assert form_3_24 == pytest.approx(expected=manually_calculated_result, rel=1e-4)
-
     def test_raise_error_when_negative_f_ck_is_given(self) -> None:
         """Test a negative value."""
         # Example values
