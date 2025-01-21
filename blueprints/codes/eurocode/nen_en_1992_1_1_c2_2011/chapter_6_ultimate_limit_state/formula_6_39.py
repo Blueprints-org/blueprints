2afd21cb395570c07766231e9a4e4de958a66b31
GerjanDorgelo <gerjan.dorgelo@anteagroup.nl>
2025-01-21 19:44:21 +0100

    Update formula 6.39 to use MM2 for shear distribution parameter

--- blueprints/codes/eurocode/nen_en_1992_1_1_c2_2011/chapter_6_ultimate_limit_state/formula_6_39.py
+++ blueprints/codes/eurocode/nen_en_1992_1_1_c2_2011/chapter_6_ultimate_limit_state/formula_6_39.py
@@ -3,7 +3,7 @@
 from blueprints.codes.eurocode.nen_en_1992_1_1_c2_2011 import NEN_EN_1992_1_1_C2_2011
 from blueprints.codes.formula import Formula
 from blueprints.codes.latex_formula import LatexFormula
-from blueprints.type_alias import DIMENSIONLESS, MM, NMM, N
+from blueprints.type_alias import DIMENSIONLESS, MM, MM2, NMM, N
 from blueprints.validations import raise_if_less_or_equal_to_zero, raise_if_negative
 
 
@@ -19,7 +19,7 @@ class Form6Dot39BetaCoefficient(Formula):
         m_ed: NMM,
         v_ed: N,
         u_1: MM,
-        w_1: MM,
+        w_1: MM2,
     ) -> None:
         r"""[:math:`\beta`] Beta coefficient [-].
 
@@ -27,16 +27,16 @@ class Form6Dot39BetaCoefficient(Formula):
 
         Parameters
         ----------
-        k : float
+        k : DIMENSIONLESS
             [:math:`k`] Coefficient dependent on the ratio between the column dimensions c1 and c2 [-].
-        m_ed : float
+        m_ed : NMM
             [:math:`M_{Ed}`] Design value of the applied moment [:math:`Nmm`].
-        v_ed : float
+        v_ed : N
             [:math:`V_{Ed}`] Design value of the applied shear force [:math:`N`].
-        u_1 : float
+        u_1 : MM
             [:math:`u_1`] Length of the basic control perimeter [:math:`mm`].
-        w_1 : float
-            [:math:`W_1`] Distribution of shear as illustrated in Figure 6.19 [:math:`mm`].
+        w_1 : MM2
+            [:math:`W_1`] Distribution of shear as illustrated in Figure 6.19 [:math:`mm^2`].
         """
         super().__init__()
         self.k = k
