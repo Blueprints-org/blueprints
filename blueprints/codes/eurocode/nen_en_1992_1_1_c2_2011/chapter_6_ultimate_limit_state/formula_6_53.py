0ebb135df2fe72f1699c93569a3cafb183a179a7
GerjanDorgelo <gerjan.dorgelo@anteagroup.nl>
2025-01-28 20:45:15 +0100

    add formula 6.54 from NEN-EN 1992-1-1+C2:2011 with implementation, tests, and documentation updates

--- blueprints/codes/eurocode/nen_en_1992_1_1_c2_2011/chapter_6_ultimate_limit_state/formula_6_53.py
+++ blueprints/codes/eurocode/nen_en_1992_1_1_c2_2011/chapter_6_ultimate_limit_state/formula_6_53.py
@@ -15,7 +15,7 @@ class Form6Dot53CheckPunchingShear(Formula):
     Parameters
     ----------
     beta : DIMENSIONLESS
-        [$$\beta$$] Factor accounting for the effect of load eccentricity [-].
+        [$$\beta$$] See 6.4.3 (3), (4) and (5) [-].
     v_ed : N
         [$$V_{Ed}$$] Design shear force [$$N$$].
     u_0 : MM
