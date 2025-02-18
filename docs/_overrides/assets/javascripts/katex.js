eea603a9b77c16293a314e75d588e5eb94a3827e
Wichard Bron <wichard.bron@wsp.com>
2025-02-18 08:58:20 +0100

    added comment

--- docs/_overrides/assets/javascripts/katex.js
+++ docs/_overrides/assets/javascripts/katex.js
@@ -1,3 +1,4 @@
+// this javascript file has to be added according to the mkdocs materials documentation: https://squidfunk.github.io/mkdocs-material/reference/math/#katex
 document$.subscribe(({ body }) => { 
     renderMathInElement(body, {
       delimiters: [
