aab232e5693f8eab465b7f26a485161431d65dfa
Wichard Bron <wichard.bron@wsp.com>
2025-03-06 09:52:35 +0100

    improve generate home_index

--- docs/_scripts/generate_index.py
+++ docs/_scripts/generate_index.py
@@ -8,15 +8,21 @@ import mkdocs_gen_files
 root = Path(__file__).parents[2]
 readme_file = root / "README.md"
 
-with open(readme_file, encoding="utf-8") as fd:
-    readme_content = fd.read()
+
+def replace_links(readme_content: str) -> str:
+    """Replace the relative links to the overview page by removing the .md extension."""
     readme_content = readme_content.replace("docs/", "")
     # replace the relative links to the overview page by removing the .md extension
     readme_content = re.sub(r"/\w+\.md", lambda match: match.group(0)[:-3], readme_content)
     # replace blueprints/code references by links to the API reference
     readme_content = readme_content.replace('"blueprints/codes', '"API reference/codes')
     # add 'https://github.com/Blueprints-org/blueprints' prefix to file in [link](file) if file is not a URL
-    readme_content = re.sub(r"\[([^\]]+)\]\((?!http)([^)]+)\)", r"[\1](https://github.com/Blueprints-org/blueprints/blob/main/\2)", readme_content)
+    return re.sub(r"\[([^\]]+)\]\((?!http)([^)]+)\)", r"[\1](https://github.com/Blueprints-org/blueprints/blob/main/\2)", readme_content)
+
+
+with open(readme_file, encoding="utf-8") as fd:
+    readme_content = fd.read()
+    readme_content = replace_links(readme_content)
     # remove the read the docs chapter, we're already in the docs
     readme_content = re.sub(r"## Read the docs!\n\n.*\n## Quick", "## Quick", readme_content, flags=re.DOTALL)
 
