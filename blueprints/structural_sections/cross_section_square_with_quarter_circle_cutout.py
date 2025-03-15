ef9e947132e12a5ff30c80a088059e50a95d4cc2
GerjanDorgelo <gerjan.dorgelo@anteagroup.nl>
2025-03-15 20:21:39 +0100

    feat: Enhance SquareWithQuarterCircleCutoutCrossSection with height and width properties, and update moment of inertia calculations

--- blueprints/structural_sections/cross_section_square_with_quarter_circle_cutout.py
+++ blueprints/structural_sections/cross_section_square_with_quarter_circle_cutout.py
@@ -5,6 +5,7 @@ from dataclasses import dataclass
 
 from shapely.geometry import Point, Polygon
 
+from blueprints.structural_sections.cross_section_rectangle import RectangularCrossSection
 from blueprints.type_alias import MM, MM2, MM3, MM4
 
 
@@ -12,24 +13,55 @@ from blueprints.type_alias import MM, MM2, MM3, MM4
 class SquareWithQuarterCircleCutoutCrossSection:
     """
     Class to represent a square cross-section with a quarter circle cutout for geometric calculations.
-
-    Note: No clean formula for the moment of inertia, elastic section modulus, or plastic section modulus.
-    These values are typically so small that they don't contribute significantly to the overall calculations.
-    Therefore, these values are set to 0.
+    Warning: The moment of inertia and elastic section modulus are approximations. Please use with caution.
 
     Parameters
     ----------
     a : MM
         The side length of the square cross-section.
+    x : MM
+        The x-coordinate of the 90 degree corner of the shape.
+    y : MM
+        The y-coordinate of the 90 degree corner of the shape.
     """
 
     a: MM
+    x: MM = 0
+    y: MM = 0
 
     def __post_init__(self) -> None:
         """Post-initialization to validate the diameter."""
         if self.a <= 0:
             msg = f"a must be a positive value, but got {self.a}"
             raise ValueError(msg)
+
+    @property
+    def height(self) -> MM:
+        """
+        Calculate the height of the partial ring cross-section [mm].
+
+        Returns
+        -------
+        MM
+            The height of the partial ring.
+        """
+        min_y = min(point.y for point in self.vertices)
+        max_y = max(point.y for point in self.vertices)
+        return max_y - min_y
+
+    @property
+    def width(self) -> MM:
+        """
+        Calculate the width of the partial ring cross-section [mm].
+
+        Returns
+        -------
+        MM
+            The width of the partial ring.
+        """
+        min_x = min(point.x for point in self.vertices)
+        max_x = max(point.x for point in self.vertices)
+        return max_x - min_x
 
     @property
     def geometry(self) -> Polygon:
@@ -96,109 +128,124 @@ class SquareWithQuarterCircleCutoutCrossSection:
     def moment_of_inertia_about_y(self) -> MM4:
         """
         Moments of inertia of the cross-section about the y-axis [mm⁴].
+        Note: No closed form equation was found, therefore this approximation is used.
 
         Returns
         -------
         MM4
             The moment of inertia about the y-axis.
         """
-        return 0
+        representative_width = self.area / self.height
+        return RectangularCrossSection(width=representative_width, height=self.height, x=self.x, y=self.y).moment_of_inertia_about_y
 
     @property
     def moment_of_inertia_about_z(self) -> MM4:
         """
         Moments of inertia of the cross-section about the z-axis [mm⁴].
+        Note: No closed form equation was found, therefore this approximation is used.
 
         Returns
         -------
         MM4
             The moment of inertia about the z-axis.
         """
-        return 0
+        representative_height = self.area / self.width
+        return RectangularCrossSection(width=self.width, height=representative_height, x=self.x, y=self.y).moment_of_inertia_about_z
 
     @property
     def polar_moment_of_inertia(self) -> MM4:
         """
-        Polar moments of inertia of the cross-section [mm⁴].
+        Polar moment of inertia of the cross-section [mm⁴].
+        Note: No closed form equation was found, therefore this approximation is used.
 
         Returns
         -------
         MM4
             The polar moment of inertia.
         """
-        return 0
+        return self.moment_of_inertia_about_y + self.moment_of_inertia_about_z
 
     @property
     def elastic_section_modulus_about_y_positive(self) -> MM3:
         """
         Elastic section modulus about the y-axis on the positive z side [mm³].
+        Note: No closed form equation was found, therefore this approximation is used.
 
         Returns
         -------
         MM3
             The elastic section modulus about the y-axis.
         """
-        return 0
+        distance_to_top = max(point.y for point in self.vertices) - self.centroid.y
+        return self.moment_of_inertia_about_y / distance_to_top
 
     @property
     def elastic_section_modulus_about_y_negative(self) -> MM3:
         """
         Elastic section modulus about the y-axis on the negative z side [mm³].
+        Note: No closed form equation was found, therefore this approximation is used.
 
         Returns
         -------
         MM3
             The elastic section modulus about the y-axis.
         """
-        return 0
+        distance_to_bottom = self.centroid.y - min(point.y for point in self.vertices)
+        return self.moment_of_inertia_about_y / distance_to_bottom
 
     @property
     def elastic_section_modulus_about_z_positive(self) -> MM3:
         """
         Elastic section modulus about the z-axis on the positive y side [mm³].
+        Note: No closed form equation was found, therefore this approximation is used.
 
         Returns
         -------
         MM3
             The elastic section modulus about the z-axis.
         """
-        return 0
+        distance_to_right = max(point.x for point in self.vertices) - self.centroid.x
+        return self.moment_of_inertia_about_z / distance_to_right
 
     @property
     def elastic_section_modulus_about_z_negative(self) -> MM3:
         """
         Elastic section modulus about the z-axis on the negative y side [mm³].
+        Note: No closed form equation was found, therefore this approximation is used.
 
         Returns
         -------
         MM3
             The elastic section modulus about the z-axis.
         """
-        return 0
+        distance_to_left = self.centroid.x - min(point.x for point in self.vertices)
+        return self.moment_of_inertia_about_z / distance_to_left
 
     @property
     def plastic_section_modulus_about_y(self) -> MM3:
         """
         Plastic section modulus about the y-axis [mm³].
+        Note: No closed form equation was found, therefore this approximation is used.
 
         Returns
         -------
         MM3
             The plastic section modulus about the y-axis.
         """
-        return 0
+        return max(self.elastic_section_modulus_about_y_positive, self.elastic_section_modulus_about_y_negative)
 
     @property
     def plastic_section_modulus_about_z(self) -> MM3:
         """
         Plastic section modulus about the z-axis [mm³].
+        Note: No closed form equation was found, therefore this approximation is used.
 
         Returns
         -------
         MM3
             The plastic section modulus about the z-axis.
         """
-        return 0
+        return max(self.elastic_section_modulus_about_z_positive, self.elastic_section_modulus_about_z_negative)
 
     @property
     def vertices(self) -> list[Point]:
