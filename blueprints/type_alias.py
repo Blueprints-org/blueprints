efa1b065b655f8de4b3b9cef4e334f677c7945a6
GerjanDorgelo <gerjan.dorgelo@anteagroup.nl>
2025-03-19 19:07:59 +0100

    feat: Add cubic millimeters per meter type alias

--- blueprints/type_alias.py
+++ blueprints/type_alias.py
@@ -85,6 +85,8 @@ CM3 = float
 """Cubic centimeters (cm³), represented as a float."""
 M3 = float
 """Cubic meters (m³), represented as a float."""
+MM3_M = float
+"""Cubic millimeters per meter (mm³/m), represented as a float."""
 M3_M = float
 """Cubic meters per meter (m³/m), represented as a float."""
 # </editor-fold>
