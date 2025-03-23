0c94fe469ea5a0b578fe09509dc0619c97c97922
GerjanDorgelo <gerjan.dorgelo@anteagroup.nl>
2025-03-23 17:32:04 +0100

    feat: Implement general steel plotter for visualizing cross-section shapes

---
+++ blueprints/structural_sections/steel/steel_cross_sections/plotters/general_steel_plotter.py
@@ -0,0 +1,76 @@
+"""Defines a general steel plotter for cross sections and its characteristics."""
+# ruff: noqa: PLR0913, F821
+
+import matplotlib.pyplot as plt
+from matplotlib.patches import Polygon as MplPolygon
+from shapely.geometry import Polygon
+
+from blueprints.materials.steel import SteelStrengthClass
+from blueprints.structural_sections.steel.steel_element import SteelElement
+
+# Define color
+STEEL_COLOR = (0.683, 0.0, 0.0)
+
+
+def plot_shapes(
+    *elements: SteelElement,
+    figsize: tuple[float, float] = (15.0, 8.0),
+    title: str = "Cross Section",
+    font_size_title: float = 18.0,
+    show: bool = False,
+) -> plt.Figure:
+    """
+    Plot the given shapes.
+
+    Parameters
+    ----------
+    elements : SteelElement
+        The steel elements to plot.
+    figsize : tuple[float, float], optional
+        The size of the figure in inches. Default is (15.0, 8.0).
+    title : str, optional
+        The title of the plot. Default is "Cross Section".
+    font_size_title : float, optional
+        The font size of the title. Default is 18.0.
+    show : bool, optional
+        Whether to show the plot. Default is False.
+    """
+    fig, ax = plt.subplots(figsize=figsize)
+
+    for element in elements:
+        if not isinstance(element.geometry, Polygon):
+            raise TypeError(f"All shapes must be Shapely polygons, but got: {type(element.geometry)}")
+
+        # Plot the exterior polygon
+        x, y = element.geometry.exterior.xy
+        patch = MplPolygon(xy=list(zip(x, y)), lw=1, fill=True, facecolor=STEEL_COLOR, edgecolor=STEEL_COLOR)
+        ax.add_patch(patch)
+
+        # Plot the interior polygons (holes) if any
+        for interior in element.geometry.interiors:
+            x, y = interior.xy
+            patch = MplPolygon(xy=list(zip(x, y)), lw=0, fill=True, facecolor="white")
+            ax.add_patch(patch)
+
+    ax.set_xlabel("X")
+    ax.set_ylabel("Y")
+    ax.set_title(title, fontsize=font_size_title)
+    ax.grid(True)
+    ax.axis("equal")
+    ax.axis("off")
+
+    if show:
+        plt.show()
+
+    assert fig is not None
+    return fig
+
+
+# Example usage
+if __name__ == "__main__":
+    from blueprints.structural_sections.steel.steel_cross_sections.chs_profile import CHSSteelProfile
+
+    # Define a sample CHS profile
+    steel_class = SteelStrengthClass.EN_10025_2_S355
+    profile = CHSSteelProfile(outer_diameter=100, wall_thickness=10, steel_class=steel_class)
+    profile.plot(show=True)
