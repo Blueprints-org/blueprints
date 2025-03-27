982d63ef5176de0424e072ba8ae9a0bb06ca27a1
GerjanDorgelo <gerjan.dorgelo@anteagroup.nl>
2025-03-27 19:17:56 +0100

    feat: Add tests for moments of inertia and section moduli in RectangularCrossSection

--- tests/structural_sections/test_cross_section_rectangle.py
+++ tests/structural_sections/test_cross_section_rectangle.py
@@ -34,3 +34,22 @@ class TestRectangularCrossSection:
         assert len(vertices) == 5
         assert (first_vertex.x, first_vertex.y) == pytest.approx(expected=(-50.0, -100.00), rel=1e-6)
         assert (last_vertex.x, last_vertex.y) == pytest.approx(expected=(-50.0, -100.00), rel=1e-6)
+
+    def test_moments_of_inertia(self, rectangular_cross_section: RectangularCrossSection) -> None:
+        """Test the moments of inertia properties of the RectangularCrossSection class."""
+        assert rectangular_cross_section.moment_of_inertia_about_y == pytest.approx(expected=1 / 12 * 100 * 200**3, rel=1e-6)
+        assert rectangular_cross_section.moment_of_inertia_about_z == pytest.approx(expected=1 / 12 * 200 * 100**3, rel=1e-6)
+
+    def test_section_moduli(self, rectangular_cross_section: RectangularCrossSection) -> None:
+        """Test the section moduli properties of the RectangularCrossSection class."""
+        assert rectangular_cross_section.elastic_section_modulus_about_y_positive == pytest.approx(expected=1 / 6 * 100 * 200**2, rel=1e-6)
+        assert rectangular_cross_section.elastic_section_modulus_about_z_positive == pytest.approx(expected=1 / 6 * 200 * 100**2, rel=1e-6)
+
+    def test_polar_moment_of_inertia(self, rectangular_cross_section: RectangularCrossSection) -> None:
+        """Test the polar moment of inertia property of the RectangularCrossSection class."""
+        assert rectangular_cross_section.polar_moment_of_inertia == pytest.approx(expected=1 / 12 * 100 * 200**3 + 1 / 12 * 200 * 100**3, rel=1e-6)
+
+    def test_plastic_section_moduli(self, rectangular_cross_section: RectangularCrossSection) -> None:
+        """Test the plastic section moduli properties of the RectangularCrossSection class."""
+        assert rectangular_cross_section.plastic_section_modulus_about_y == pytest.approx(expected=1 / 4 * 100 * 200**2, rel=1e-6)
+        assert rectangular_cross_section.plastic_section_modulus_about_z == pytest.approx(expected=1 / 4 * 200 * 100**2, rel=1e-6)
