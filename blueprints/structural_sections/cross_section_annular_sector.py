fix: Update centroid calculation in AnnularSectorCrossSection for accuracy

blueprints/structural_sections/cross_section_annular_sector.py
@@ -27,9 +27,9 @@ class AnnularSectorCrossSection:
     end_angle : float
         The end angle of the annular sector in degrees (must be larger than start angle but not more than 360 degrees more).
     x : MM
-        The x-coordinate of the annular sector's center.
+        The x-coordinate of the annular sector's radius center.
     y : MM
-        The y-coordinate of the annular sector's center.
+        The y-coordinate of the annular sector's radius center.
     name : str
         The name of the rectangular cross-section, default is "Annular Sector".
     """
@@ -167,9 +167,9 @@ class AnnularSectorCrossSection:
         Point
             The centroid of the annular sector.
         """
-        angle_radians = math.radians(self.end_angle - self.start_angle)
+        halve_angle_radians = math.radians(self.end_angle - self.start_angle) / 2
         centroid_radius = (
-            (2 * np.sin(angle_radians) / 3 / angle_radians)
+            (2 * np.sin(halve_angle_radians) / 3 / halve_angle_radians)
             * (self.outer_radius**3 - self.inner_radius**3)
             / (self.outer_radius**2 - self.inner_radius**2)
         )

tests/structural_sections/test_cross_section_annular_sector.py
@@ -48,8 +48,8 @@ class TestAnnularSectorCrossSection:
     def test_centroid(self, annular_sector_cross_section: AnnularSectorCrossSection) -> None:
         """Test the centroid property of the AnnularSectorCrossSection class."""
         centroid = annular_sector_cross_section.centroid
-        angle_radians = np.radians(annular_sector_cross_section.end_angle - annular_sector_cross_section.start_angle)
-        centroid_radius = (2 * np.sin(angle_radians) / (3 * angle_radians)) * (
+        halve_angle_radians = np.radians(annular_sector_cross_section.end_angle - annular_sector_cross_section.start_angle) / 2
+        centroid_radius = (2 * np.sin(halve_angle_radians) / (3 * halve_angle_radians)) * (
             (annular_sector_cross_section.outer_radius**3 - annular_sector_cross_section.inner_radius**3)
             / (annular_sector_cross_section.outer_radius**2 - annular_sector_cross_section.inner_radius**2)
         )