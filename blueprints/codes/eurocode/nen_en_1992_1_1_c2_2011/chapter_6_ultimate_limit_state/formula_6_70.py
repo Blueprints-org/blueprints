30e21ecde2079f8f286eef38c3b84845ae9b3632
Gerjan Dorgelo <gerjan.dorgelo@anteagroup.nl>
2025-04-09 10:51:04 +0200

    Refactor import statement for consistency in formula 6.70

--- blueprints/codes/eurocode/nen_en_1992_1_1_c2_2011/chapter_6_ultimate_limit_state/formula_6_70.py
+++ blueprints/codes/eurocode/nen_en_1992_1_1_c2_2011/chapter_6_ultimate_limit_state/formula_6_70.py
@@ -1,11 +1,12 @@
 """Formula 6.70 from NEN-EN 1992-1-1+C2:2011: Chapter 6 - Ultimate limit state."""
 
+from itertools import zip_longest
+
 from blueprints.codes.eurocode.nen_en_1992_1_1_c2_2011 import NEN_EN_1992_1_1_C2_2011
 from blueprints.codes.formula import Formula
 from blueprints.codes.latex_formula import LatexFormula
 from blueprints.type_alias import MPA
 from blueprints.validations import raise_if_less_or_equal_to_zero, raise_if_lists_differ_in_length, raise_if_negative
-from itertools import zip_longest
 
 
 class Form6Dot70FatigueDamageFactor(Formula):
