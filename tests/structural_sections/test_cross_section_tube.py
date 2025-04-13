fix: correct perimeter calculation in TubeCrossSection class and enhance related tests

tests/structural_sections/test_cross_section_tube.py
@@ -16,7 +16,7 @@ class TestTubeCrossSection:
 
     def test_perimeter(self, tube_cross_section: TubeCrossSection) -> None:
         """Test the perimeter property of the TubeCrossSection class."""
-        expected_perimeter = 2 * np.pi * (50.0 + 25.0)
+        expected_perimeter = 2 * np.pi * 50.0
         assert tube_cross_section.perimeter == pytest.approx(expected=expected_perimeter, rel=1e-6)
 
     def test_wall_thickness(self, tube_cross_section: TubeCrossSection) -> None:
@@ -79,3 +79,14 @@ class TestTubeCrossSection:
         """Test the section properties of the TubeCrossSection class."""
         section_properties = tube_cross_section.section_properties()
         assert section_properties.mass == pytest.approx(expected=tube_cross_section.area, rel=1e-2)
+        assert section_properties.perimeter == pytest.approx(expected=tube_cross_section.perimeter, rel=1e-2)
+        assert section_properties.cx == pytest.approx(expected=tube_cross_section.centroid.x, rel=1e-2)
+        assert section_properties.cy == pytest.approx(expected=tube_cross_section.centroid.y, rel=1e-2)
+        assert section_properties.ixx_c == pytest.approx(expected=tube_cross_section.moment_of_inertia_about_y, rel=1e-2)
+        assert section_properties.iyy_c == pytest.approx(expected=tube_cross_section.moment_of_inertia_about_z, rel=1e-2)
+        assert section_properties.zxx_plus == pytest.approx(expected=tube_cross_section.elastic_section_modulus_about_y_positive, rel=1e-2)
+        assert section_properties.zyy_plus == pytest.approx(expected=tube_cross_section.elastic_section_modulus_about_z_positive, rel=1e-2)
+        assert section_properties.zxx_minus == pytest.approx(expected=tube_cross_section.elastic_section_modulus_about_y_negative, rel=1e-2)
+        assert section_properties.zyy_minus == pytest.approx(expected=tube_cross_section.elastic_section_modulus_about_z_negative, rel=1e-2)
+        assert section_properties.sxx == pytest.approx(expected=tube_cross_section.plastic_section_modulus_about_y, rel=1e-2)
+        assert section_properties.syy == pytest.approx(expected=tube_cross_section.plastic_section_modulus_about_z, rel=1e-2)