6fd18a69139f1f1485bdca3172a94a2a31831de7
GerjanDorgelo <gerjan.dorgelo@anteagroup.nl>
2025-04-21 13:23:47 +0200

    Add StripSteelProfile class and corresponding tests for steel strip profile functionality

---
+++ blueprints/structural_sections/steel/steel_cross_sections/strip_profile.py
@@ -0,0 +1,103 @@
+"""Steel Strip Profile."""
+
+from matplotlib import pyplot as plt
+
+from blueprints.codes.eurocode.nen_en_1993_1_1_c2_a1_2016.chapter_3_materials.table_3_1 import SteelStrengthClass
+from blueprints.materials.steel import SteelMaterial
+from blueprints.structural_sections.cross_section_rectangle import RectangularCrossSection
+from blueprints.structural_sections.steel.steel_cross_sections.base import SteelCrossSection
+from blueprints.structural_sections.steel.steel_cross_sections.plotters.general_steel_plotter import plot_shapes
+from blueprints.structural_sections.steel.steel_cross_sections.standard_profiles.strip import Strip
+from blueprints.structural_sections.steel.steel_element import SteelElement
+from blueprints.type_alias import MM
+
+
+class StripSteelProfile(SteelCrossSection):
+    """Representation of a Steel Strip profile.
+
+    Parameters
+    ----------
+    width : MM
+        The width of the strip profile [mm].
+    height : MM
+        The height (thickness) of the strip profile [mm].
+    steel_class : SteelStrengthClass
+        The steel strength class of the profile.
+    """
+
+    def __init__(
+        self,
+        width: MM,
+        height: MM,
+        steel_class: SteelStrengthClass,
+    ) -> None:
+        """Initialize the Steel Strip profile."""
+        self.width = width
+        self.height = height
+
+        self.strip = RectangularCrossSection(
+            name="Steel Strip",
+            width=self.width,
+            height=self.height,
+            x=0,
+            y=0,
+        )
+
+        self.steel_material = SteelMaterial(steel_class=steel_class)
+        self.elements = [SteelElement(cross_section=self.strip, material=self.steel_material)]
+
+    def plot(self, *args, **kwargs) -> plt.Figure:
+        """Plot the cross-section. Making use of the standard plotter.
+
+        Parameters
+        ----------
+        *args
+            Additional arguments passed to the plotter.
+        **kwargs
+            Additional keyword arguments passed to the plotter.
+        """
+        return plot_shapes(
+            self,
+            *args,
+            **kwargs,
+        )
+
+
+class LoadStandardStrip:
+    r"""Class to load in values for standard Strip profile.
+
+    Parameters
+    ----------
+    steel_class: SteelStrengthClass
+        Enumeration of steel strength classes (default: S355)
+    profile: Strip
+        Enumeration of standard steel strip profiles (default: STRIP160x5)
+    """
+
+    def __init__(
+        self,
+        steel_class: SteelStrengthClass = SteelStrengthClass.S355,
+        profile: Strip = Strip.STRIP160x5,
+    ) -> None:
+        self.steel_class = steel_class
+        self.profile = profile
+
+    def __str__(self) -> str:
+        """Return the steel class and profile."""
+        return f"Steel class: {self.steel_class}, Profile: {self.profile}"
+
+    def alias(self) -> str:
+        """Return the code of the strip profile."""
+        return self.profile.alias
+
+    def width(self) -> MM:
+        """Return the width of the strip profile."""
+        return self.profile.width
+
+    def height(self) -> MM:
+        """Return the height (thickness) of the strip profile."""
+        return self.profile.height
+
+    def get_profile(self) -> StripSteelProfile:
+        """Return the strip profile."""
+        return StripSteelProfile(width=self.width(), height=self.height(), steel_class=self.steel_class)
