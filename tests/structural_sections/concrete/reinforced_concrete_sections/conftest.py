d23d148b8a11cf7f31d77358890d9ec8652fe51a
Enrique García <enriquegarcia@live.nl>
2025-04-21 17:05:15 +0200

    Add pytest fixture for RectangularReinforcedCrossSection and remove redundant code

--- tests/structural_sections/concrete/reinforced_concrete_sections/conftest.py
+++ tests/structural_sections/concrete/reinforced_concrete_sections/conftest.py
@@ -4,8 +4,68 @@ import pytest
 
 from blueprints.materials.concrete import ConcreteMaterial, ConcreteStrengthClass
 from blueprints.materials.reinforcement_steel import ReinforcementSteelMaterial, ReinforcementSteelQuality
+from blueprints.structural_sections.concrete.covers import CoversRectangular
 from blueprints.structural_sections.concrete.rebar import Rebar
 from blueprints.structural_sections.concrete.reinforced_concrete_sections.circular import CircularReinforcedCrossSection
+from blueprints.structural_sections.concrete.reinforced_concrete_sections.rectangular import RectangularReinforcedCrossSection
+
+
+@pytest.fixture
+def rectangular_reinforced_cross_section() -> RectangularReinforcedCrossSection:
+    """Return a rectangular reinforced cross-section."""
+    # Define a concrete material
+    concrete = ConcreteMaterial(concrete_class=ConcreteStrengthClass.C35_45)
+
+    # Define a reinforcement steel material
+    steel = ReinforcementSteelMaterial(steel_quality=ReinforcementSteelQuality.B500B)
+
+    # Define a rectangular reinforced cross-section
+    cs = RectangularReinforcedCrossSection(
+        name="Rectangular",
+        width=1000,
+        height=800,
+        covers=CoversRectangular(upper=45, right=30, lower=35, left=50),
+        concrete_material=concrete,
+    )
+
+    # Add reinforcement to the cross-section
+    cs.add_longitudinal_reinforcement_by_quantity(
+        n=5,
+        diameter=14,
+        edge="upper",
+        material=steel,
+    )
+    cs.add_longitudinal_reinforcement_by_quantity(
+        n=4,
+        diameter=40,
+        edge="lower",
+        material=steel,
+    )
+
+    # Add stirrups to the cross-section
+    cs.add_stirrup_along_edges(
+        diameter=8,
+        distance=150,
+        material=steel,
+    )
+    # Add stirrups to the cross-section
+    cs.add_stirrup_along_edges(
+        diameter=12,
+        distance=300,
+        material=steel,
+    )
+
+    # Add a longitudinal rebar to the cross-section
+    cs.add_longitudinal_rebar(
+        rebar=Rebar(
+            diameter=12,
+            x=-250,
+            y=-100,
+            material=steel,
+        )
+    )
+
+    return cs
 
 
 @pytest.fixture
