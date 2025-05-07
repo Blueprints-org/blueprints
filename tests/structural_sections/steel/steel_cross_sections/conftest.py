7655596fe344db3fbac28a2e984f9ce18de52465
Enrique García <enriquegarcia@live.nl>
2025-05-07 15:18:41 +0200

    Refactor steel profile fixtures to use `from_standard_profile`.
    Replaced `LoadStandard*` classes with direct calls to `from_standard_profile` on steel profile classes, improving clarity and consistency. Updated imports to reflect the changes and streamlined the use of `SteelMaterial` for profile creation.

--- tests/structural_sections/steel/steel_cross_sections/conftest.py
+++ tests/structural_sections/steel/steel_cross_sections/conftest.py
@@ -3,12 +3,13 @@
 import pytest
 
 from blueprints.codes.eurocode.nen_en_1993_1_1_c2_a1_2016.chapter_3_materials.table_3_1 import SteelStrengthClass
-from blueprints.structural_sections.steel.steel_cross_sections.chs_profile import CHSSteelProfile, LoadStandardCHS
-from blueprints.structural_sections.steel.steel_cross_sections.i_profile import ISteelProfile, LoadStandardIProfile
+from blueprints.materials.steel import SteelMaterial
+from blueprints.structural_sections.steel.steel_cross_sections.chs_profile import CHSSteelProfile
+from blueprints.structural_sections.steel.steel_cross_sections.i_profile import ISteelProfile
 from blueprints.structural_sections.steel.steel_cross_sections.standard_profiles.chs import CHS
 from blueprints.structural_sections.steel.steel_cross_sections.standard_profiles.heb import HEB
 from blueprints.structural_sections.steel.steel_cross_sections.standard_profiles.strip import Strip
-from blueprints.structural_sections.steel.steel_cross_sections.strip_profile import LoadStandardStrip, StripSteelProfile
+from blueprints.structural_sections.steel.steel_cross_sections.strip_profile import StripSteelProfile
 
 
 @pytest.fixture
@@ -16,7 +17,7 @@ def strip_profile() -> StripSteelProfile:
     """Fixture to set up a Strip profile for testing."""
     profile = Strip.STRIP160x5
     steel_class = SteelStrengthClass.S355
-    return LoadStandardStrip(profile=profile, steel_class=steel_class).get_profile()
+    return StripSteelProfile.from_standard_profile(profile=profile, steel_material=SteelMaterial(steel_class))
 
 
 @pytest.fixture
@@ -24,7 +25,7 @@ def chs_profile() -> CHSSteelProfile:
     """Fixture to set up a CHS profile for testing."""
     profile: CHS = CHS.CHS508x16
     steel_class: SteelStrengthClass = SteelStrengthClass.S355
-    return LoadStandardCHS(profile=profile, steel_class=steel_class).get_profile()
+    return CHSSteelProfile.from_standard_profile(profile=profile, steel_material=SteelMaterial(steel_class))
 
 
 @pytest.fixture
@@ -32,4 +33,4 @@ def i_profile() -> ISteelProfile:
     """Fixture to set up an I-profile for testing."""
     profile = HEB.HEB360
     steel_class = SteelStrengthClass.S355
-    return LoadStandardIProfile(profile=profile, steel_class=steel_class).get_profile()
+    return ISteelProfile.from_standard_profile(profile=profile, steel_material=SteelMaterial(steel_class))
