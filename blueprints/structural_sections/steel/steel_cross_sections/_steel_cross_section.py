4b6baabe5b304061edd5381c264b8c1b057f9873
Enrique García <enriquegarcia@live.nl>
2025-05-07 12:37:27 +0200

    Refactor steel cross-section class for dataclass support
    Refactored the base steel cross-section class to use `@dataclass` for improved readability and maintainability. Renamed the class to `CombinedSteelCrossSection`, added proper type annotations, and updated methods to better align with the new structure. Improved handling of elements, polygon merging, and strength calculations for compatibility with the updated architecture.

--- blueprints/structural_sections/steel/steel_cross_sections/_steel_cross_section.py
+++ blueprints/structural_sections/steel/steel_cross_sections/_steel_cross_section.py
@@ -1,155 +1,154 @@
 """Base class of all steel cross-sections."""
 
 from abc import ABC
+from collections.abc import Sequence
+from dataclasses import dataclass, field
 
-from sectionproperties.analysis import Section
-from sectionproperties.post.post import SectionProperties
-from sectionproperties.pre import Geometry
-from shapely.geometry import Point, Polygon
+from shapely.geometry import Polygon
 from shapely.geometry.base import BaseGeometry
-from shapely.geometry.polygon import orient  # Add this import for ensuring valid polygons
+from shapely.geometry.polygon import orient
 
-from blueprints.materials.steel import SteelMaterial
+from blueprints.structural_sections._cross_section import CrossSection
 from blueprints.structural_sections.steel.steel_element import SteelElement
-from blueprints.type_alias import KG_M, M3_M, MM, MM2
+from blueprints.type_alias import KG_M, M3_M, MM3, MPA
 from blueprints.unit_conversion import MM3_TO_M3
 
 
-class SteelCrossSection(ABC):
-    """Base class of all steel cross-sections."""
+@dataclass(kw_only=True)
+class CombinedSteelCrossSection(CrossSection, ABC):
+    """Base class of all steel cross-sections.
 
-    def __init__(
-        self,
-        steel_material: SteelMaterial,
-    ) -> None:
-        """Initialize the steel cross-section.
+    Parameters
+    ----------
+    elements : Sequence[SteelElement], optional
+        A sequence of steel elements that make up the cross-section.
+        Default is an empty list.
+    name : str, optional
+        The name of the cross-section. Default is "Combined Steel Cross Section".
+    """
 
-        Parameters
-        ----------
-        steel_material : SteelMaterial
-            Material properties of the steel.
-        """
-        self.steel_material = steel_material
-        self.elements: list[SteelElement] = []
+    elements: Sequence[SteelElement] = field(default_factory=list)
+    name: str = "Combined Steel Cross Section"
 
     @property
     def polygon(self) -> Polygon:
         """Return the polygon of the steel cross-section."""
-        combined_polygon: BaseGeometry = self.elements[0].polygon
+        # check if there are any elements
+        if not self.elements:
+            raise ValueError("No elements have been added to the cross-section.")
+
+        # return the polygon of the first element if there is only one
+        if len(self.elements) == 1:
+            return self.elements[0].cross_section.polygon
+
+        # Combine the polygons of all elements if there is multiple
+        combined_polygon: BaseGeometry = self.elements[0].cross_section.polygon
         for element in self.elements[1:]:
-            combined_polygon = combined_polygon.union(element.polygon)
+            combined_polygon = combined_polygon.union(element.cross_section.polygon)
 
         # Ensure the result is a valid Polygon
-        if isinstance(combined_polygon, Polygon):
-            return orient(combined_polygon)  # Ensure consistent orientation
-        raise TypeError("The combined geometry is not a valid Polygon.")
+        if not isinstance(combined_polygon, Polygon):
+            raise TypeError("The combined geometry is not a valid Polygon.")
+
+        # Ensure consistent orientation
+        return orient(combined_polygon)
 
     @property
-    def steel_volume_per_meter(self) -> M3_M:
+    def volume_per_meter(self) -> M3_M:
         """Total volume of the reinforced cross-section per meter length [m³/m]."""
         length = 1000  # mm
-        return self.polygon.area * length * MM3_TO_M3
+        return self.area * length * MM3_TO_M3
 
     @property
-    def steel_weight_per_meter(self) -> KG_M:
-        """Total weight of the steel elements per meter length [kg/m]."""
-        return self.steel_material.density * self.steel_volume_per_meter
+    def weight_per_meter(self) -> KG_M:
+        """
+        Calculate the weight per meter of the steel element.
 
-    @property
-    def area(self) -> MM2:
-        """Total cross-sectional area of the steel element [mm²]."""
-        return self.polygon.area
-
-    @property
-    def centroid(self) -> Point:
-        """Centroid of the steel cross-section."""
-        return self.polygon.centroid
+        Returns
+        -------
+        KG_M
+            The weight per meter of the steel element.
+        """
+        return sum(element.weight_per_meter for element in self.elements)
 
     @property
     def moment_of_inertia_about_y(self) -> KG_M:
         """Moment of inertia about the y-axis per meter length [mm⁴]."""
-        body_moments_of_inertia = sum(element.moment_of_inertia_about_y for element in self.elements)
-        parallel_axis_theorem = sum(element.area * (element.centroid.y - self.centroid.y) ** 2 for element in self.elements)
+        body_moments_of_inertia = sum(element.cross_section.moment_of_inertia_about_y for element in self.elements)
+        parallel_axis_theorem = sum(
+            element.cross_section.area * (element.cross_section.centroid.y - self.centroid.y) ** 2 for element in self.elements
+        )
         return body_moments_of_inertia + parallel_axis_theorem
 
     @property
     def moment_of_inertia_about_z(self) -> KG_M:
         """Moment of inertia about the z-axis per meter length [mm⁴]."""
-        body_moments_of_inertia = sum(element.moment_of_inertia_about_z for element in self.elements)
-        parallel_axis_theorem = sum(element.area * (element.centroid.x - self.centroid.x) ** 2 for element in self.elements)
+        body_moments_of_inertia = sum(element.cross_section.moment_of_inertia_about_z for element in self.elements)
+        parallel_axis_theorem = sum(
+            element.cross_section.area * (element.cross_section.centroid.x - self.centroid.x) ** 2 for element in self.elements
+        )
         return body_moments_of_inertia + parallel_axis_theorem
 
     @property
     def elastic_section_modulus_about_y_positive(self) -> KG_M:
         """Elastic section modulus about the y-axis on the positive z side [mm³]."""
-        distance_to_top = max(y for element in self.elements for _, y in element.geometry.points) - self.centroid.y
+        distance_to_top = max(y for _, y in self.polygon.exterior.coords) - self.centroid.y
         return self.moment_of_inertia_about_y / distance_to_top
 
     @property
     def elastic_section_modulus_about_y_negative(self) -> KG_M:
         """Elastic section modulus about the y-axis on the negative z side [mm³]."""
-        distance_to_bottom = self.centroid.y - min(y for element in self.elements for _, y in element.geometry.points)
+        distance_to_bottom = self.centroid.y - min(y for _, y in self.polygon.exterior.coords)
         return self.moment_of_inertia_about_y / distance_to_bottom
 
     @property
     def elastic_section_modulus_about_z_positive(self) -> KG_M:
         """Elastic section modulus about the z-axis on the positive y side [mm³]."""
-        distance_to_right = max(x for element in self.elements for x, _ in element.geometry.points) - self.centroid.x
+        distance_to_right = max(x for x, _ in self.polygon.exterior.coords) - self.centroid.x
         return self.moment_of_inertia_about_z / distance_to_right
 
     @property
     def elastic_section_modulus_about_z_negative(self) -> KG_M:
         """Elastic section modulus about the z-axis on the negative y side [mm³]."""
-        distance_to_left = self.centroid.x - min(x for element in self.elements for x, _ in element.geometry.points)
+        distance_to_left = self.centroid.x - min(x for x, _ in self.polygon.exterior.coords)
         return self.moment_of_inertia_about_z / distance_to_left
 
-    def geometry(
-        self,
-        mesh_size: MM | None = None,
-    ) -> Geometry:
-        """Return the geometry of the cross-section.
+    @property
+    def plastic_section_modulus_about_y(self) -> MM3 | None:
+        """Plastic section modulus about the y-axis [mm³]."""
+        return self.section_properties().sxx
 
-        Properties
-        ----------
-        mesh_size : MM
-            Maximum mesh element area to be used within
-            the Geometry-object finite-element mesh. If not provided, a default value will be used.
+    @property
+    def plastic_section_modulus_about_z(self) -> MM3 | None:
+        """Plastic section modulus about the z-axis [mm³]."""
+        return self.section_properties().syy
 
+    @property
+    def yield_strength(self) -> MPA:
         """
-        if mesh_size is None:
-            mesh_size = 2.0
+        Calculate the yield strength of the steel element.
 
-        geom = Geometry(geom=self.polygon)
-        geom.create_mesh(mesh_sizes=mesh_size)
-        return geom
+        This is the minimum yield strength of all elements in the cross-section.
 
-    def section(self) -> Section:
-        """Section object representing the cross-section."""
-        return Section(geometry=self.geometry())
-
-    def section_properties(
-        self,
-        geometric: bool = True,
-        plastic: bool = True,
-        warping: bool = True,
-    ) -> SectionProperties:
-        """Calculate and return the section properties of the cross-section.
-
-        Parameters
-        ----------
-        geometric : bool
-            Whether to calculate geometric properties.
-        plastic: bool
-            Whether to calculate plastic properties.
-        warping: bool
-            Whether to calculate warping properties.
+        Returns
+        -------
+        MPa
+            The yield strength of the steel element.
         """
-        section = self.section()
+        # let's find the minimum yield strength of all elements
+        return min(element.yield_strength for element in self.elements)
 
-        if any([geometric, plastic, warping]):
-            section.calculate_geometric_properties()
-        if warping:
-            section.calculate_warping_properties()
-        if plastic:
-            section.calculate_plastic_properties()
-        return section.section_props
+    @property
+    def ultimate_strength(self) -> MPA:
+        """
+        Calculate the ultimate strength of the steel element.
+
+        This is the minimum ultimate strength of all elements in the cross-section.
+
+        Returns
+        -------
+        MPa
+            The ultimate strength of the steel element.
+        """
+        # let's find the minimum ultimate strength of all elements
+        return min(element.yield_strength for element in self.elements)
