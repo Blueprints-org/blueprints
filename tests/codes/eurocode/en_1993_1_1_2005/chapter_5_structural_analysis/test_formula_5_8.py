401f0722fc185aec26daf4ec2aa6535d0e083e30
Enrique García <enriquegarcia@live.nl>
2025-06-11 14:30:28 +0200

    Add tests for EN 1993-1-1:2005 and update import paths in test_formula_5_8

--- tests/codes/eurocode/en_1993_1_1_2005/chapter_5_structural_analysis/test_formula_5_8.py
+++ tests/codes/eurocode/en_1993_1_1_2005/chapter_5_structural_analysis/test_formula_5_8.py
@@ -2,7 +2,7 @@
 
 import pytest
 
-from blueprints.codes.eurocode.nen_en_1993_1_1_c2_a1_2016.chapter_5_structural_analysis.formula_5_8 import Form5Dot8CheckSlenderness
+from blueprints.codes.eurocode.en_1993_1_1_2005.chapter_5_structural_analysis.formula_5_8 import Form5Dot8CheckSlenderness
 from blueprints.validations import LessOrEqualToZeroError
 
 
