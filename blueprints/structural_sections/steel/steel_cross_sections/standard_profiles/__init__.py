d87503ff3c53aae5422aa21b5f907500fd2b9940
Sina Zel taat <sina.zt@gmail.com>
2025-07-23 15:12:47 +0200

    add easy import for standard steel profiles

--- blueprints/structural_sections/steel/steel_cross_sections/standard_profiles/__init__.py
+++ blueprints/structural_sections/steel/steel_cross_sections/standard_profiles/__init__.py
@@ -1,1 +1,13 @@
 """Common steel cross sectional shapes."""
+
+from blueprints.structural_sections.steel.steel_cross_sections.standard_profiles.chs import CHS  # noqa: F401
+from blueprints.structural_sections.steel.steel_cross_sections.standard_profiles.hea import HEA  # noqa: F401
+from blueprints.structural_sections.steel.steel_cross_sections.standard_profiles.heb import HEB  # noqa: F401
+from blueprints.structural_sections.steel.steel_cross_sections.standard_profiles.hem import HEM  # noqa: F401
+from blueprints.structural_sections.steel.steel_cross_sections.standard_profiles.ipe import IPE  # noqa: F401
+from blueprints.structural_sections.steel.steel_cross_sections.standard_profiles.rhs import RHS  # noqa: F401
+from blueprints.structural_sections.steel.steel_cross_sections.standard_profiles.rhscf import RHSCF  # noqa: F401
+from blueprints.structural_sections.steel.steel_cross_sections.standard_profiles.shs import SHS  # noqa: F401
+from blueprints.structural_sections.steel.steel_cross_sections.standard_profiles.shscf import SHSCF  # noqa: F401
+from blueprints.structural_sections.steel.steel_cross_sections.standard_profiles.strip import Strip  # noqa: F401
+from blueprints.structural_sections.steel.steel_cross_sections.standard_profiles.unp import UNP  # noqa: F401
