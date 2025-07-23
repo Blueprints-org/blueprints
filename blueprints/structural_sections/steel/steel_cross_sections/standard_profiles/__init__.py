a4ff38f4c60a5ba8281d5c467f2eb87b2d359f37
Enrique García <enriquegarcia@live.nl>
2025-07-23 15:44:31 +0200

    Refactor imports in __init__.py to include all standard steel profiles

--- blueprints/structural_sections/steel/steel_cross_sections/standard_profiles/__init__.py
+++ blueprints/structural_sections/steel/steel_cross_sections/standard_profiles/__init__.py
@@ -1,13 +1,27 @@
 """Common steel cross sectional shapes."""
 
-from blueprints.structural_sections.steel.steel_cross_sections.standard_profiles.chs import CHS  # noqa: F401
-from blueprints.structural_sections.steel.steel_cross_sections.standard_profiles.hea import HEA  # noqa: F401
-from blueprints.structural_sections.steel.steel_cross_sections.standard_profiles.heb import HEB  # noqa: F401
-from blueprints.structural_sections.steel.steel_cross_sections.standard_profiles.hem import HEM  # noqa: F401
-from blueprints.structural_sections.steel.steel_cross_sections.standard_profiles.ipe import IPE  # noqa: F401
-from blueprints.structural_sections.steel.steel_cross_sections.standard_profiles.rhs import RHS  # noqa: F401
-from blueprints.structural_sections.steel.steel_cross_sections.standard_profiles.rhscf import RHSCF  # noqa: F401
-from blueprints.structural_sections.steel.steel_cross_sections.standard_profiles.shs import SHS  # noqa: F401
-from blueprints.structural_sections.steel.steel_cross_sections.standard_profiles.shscf import SHSCF  # noqa: F401
-from blueprints.structural_sections.steel.steel_cross_sections.standard_profiles.strip import Strip  # noqa: F401
-from blueprints.structural_sections.steel.steel_cross_sections.standard_profiles.unp import UNP  # noqa: F401
+from blueprints.structural_sections.steel.steel_cross_sections.standard_profiles.chs import CHS
+from blueprints.structural_sections.steel.steel_cross_sections.standard_profiles.hea import HEA
+from blueprints.structural_sections.steel.steel_cross_sections.standard_profiles.heb import HEB
+from blueprints.structural_sections.steel.steel_cross_sections.standard_profiles.hem import HEM
+from blueprints.structural_sections.steel.steel_cross_sections.standard_profiles.ipe import IPE
+from blueprints.structural_sections.steel.steel_cross_sections.standard_profiles.rhs import RHS
+from blueprints.structural_sections.steel.steel_cross_sections.standard_profiles.rhscf import RHSCF
+from blueprints.structural_sections.steel.steel_cross_sections.standard_profiles.shs import SHS
+from blueprints.structural_sections.steel.steel_cross_sections.standard_profiles.shscf import SHSCF
+from blueprints.structural_sections.steel.steel_cross_sections.standard_profiles.strip import Strip
+from blueprints.structural_sections.steel.steel_cross_sections.standard_profiles.unp import UNP
+
+__all__ = [
+    "CHS",
+    "HEA",
+    "HEB",
+    "HEM",
+    "IPE",
+    "RHS",
+    "RHSCF",
+    "SHS",
+    "SHSCF",
+    "UNP",
+    "Strip",
+]
