44ec66b7ec7b5a1d0d29d1d09c7e99066bd10f46
Rick van Dijk <rvandijk@viktor.ai>
2025-07-23 15:02:46 +0200

    Run ruff

--- blueprints/utils/lazy_imports.py
+++ blueprints/utils/lazy_imports.py
@@ -3,10 +3,9 @@
 import importlib
 import re
 from types import ModuleType
-from typing import List
 
 
-def lazy_import_get_attr(_package: str, _name: str, _chapters: List[str]) -> ModuleType:
+def lazy_import_get_attr(_package: str, _name: str, _chapters: list[str]) -> ModuleType:
     """Function overrides the getattr for using modules, to allow for lazy importing.
 
     This is used as (for example):
@@ -32,43 +31,33 @@ def lazy_import_get_attr(_package: str, _name: str, _chapters: List[str]) -> Mod
     # Parse the given Form name, to get the path to it
     match = re.match(r"(Form|SubForm|Table)(\d+|[a-zA-Z])Dot(\w+)", _name)
     if match:
-        print("A")
         formula_type, chapter, rest = match.groups()
         num_match = re.match(r"((?:\d+And)*\d+)([a-z]*)([A-Z]\w*)", rest)
         if not num_match:
-            print("B")
             raise AttributeError(f"module {_package} has no attribute {_name}")
         number_part, prefix, suffix = num_match.groups()
         formula_numbers = "_".join(number_part.split("And"))
 
         # get the right sub_module, based on the pattern. Pattern is always chapter_3_... or annex_a...
         if chapter.isdigit():
-            print("C")
             pattern = re.compile(rf"chapter_{chapter}(_|$)")
             result = next((c for c in _chapters if pattern.search(c)), None)
         else:
-            print("D")
             pattern = re.compile(rf"annex_{chapter.lower()}(_|$)")
             result = next((c for c in _chapters if pattern.search(c)), None)
 
         if formula_type == "Table":
-            print("E")
             module_name = f"{result}.table_{chapter.lower()}_{formula_numbers}"
         else:
-            print("F")
             module_name = f"{result}.formula_{chapter.lower()}_{formula_numbers}"
 
             # If name of the formula starts with an capital N and next character is upper (name check); add n
             if prefix:
-                print("G")
                 module_name += f"{prefix}"
 
         try:
-            print("H")
             module = importlib.import_module(f".{module_name}", _package)
             return getattr(module, _name)
         except (ImportError, AttributeError):
-            print("G")
             raise AttributeError(f"module {_package} has no attribute {_name}")
-    print("I")
     raise AttributeError(f"module {_package} has no attribute {_name}")
--- tests/utils/test_lazy_importing.py
+++ tests/utils/test_lazy_importing.py
@@ -1,47 +1,26 @@
 """Tests for the ABCEnumMeta class."""
-import importlib
-import pkgutil
-from abc import ABCMeta, abstractmethod
-from enum import Enum, EnumMeta
-from types import ModuleType
 
 import pytest
 
-from blueprints.utils.abc_enum_meta import ABCEnumMeta
-
-
-def get_first_submodule(package):
-    """Returns the first submodule if the input is a package."""
-    if not hasattr(package, '__path__'):
-        print(f"⚠️ {package.__name__} is not a package, stopping here.")
-        return None  # Not a package
-    submodules = list(pkgutil.iter_modules(package.__path__))
-    if not submodules:
-        print(f"⚠️ {package.__name__} has no submodules.")
-        return None
-    first = sorted(submodules, key=lambda x: x.name)[0]
-    full_name = f"{package.__name__}.{first.name}"
-    return importlib.import_module(full_name)
-
 
 class TestLazyImporting:
-    """Test class for ABCEnumMeta."""
+    """Test class for Lazy Importing."""
 
     def test_eurocode_skip(self) -> None:
-        """Test if imports are properly working when the eurocode module is skipped"""
+        """Test if imports are properly working when the eurocode module is skipped."""
         import blueprints.codes.eurocode.en_1992_2_2005 as en
+
         assert en is not None
         import blueprints.codes.en_1992_2_2005 as en2
+
         assert en == en2
 
     def test_norm_items(self) -> None:
-        """From all norms, test the inputs of at least 1 (edgecase) formula, to ensure the nen/__init__ works"""
-
+        """From all norms, test the inputs of at least 1 (edgecase) formula, to ensure the nen/__init__ works."""
         # Normal import
         import blueprints.codes.eurocode.en_1992_1_1_2004 as en
-        from blueprints.codes.eurocode.en_1992_1_1_2004.chapter_3_materials.formula_3_1 import (
-            Form3Dot1EstimationConcreteCompressiveStrength
-        )
+        from blueprints.codes.eurocode.en_1992_1_1_2004.chapter_3_materials.formula_3_1 import Form3Dot1EstimationConcreteCompressiveStrength
+
         assert en.Form3Dot1EstimationConcreteCompressiveStrength == Form3Dot1EstimationConcreteCompressiveStrength
 
         # Import table and import with n
@@ -49,55 +28,63 @@ class TestLazyImporting:
 
         # Import with large dot number
         import blueprints.codes.eurocode.en_1992_2_2005 as en
+
         assert en.Form5Dot101Imperfections is not None
 
         # Normal import
         import blueprints.codes.eurocode.en_1993_1_1_2005 as en
+
         assert en.Form6Dot23CheckTorsionalMoment is not None
 
         # Normal import
         import blueprints.codes.eurocode.en_1993_1_8_2005 as en
+
         assert en.Form4Dot2CheckWeldedConnection is not None
 
         # Import annex form
-        print("THIS SHOULD TRIGGER D")
         import blueprints.codes.eurocode.en_1993_1_9_2005 as en
+
         assert en.FormADot1DamageDuringDesignLife is not None
 
         # Normal import
         import blueprints.codes.eurocode.en_1993_5_2007 as en
+
         assert en.Form5Dot20ReducedMomentResistanceClass2ZProfiles is not None
 
         # Normal import
         import blueprints.codes.eurocode.en_1995_1_1_2004 as en
+
         assert en.Form7Dot5NaturalFrequency is not None
 
         # Import with b
         import blueprints.codes.eurocode.nen_9997_1_c2_2017 as en
+
         assert en.Form2Dot1bRepresentativeValue is not None
 
         # Normal import
         import blueprints.codes.eurocode.nen_en_1992_1_1_a1_2020 as en
+
         assert en.Form8Dot14EquivalentDiameterBundledBars is not None
 
     def test_lazy_import_errors(self) -> None:
         """Test if lazy importing raises the right errors."""
-
         # Calculate attribute error when poorly importing
+        import blueprints.codes.eurocode.en_1992_1_1_2004 as en
+
         with pytest.raises(AttributeError):
-            import blueprints.codes.eurocode.en_1992_1_1_2004 as en
             en.NotAForm(1, 1)
 
+        import blueprints.codes.eurocode.en_1992_1_1_2004 as en
+
         with pytest.raises(AttributeError):
-            import blueprints.codes.eurocode.en_1992_1_1_2004 as en
             en.Form999Dot999NonExistingFunction(1, 1)
 
+        import blueprints.codes.eurocode.en_1992_1_1_2004 as en
+
         with pytest.raises(AttributeError):
-            import blueprints.codes.eurocode.en_1992_1_1_2004 as en
             en.Form1Dothello_world(1, 1)
 
+        import blueprints.codes.eurocode.en_1992_1_1_2004 as en
+
         with pytest.raises(AttributeError):
-            import blueprints.codes.eurocode.en_1992_1_1_2004 as en
             en.Table3Dot123abcMyClass(1, 1)
-
-
