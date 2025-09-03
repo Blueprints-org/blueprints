d2bb1ea15842ead8eb294294148152447d3b4df8
marianthisous <marianthi.sousamli@bam.com>
2025-09-03 09:37:43 +0200

    #en1995_2023_appen_e Formulas E1-E7 implementation

---
+++ blueprints/codes/eurocode/nen_en_1995_1_1_2023/appendix_e/formula_e_2.py
@@ -0,0 +1,86 @@
+"""Formula E.1 from NEN-EN 1995-1-1:2023."""
+
+import math
+
+from blueprints.codes.eurocode.nen_en_1995_1_1_2023 import NEN_EN_1995_1_1_2023
+from blueprints.codes.formula import Formula
+from blueprints.codes.latex_formula import LatexFormula, latex_replace_symbols
+from blueprints.type_alias import DIMENSIONLESS, MM, MM2, MPA, N_MM
+from blueprints.validations import raise_if_less_or_equal_to_zero
+
+
+class FormEDot2MechanicalConnectEfficiencyFactor(Formula):
+    r"""Class representing formula E.2 for the factors for the efficienct of the mechanical \\
+        connections of the respective i-numbered parts of the cross-section.
+    """
+
+    label = "E.2"
+    source_document = NEN_EN_1995_1_1_2023
+
+    def __init__(self, i: DIMENSIONLESS, e_i: MPA, a_i: MM2, s_i: MM, k_i: N_MM, length: MM) -> None:
+        r"""[$\gamma_i$] Factor for the efficiency of the mechanical connections of the respective i-numbered parts of the cross-section.
+
+        NEN-EN 1995-1-1 art E.4(1) - Formula (E.2)
+
+        Parameters
+        ----------
+        i : DIMENSIONLESS
+            [$i$] Number of layer [$-$].
+        e_i : MPA
+            [$E_i$] Modulus of elasticity of the i-numbered part of the cross-section [$MPA$].
+        a_i : MM2
+            [$A_i$] Area of the i-numbered part of the cross-section [$mm^2$].
+        s_i : MM
+            [$s_i$] Spacing between the connections, for i=1 and i=3 [$mm$].
+                    For this formula implementation s_2 for i=2 needs to be given too, but it won't be used.
+        k_i : N_MM
+            [$K_i$] Stiffness of connectors, according to the limit state under consideration [$N/mm$].
+        length : MM
+            [$l$] Span of the member [$mm$].
+
+        Returns
+        -------
+        None
+        """
+        super().__init__()
+        self.i = i
+        self.e_i = e_i
+        self.a_i = a_i
+        self.s_i = s_i
+        self.k_i = k_i
+        self.length = length
+
+    @staticmethod
+    def _evaluate(i: int, e_i: MPA, a_i: MM2, s_i: MM, k_i: N_MM, length: MM) -> DIMENSIONLESS:
+        """Evaluates the formula, for more information see the __init__method."""
+        # Ensure that a valid layer number is used
+        if i not in {1, 2, 3}:
+            raise ValueError("The number of the layer must be either 1, 2 or 3.")
+
+        # Ensure that the input parameters have valid values
+        raise_if_less_or_equal_to_zero(length=length, e_i=e_i, a_i=a_i, s_i=s_i, k_i=k_i)
+
+        pi = math.pi
+        l_2 = 2
+
+        return 1 if i == l_2 else 1 / (1 + pi**2 * e_i * a_i * s_i / (k_i * length**2))
+
+    def latex(self) -> LatexFormula:
+        """Returns LatexFormula object for formula E.2."""
+        l_2 = 2
+        if self.i == l_2:
+            eq_i = f"{self:.2f}"
+            numeric_eq = f"{self:.2f}"
+        else:
+            eq_i = f"\\frac{{1}}{{1+\\frac{{\\pi^2 E_{self.i} A_{self.i} s_{self.i}}}{{K_{self.i} l^2}}}}"
+
+            e_istr = {f"E_{self.i}": rf"{self.e_i:.2f} \cdot"}
+            a_istr = {f"A_{self.i}": rf"{self.a_i:.2f} \cdot"}
+            s_istr = {f"s_{self.i}": rf"{self.s_i:.2f}"}
+            k_istr = {f"K_{self.i}": rf"{self.k_i:.2f} \cdot"}
+            l_str = {"l": f"{self.length:.2f}"}
+            repl_symb = e_istr | a_istr | s_istr | k_istr | l_str
+            numeric_eq = latex_replace_symbols(eq_i, repl_symb)
+        return LatexFormula(
+            return_symbol=f"\\gamma_{self.i}", result=f"{self:.2f}", equation=eq_i, numeric_equation=numeric_eq, comparison_operator_label="="
+        )
---
+++ blueprints/codes/eurocode/nen_en_1995_1_1_2023/appendix_e/formula_e_7.py
@@ -0,0 +1,62 @@
+"""Formula E.7 from NEN-EN 1995-1-1:2023."""
+
+from blueprints.codes.eurocode.nen_en_1995_1_1_2023 import NEN_EN_1995_1_1_2023
+from blueprints.codes.formula import Formula
+from blueprints.codes.latex_formula import LatexFormula, latex_replace_symbols
+from blueprints.type_alias import MM, MM4
+from blueprints.validations import raise_if_less_or_equal_to_zero
+
+
+class FormEDot7SecondMomentInertia(Formula):
+    r"""Class representing formula E.7 for second moment of inertia of the i-numbered part of the cross-section."""
+
+    label = "E.7"
+    source_document = NEN_EN_1995_1_1_2023
+
+    def __init__(self, b_i: MM, h_i: MM, i: int) -> None:
+        r"""[$I_i$] Second moment of inertia of the i-numbered part of the cross-section, in [$mm^4$].
+
+        NEN-EN 1995-1-1 art E.4(1) - Formula (E.7)
+
+        Parameters
+        ----------
+        b_i : MM
+            [$b_i$] Width of the i-numbered part of the cross-section [$mm$].
+        h_i : MM
+            [$h_2$] Depth of the i-numbered part of the cross-section [$mm$].
+        i : DIMENSIONLESS
+            [$i$] Number of layer i of cross-section.
+
+        Returns
+        -------
+        None
+        """
+        super().__init__()
+        self.b_i = b_i
+        self.h_i = h_i
+        self.i = i
+
+    @staticmethod
+    def _evaluate(b_i: MM, h_i: MM, i: int) -> MM4:
+        """Evaluates the formula, for more information see the __init__method."""
+        # Ensure that a valid layer number is used
+        if i not in {1, 2, 3}:
+            raise ValueError("The number of the layer must be either 1, 2 or 3.")
+
+        # Ensure that the input parameters have valid values
+        raise_if_less_or_equal_to_zero(b_i=b_i, h_i=h_i)
+
+        return (b_i * h_i**3) / 12
+
+    def latex(self) -> LatexFormula:
+        """Returns LatexFormula object for formula E.7."""
+        eq_i = f"\\frac{{b_{self.i} h_{self.i}^3}}{{12}}"
+
+        repl_symb = {
+            f"b_{self.i}": rf"{self.b_i:.2f} \cdot",
+            f"h_{self.i}": rf"{self.h_i:.2f}",
+        }
+        numeric_eq = latex_replace_symbols(eq_i, repl_symb)
+        return LatexFormula(
+            return_symbol=f"I_{self.i}", result=f"{self:.2f}", equation=eq_i, numeric_equation=numeric_eq, comparison_operator_label="="
+        )
