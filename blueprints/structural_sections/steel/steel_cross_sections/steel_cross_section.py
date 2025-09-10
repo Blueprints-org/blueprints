3291eacd3a4dbe4c303b0371ecc4f11a4c5a6ddf
Sina Zel taat <sina.zt@gmail.com>
2025-09-10 12:25:50 +0200

    remove unnecessary yield and ultimate strength methods [YAGNI]

--- blueprints/structural_sections/steel/steel_cross_sections/steel_cross_section.py
+++ blueprints/structural_sections/steel/steel_cross_sections/steel_cross_section.py
@@ -1,22 +1,22 @@
-"""Module containing the class definition for a steel cross-section element."""
+"""Module containing the class definition for a steel cross-section."""
 
 from dataclasses import dataclass
 
 from blueprints.materials.steel import SteelMaterial
 from blueprints.structural_sections._cross_section import CrossSection
-from blueprints.type_alias import KG_M, MM, MPA
+from blueprints.type_alias import KG_M
 from blueprints.unit_conversion import MM2_TO_M2
 
 
 @dataclass(frozen=True, kw_only=True)
 class SteelCrossSection:
     """
-    Representation of steel cross-sections for any given cross-section.
+    Representation of a steel cross-section for any given cross-section or profile.
 
     Parameters
     ----------
     cross_section : CrossSection
-        The cross-section.
+        The cross-section. This can be a predefined profile or a generic cross-section.
     material : SteelMaterial
         The material type of the steel.
     """
@@ -27,55 +27,11 @@ class SteelCrossSection:
     @property
     def weight_per_meter(self) -> KG_M:
         """
-        Calculate the weight per meter of the steel element.
+        Calculate the weight per meter of the steel cross-section.
 
         Returns
         -------
         KG_M
-            The weight per meter of the steel element.
+            The weight per meter of the steel cross-section.
         """
         return self.material.density * (self.cross_section.area * MM2_TO_M2)
-
-    def yield_strength(self, nominal_thickness: MM) -> MPA:
-        """
-        Calculate the yield strength of the steel element.
-
-        Parameters
-        ----------
-        nominal_thickness : MM
-            The nominal thickness of the steel cross-section.
-
-        Attention: There is no internal check to make sure that the given nominal thickness of this steel cross-section
-        is actually the same thickness of the cross-section.
-
-        Returns
-        -------
-        MPa
-            The yield strength of the steel element.
-        """
-        fy = self.material.yield_strength(thickness=nominal_thickness)
-        if fy is None:
-            raise ValueError("Yield strength is not defined for this material.")
-        return fy
-
-    def ultimate_strength(self, nominal_thickness: MM) -> MPA:
-        """
-        Calculate the ultimate strength of the steel element.
-
-        Parameters
-        ----------
-        nominal_thickness : MM
-            The nominal thickness of the steel cross-section.
-
-        Attention: There is no internal check to make sure that the given nominal thickness of this steel cross-section
-        is actually the same thickness of the cross-section.
-
-        Returns
-        -------
-        MPa
-            The ultimate strength of the steel element.
-        """
-        fu = self.material.ultimate_strength(thickness=nominal_thickness)
-        if fu is None:
-            raise ValueError("Ultimate strength is not defined for this material.")
-        return fu
--- tests/structural_sections/steel/steel_cross_sections/test_steel_cross_section.py
+++ tests/structural_sections/steel/steel_cross_sections/test_steel_cross_section.py
@@ -2,8 +2,6 @@
 
 from unittest.mock import Mock
 
-import pytest
-
 from blueprints.structural_sections.steel.steel_cross_sections.steel_cross_section import SteelCrossSection
 
 
@@ -31,27 +29,3 @@ def test_weight_per_meter(steel_cross_section: SteelCrossSection, mock_cross_sec
     """Test that the SteelCrossSection weight per meter is calculated correctly."""
     expected_weight: float = mock_material.density * (mock_cross_section.area * 1e-6)
     assert steel_cross_section.weight_per_meter == expected_weight
-
-
-def test_yield_strength(steel_cross_section: SteelCrossSection, mock_material: Mock) -> None:
-    """Test that the SteelCrossSection yield strength matches the mock material yield strength."""
-    assert steel_cross_section.yield_strength(nominal_thickness=10) == mock_material.yield_strength.return_value
-
-
-def test_ultimate_strength(steel_cross_section: SteelCrossSection, mock_material: Mock) -> None:
-    """Test that the SteelCrossSection ultimate strength matches the mock material ultimate strength."""
-    assert steel_cross_section.ultimate_strength(nominal_thickness=10) == mock_material.ultimate_strength.return_value
-
-
-def test_invalid_yield_strength(steel_cross_section: SteelCrossSection, mock_material: Mock) -> None:
-    """Test that accessing yield strength raises a ValueError if not defined."""
-    mock_material.yield_strength.return_value = None
-    with pytest.raises(ValueError, match="Yield strength is not defined for this material."):
-        _ = steel_cross_section.yield_strength(nominal_thickness=10)
-
-
-def test_invalid_ultimate_strength(steel_cross_section: SteelCrossSection, mock_material: Mock) -> None:
-    """Test that accessing ultimate strength raises a ValueError if not defined."""
-    mock_material.ultimate_strength.return_value = None
-    with pytest.raises(ValueError, match="Ultimate strength is not defined for this material."):
-        _ = steel_cross_section.ultimate_strength(nominal_thickness=10)
