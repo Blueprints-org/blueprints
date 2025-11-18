e4f120d0e5972152f70c6f17557dbb890aad4efe
GerjanDorgelo <gerjan.dorgelo@anteagroup.nl>
2025-11-18 16:36:15 +0100

    Fix normal force calculation in SteelIProfileStrengthClass3 to handle None area values and ensure correct tension and compression checks.

--- blueprints/checks/steel/strength/steel_i_profile_strength_class_3.py
+++ blueprints/checks/steel/strength/steel_i_profile_strength_class_3.py
@@ -74,17 +74,17 @@ class SteelIProfileStrengthClass3:
             if self.load_combination.normal_force == 0:
                 return []
             if self.load_combination.normal_force > 0:  # tension, based on chapter 6.2.3
-                n_ed = self.load_combination.normal_force * KN_TO_N
-                a = self.properties.area
+                a = self.properties.area if self.properties.area is not None else 0
                 f_y = min(element.yield_strength for element in self.profile.elements)
+                n_ed = self.load_combination.normal_force * KN_TO_N
                 n_t_rd = formula_6_6.Form6Dot6DesignPlasticRestistanceGrossCrossSection(a=a, f_y=f_y, gamma_m0=self.gamma_m0)
                 check_tension = formula_6_5.Form6Dot5UnityCheckTensileStrength(n_ed=n_ed, n_t_rd=n_t_rd)
                 return [n_t_rd, check_tension]
 
-            # compression
-            n_ed = -self.load_combination.normal_force * KN_TO_N
-            a = self.properties.area
+            # compression, based on chapter 6.2.4
+            a = self.properties.area if self.properties.area is not None else 0
             f_y = min(element.yield_strength for element in self.profile.elements)
+            n_ed = -self.load_combination.normal_force * KN_TO_N
             n_c_rd = formula_6_10.Form6Dot10NcRdClass1And2And3(a=a, f_y=f_y, gamma_m0=self.gamma_m0)
             check_compression = formula_6_9.Form6Dot9CheckCompressionForce(n_ed=n_ed, n_c_rd=n_c_rd)
             return [n_c_rd, check_compression]
