9a75847e958d1a02f96772a24ff38c5941457c72
GerjanDorgelo <gerjan.dorgelo@anteagroup.nl>
2025-11-28 18:06:10 +0100

    Add test cases for translation using new _test.csv file

--- tests/language/test_translate.py
+++ tests/language/test_translate.py
@@ -9,11 +9,17 @@ class TestCalculateRotationAngle:
     def test_translate_with_formula_manual(self) -> None:
         """Test Translate with translation from CSV."""
         latex = r"\text{This is a test}"
-        result = Translate(latex, "nl")
-        assert str(result) == r"\text{Dit is een test}"
+        result = Translate(latex, "_test")
+        assert str(result) == r"\text{This is a translation}"
 
     def test_translate_with_formula_manual_and_wildcard(self) -> None:
         """Test Translate with wildcard translation from CSV."""
-        latex = r"\text{With formula this should not be translated:}"
-        result = Translate(latex, "nl")
-        assert str(result) == r"\text{Met formule this should not be translated:}"
+        latex = r"\text{This IS A TEST WITH A wildcard}"
+        result = Translate(latex, "_test")
+        assert str(result) == r"\text{Different text |IS A TEST WITH A| here}"
+
+    def test_translate_with_formula_manual_and_multiple_wildcard(self) -> None:
+        """Test Translate with multiple wildcard translation from CSV."""
+        latex = r"\text{Wildcards CAN BE used MULTIPLE TIMES here AND AS OFTEN AS YOU want}"
+        result = Translate(latex, "_test")
+        assert str(result) == r"\text{Completely |CAN BE| different |MULTIPLE TIMES| text |AND AS OFTEN AS YOU| here}"
