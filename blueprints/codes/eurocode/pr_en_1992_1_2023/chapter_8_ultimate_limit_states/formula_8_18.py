1d3a25ed10931ad5b796058bd7494e55323837c1
Casper van der Krift <casper.van.der.krift@rhdhv.com>
2025-12-12 11:09:52 +0100

    format with ruff

--- blueprints/codes/eurocode/pr_en_1992_1_2023/chapter_8_ultimate_limit_states/formula_8_18.py
+++ blueprints/codes/eurocode/pr_en_1992_1_2023/chapter_8_ultimate_limit_states/formula_8_18.py
@@ -1,19 +1,19 @@
 """Formula 8.18 from prEN-1992-1-1:2023: Chapter 8: Ultimate limit states (ULS)."""
 
+from blueprints.codes.eurocode.pr_en_1992_1_2023 import PR_EN_1992_1_1_2023
 from blueprints.codes.formula import Formula
-from blueprints.codes.eurocode.pr_en_1992_1_2023 import pr_EN_1992_1_1_2023
-from blueprints.type_alias import MPA, N, MM
-from blueprints.validations import raise_if_negative, raise_if_less_or_equal_to_zero
-from blueprints.codes.latex_formula import LatexFormula
-from blueprints.codes.latex_formula import latex_replace_symbols
+from blueprints.codes.latex_formula import LatexFormula, latex_replace_symbols
+from blueprints.type_alias import MM, MPA, N
+from blueprints.validations import raise_if_less_or_equal_to_zero, raise_if_negative
 
 
 class Form8Dot18AverageShearStress(Formula):
     """Class representing formula 8.18 for the calculation of the average shear stress over the cross-section
-    in regions of members without geometric discontinuities."""
+    in regions of members without geometric discontinuities.
+    """
 
     label = "8.18"
-    source_document = pr_EN_1992_1_1_2023
+    source_document = PR_EN_1992_1_1_2023
 
     def __init__(self, v_ed: N, b_w: MM, z: MM) -> None:
         r"""[$\tau_{Ed}$] Average shear stress over the cross-section area.
@@ -41,21 +41,14 @@ class Form8Dot18AverageShearStress(Formula):
             v_ed=v_ed,
         )
 
-        raise_if_less_or_equal_to_zero(
-            b_w=b_w,
-            z=z
-        )
+        raise_if_less_or_equal_to_zero(b_w=b_w, z=z)
         return v_ed / (b_w * z)
 
     def latex(self, n: int = 3) -> LatexFormula:
         """Returns LatexFormula object for formula 8.18."""
         _equation: str = r"\frac{V_{Ed}}{b_w \cdot z}"
         _numeric_equation: str = latex_replace_symbols(
-            _equation,
-            replacements={r"V_{Ed}": f"{self.v_ed:.{n}f}",
-                          r"b_w": f"{self.b_w:.{n}f}",
-                          r"z": f"{self.z:.{n}f}"},
-            unique_symbol_check=False
+            _equation, replacements={r"V_{Ed}": f"{self.v_ed:.{n}f}", r"b_w": f"{self.b_w:.{n}f}", r"z": f"{self.z:.{n}f}"}, unique_symbol_check=False
         )
         return LatexFormula(
             return_symbol=r"\tau_{Ed}",
@@ -63,13 +56,5 @@ class Form8Dot18AverageShearStress(Formula):
             equation=_equation,
             numeric_equation=_numeric_equation,
             comparison_operator_label="=",
-            unit="MPa"
+            unit="MPa",
         )
-
-
-if __name__ == '__main__':
-    my_form = Form8Dot18AverageShearStress(v_ed=10000.0, b_w=-50.0, z=215.0)
-    print(my_form)
-    print(my_form.latex())
-    print(my_form.latex().complete)
-    print(my_form.latex().short)
--- tests/codes/eurocode/pr_en_1992_1_2023/chapter_8_ultimate_limit_states/test_formula_8_18.py
+++ tests/codes/eurocode/pr_en_1992_1_2023/chapter_8_ultimate_limit_states/test_formula_8_18.py
@@ -1,4 +1,4 @@
-"""Testing formula 8.18 of prEN 1992-1-1:2023"""
+"""Testing formula 8.18 of prEN 1992-1-1:2023."""
 
 import pytest
 
@@ -17,28 +17,21 @@ class TestForm8Dot18AverageShearStress:
         z = 215.0
 
         # Create object to test
-        test_formula = Form8Dot18AverageShearStress(
-            v_ed=v_ed,
-            b_w=b_w,
-            z=z
-        )
+        test_formula = Form8Dot18AverageShearStress(v_ed=v_ed, b_w=b_w, z=z)
 
         # Expected result, manually calculated
-        manually_calculated_result = 0.93023 # MPa
+        manually_calculated_result = 0.93023  # MPa
 
         # Perform test by assert
-        assert test_formula == pytest.approx(
-            expected=manually_calculated_result,
-            rel=1e-4
-        )
+        assert test_formula == pytest.approx(expected=manually_calculated_result, rel=1e-4)
 
     @pytest.mark.parametrize(
         ("v_ed", "b_w", "z"),
         [
-            (-10000.0, 50.0, 215.0), # v_ed is negative
+            (-10000.0, 50.0, 215.0),  # v_ed is negative
         ],
     )
-    def test_raise_error_when_negative_values_are_given(self, v_ed: float, b_w: float, z:float):
+    def test_raise_error_when_negative_values_are_given(self, v_ed: float, b_w: float, z: float) -> None:
         """Test if error is raised for parameters that are not allowed to be negative."""
         with pytest.raises(NegativeValueError):
             Form8Dot18AverageShearStress(v_ed=v_ed, b_w=b_w, z=z)
@@ -60,16 +53,11 @@ class TestForm8Dot18AverageShearStress:
     @pytest.mark.parametrize(
         ("representation", "expected"),
         [
-            (
-                "complete",
-                r"\tau_{Ed} = \frac{V_{Ed}}{b_w \cdot z} = \frac{10000.000}{50.000 \cdot 215.000} = 0.930 \ MPa"
-            ),
-            (
-                "short", r"\tau_{Ed} = 0.930 \ MPa"
-            )
-        ]
+            ("complete", r"\tau_{Ed} = \frac{V_{Ed}}{b_w \cdot z} = \frac{10000.000}{50.000 \cdot 215.000} = 0.930 \ MPa"),
+            ("short", r"\tau_{Ed} = 0.930 \ MPa"),
+        ],
     )
-    def test_latex(self, representation: str, expected: str):
+    def test_latex(self, representation: str, expected: str) -> None:
         """Test the latex representation of the formula."""
         # Example values
         v_ed = 10000.0
@@ -79,9 +67,6 @@ class TestForm8Dot18AverageShearStress:
         # Object to test
         test_latex = Form8Dot18AverageShearStress(v_ed=v_ed, b_w=b_w, z=z).latex()
 
-        actual = {
-            "complete": test_latex.complete,
-            "short": test_latex.short
-        }
+        actual = {"complete": test_latex.complete, "short": test_latex.short}
 
         assert expected == actual[representation]
