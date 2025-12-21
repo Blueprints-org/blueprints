0607643a0f4ddd6db2bf703d12d8171454ab4c97
GerjanDorgelo <gerjan.dorgelo@anteagroup.nl>
2025-12-21 13:32:29 +0100

    feat: implement steel I-profile strength checks for class 3 according to Eurocode 3

---
+++ blueprints/checks/eurocode/en_1993_1_1_2005/ultimate_limit_states/__init__.py
@@ -0,0 +1,1 @@
+"""Steel ultimate limit state checks."""
--- tests/checks/eurocode/en_1993_1_1_2005/ultimate_limit_states/test_normal_force.py
+++ tests/checks/eurocode/en_1993_1_1_2005/ultimate_limit_states/test_normal_force.py
@@ -3,7 +3,7 @@
 import pytest
 from sectionproperties.post.post import SectionProperties
 
-from blueprints.checks.eurocode.en_1993_1_1_2005.strength.normal_force import NormalForceClass123
+from blueprints.checks.eurocode.en_1993_1_1_2005.ultimate_limit_states.normal_force import NormalForceClass123
 from blueprints.saf.results.result_internal_force_1d import ResultFor, ResultInternalForce1D, ResultOn
 from blueprints.structural_sections.steel.steel_cross_sections.i_profile import ISteelProfile
 
