352f06471b08ded17d8dd9f1997b99f6fdbfaaea
GerjanDorgelo <gerjan.dorgelo@anteagroup.nl>
2025-12-21 13:17:28 +0100

    fix: update import paths for nominal concrete cover and steel I profile strength classes to reflect Eurocode structure

--- tests/checks/eurocode/steel/en_1993_1_1_2005/strength/test_steel_i_profile_strength_class_3.py
+++ tests/checks/eurocode/steel/en_1993_1_1_2005/strength/test_steel_i_profile_strength_class_3.py
@@ -3,7 +3,7 @@
 import pytest
 from sectionproperties.post.post import SectionProperties
 
-from blueprints.checks.eurocode.steel.strength.steel_i_profile_strength_class_3 import SteelIProfileStrengthClass3
+from blueprints.checks.eurocode.steel.en_1993_1_1_2005.strength.steel_i_profile_strength_class_3 import SteelIProfileStrengthClass3
 from blueprints.saf.results.result_internal_force_1d import ResultFor, ResultInternalForce1D, ResultOn
 from blueprints.structural_sections.steel.steel_cross_sections.i_profile import ISteelProfile
 
