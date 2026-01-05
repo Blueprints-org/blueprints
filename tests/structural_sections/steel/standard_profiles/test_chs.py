b50f7783edea1c2adda263d60bef6c47b790c537
SZeltaat <111974143+SZeltaat@users.noreply.github.com>
2026-01-05 16:01:52 +0100

    fix tests

--- tests/structural_sections/steel/standard_profiles/test_chs.py
+++ tests/structural_sections/steel/standard_profiles/test_chs.py
@@ -3,13 +3,13 @@
 import pytest
 
 from blueprints.structural_sections.steel.profile_definitions.chs_profile import CHSProfile
-from blueprints.structural_sections.steel.standard_profiles.chs import CHS, CHS_PROFILES
+from blueprints.structural_sections.steel.standard_profiles.chs import CHS, CHS_PROFILES_DATABASE
 
 
 class TestCHS:
     """Tests for the CHS class."""
 
-    @pytest.mark.parametrize(("profile_name", "expected_data"), CHS_PROFILES.items())
+    @pytest.mark.parametrize(("profile_name", "expected_data"), CHS_PROFILES_DATABASE.items())
     def test_as_chs_profile(self, profile_name: str, expected_data: tuple) -> None:
         """Test that the CHS instance is converted to a CHSProfile correctly."""
         profile = getattr(CHS, profile_name)
