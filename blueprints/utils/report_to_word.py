51faab718377f0eea508d884a4a0a1ddb33654a6
GerjanDorgelo <gerjan.dorgelo@anteagroup.nl>
2026-01-07 19:26:51 +0100

    feat: remove example usage from ReportToWordConverter for cleaner code

--- blueprints/utils/report_to_word.py
+++ blueprints/utils/report_to_word.py
@@ -814,33 +814,3 @@ class ReportToWordConverter:
             3: f"{chr(64 + number)}.",  # Level 4: A., B., C., ...
         }
         return formats.get(level, f"{number}.")
-
-
-# example usage:
-if __name__ == "__main__":
-    from blueprints.utils.report import LatexReport
-
-    report = LatexReport("Testing Title")
-    report.add_section("Testing Section")
-    report.add_subsection("Testing Subsection")
-    report.add_subsubsection("Testing Subsubsection")
-    report.add_text("Bold and", bold=True).add_text(" normal text.").add_newline(n=6)
-    report.add_text("And Italic text.", italic=True).add_text(" And also bold and italic.", bold=True, italic=True).add_newline()
-    report.add_text("Here is an inline equation: $E=mc^2$ within the text.").add_newline()
-    report.add_text("test").add_equation("E=mc^2", tag="1", inline=True).add_text("more text.").add_newline()
-    report.add_text("test").add_equation("E=mc^2", inline=True).add_text("more text.").add_newline()
-    report.add_equation("E=mc^2", tag="4")
-    report.add_equation(r"\int_a^b f(x)dx = F(b) - F(a)")
-    report.add_enumerate(["One", ["A", "B", "C"], "Two", ["A", ["I", "II", ["A", "B"], "III"]]])
-    report.add_itemize(["First", "Second", ["Subfirst", "Subsecond"], "Third"])
-    report.add_text("Here is a table:")
-    report.add_table(
-        headers=["Header 1", "Header 2", "Header 3 with math $E=mc^2$"],
-        rows=[["Row 1 Col 1", "Row 1 Col 2 with inline math $a^2 + b^2 = c^2$", "Row 1 Col 3"], ["Row 2 Col 1", "Row 2 Col 2", "Row 2 Col 3"]],
-    )
-    report.add_section("Another Section")
-    report.add_subsection("Another Subsection")
-    report.add_subsection("Yet another Subsection")
-    report.add_subsubsection("Yet another Subsubsection")
-    report.add_subsubsection("Yet another Subsubsection v2")
-    report.to_word("test_report.docx", language="zh")
