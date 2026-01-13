06fc116909914a9da88ce08228fbf9e123dffa46
GerjanDorgelo <gerjan.dorgelo@anteagroup.nl>
2026-01-13 22:03:31 +0100

    refactor: correct class name and remove unused class for formula 8.14 and 8.37 validation

--- tests/codes/eurocode/en_1993_1_1_2025/chapter_8_ultimate_limit_state/test_formula_8_14.py
+++ tests/codes/eurocode/en_1993_1_1_2025/chapter_8_ultimate_limit_state/test_formula_8_14.py
@@ -3,12 +3,12 @@
 import pytest
 
 from blueprints.codes.eurocode.en_1993_1_1_2025.chapter_8_ultimate_limit_state.formula_8_14 import (
-    Form8Dot14DesignPlasticResistanceGrossCrossSection,
+    Form8Dot14DesignPlasticRestistanceGrossCrossSection,
 )
 from blueprints.validations import LessOrEqualToZeroError, NegativeValueError
 
 
-class TestForm8Dot14DesignPlasticResistanceGrossCrossSection:
+class TestForm8Dot14DesignPlasticRestistanceGrossCrossSection:
     """Validation for formula 8.14 from EN 1993-1-1:2025, chapter 8, ultimate limit state."""
 
     def test_evaluation(self) -> None:
@@ -19,7 +19,7 @@ class TestForm8Dot14DesignPlasticResistanceGrossCrossSection:
         gamma_m0 = 1.0
 
         # Object to test
-        formula = Form8Dot14DesignPlasticResistanceGrossCrossSection(a=a, f_y=f_y, gamma_m0=gamma_m0)
+        formula = Form8Dot14DesignPlasticRestistanceGrossCrossSection(a=a, f_y=f_y, gamma_m0=gamma_m0)
 
         # Expected result, manually calculated
         manually_calculated_result = 1775000.0  # N
@@ -36,7 +36,7 @@ class TestForm8Dot14DesignPlasticResistanceGrossCrossSection:
     def test_raise_error_when_negative_values_are_given(self, a: float, f_y: float, gamma_m0: float) -> None:
         """Test invalid values."""
         with pytest.raises(NegativeValueError):
-            Form8Dot14DesignPlasticResistanceGrossCrossSection(a=a, f_y=f_y, gamma_m0=gamma_m0)
+            Form8Dot14DesignPlasticRestistanceGrossCrossSection(a=a, f_y=f_y, gamma_m0=gamma_m0)
 
     @pytest.mark.parametrize(
         ("a", "f_y", "gamma_m0"),
@@ -48,7 +48,7 @@ class TestForm8Dot14DesignPlasticResistanceGrossCrossSection:
     def test_raise_error_when_less_or_equal_to_zero_values_are_given(self, a: float, f_y: float, gamma_m0: float) -> None:
         """Test invalid values."""
         with pytest.raises(LessOrEqualToZeroError):
-            Form8Dot14DesignPlasticResistanceGrossCrossSection(a=a, f_y=f_y, gamma_m0=gamma_m0)
+            Form8Dot14DesignPlasticRestistanceGrossCrossSection(a=a, f_y=f_y, gamma_m0=gamma_m0)
 
     @pytest.mark.parametrize(
         ("representation", "expected"),
@@ -68,7 +68,7 @@ class TestForm8Dot14DesignPlasticResistanceGrossCrossSection:
         gamma_m0 = 1.0
 
         # Object to test
-        latex = Form8Dot14DesignPlasticResistanceGrossCrossSection(a=a, f_y=f_y, gamma_m0=gamma_m0).latex()
+        latex = Form8Dot14DesignPlasticRestistanceGrossCrossSection(a=a, f_y=f_y, gamma_m0=gamma_m0).latex()
 
         actual = {
             "complete": latex.complete,
