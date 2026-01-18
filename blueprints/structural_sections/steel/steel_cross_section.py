af224132809351eec7498b13f869e97bc3ead07b
GerjanDorgelo <gerjan.dorgelo@anteagroup.nl>
2026-01-18 11:21:35 +0100

    feat: add yield_strength and ultimate_strength properties to SteelCrossSection for strength calculations

--- blueprints/structural_sections/steel/steel_cross_section.py
+++ blueprints/structural_sections/steel/steel_cross_section.py
@@ -6,7 +6,7 @@ from dataclasses import dataclass
 
 from blueprints.materials.steel import SteelMaterial
 from blueprints.structural_sections._profile import Profile
-from blueprints.type_alias import KG_M
+from blueprints.type_alias import KG_M, MM
 from blueprints.unit_conversion import MM2_TO_M2
 
 
@@ -27,6 +27,30 @@ class SteelCrossSection:
     """The profile. This can be a predefined profile or a generic profile."""
     material: SteelMaterial
     """The material type of the steel."""
+
+    @property
+    def yield_strength(self) -> MM:
+        """
+        Get the yield strength of the steel material.
+
+        Returns
+        -------
+        MM
+            The yield strength of the steel material.
+        """
+        return self.material.yield_strength(thickness=self.profile.max_element_thickness)
+
+    @property
+    def ultimate_strength(self) -> MM:
+        """
+        Get the ultimate strength of the steel material.
+
+        Returns
+        -------
+        MM
+            The ultimate strength of the steel material.
+        """
+        return self.material.ultimate_strength(thickness=self.profile.max_element_thickness)
 
     @property
     def weight_per_meter(self) -> KG_M:
