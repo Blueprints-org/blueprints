52ae531bb527323c44cdf448f0d5df31b3bfb205
GerjanDorgelo <gerjan.dorgelo@anteagroup.nl>
2026-01-18 09:04:33 +0100

    feat: implement NormalForceClass123 to check normal force resistance for steel I-profiles

--- blueprints/checks/eurocode/en_1993_1_1_2005/ultimate_limit_states/normal_force.py
+++ blueprints/checks/eurocode/en_1993_1_1_2005/ultimate_limit_states/normal_force.py
@@ -7,6 +7,7 @@ from dataclasses import dataclass
 
 from sectionproperties.post.post import SectionProperties
 
+from blueprints.checks.check_protocol import CheckProtocol
 from blueprints.checks.check_result import CheckResult
 from blueprints.codes.eurocode.en_1993_1_1_2005 import EN_1993_1_1_2005
 from blueprints.codes.eurocode.en_1993_1_1_2005.chapter_6_ultimate_limit_state import (
@@ -17,20 +18,20 @@ from blueprints.codes.eurocode.en_1993_1_1_2005.chapter_6_ultimate_limit_state i
 )
 from blueprints.codes.formula import Formula
 from blueprints.saf.results.result_internal_force_1d import ResultInternalForce1D
-from blueprints.structural_sections.steel.steel_cross_sections.i_profile import ISteelProfile
+from blueprints.structural_sections.steel.profile_definitions.i_profile import IProfile
 from blueprints.type_alias import DIMENSIONLESS
 from blueprints.unit_conversion import KN_TO_N
 
 
 @dataclass(frozen=True)
-class NormalForceClass123:
+class NormalForceClass123(CheckProtocol):
     """Class to perform normal force resistance check.
 
     Performs strength checks on steel I-profiles according to Eurocode 3, for class 1, 2 and 3 cross-sections.
 
     Parameters
     ----------
-    profile : ISteelProfile
+    profile : IProfile
         The steel I-profile to check.
     properties : SectionProperties
         The section properties of the profile.
@@ -44,13 +45,13 @@ class NormalForceClass123:
     from blueprints.checks.eurocode.en_1993_1_1_2005.ultimate_limit_states.normal_force import NormalForceClass123
     from blueprints.codes.eurocode.en_1993_1_1_2005.chapter_3_materials.table_3_1 import SteelStrengthClass
     from blueprints.materials.steel import SteelMaterial
-    from blueprints.structural_sections.steel.steel_cross_sections.i_profile import ISteelProfile
+    from blueprints.structural_sections.steel.steel_cross_sections.i_profile import IProfile
     from blueprints.structural_sections.steel.steel_cross_sections.standard_profiles.heb import HEB
     from blueprints.saf.results.result_internal_force_1d import ResultFor, ResultInternalForce1D, ResultOn
-    from blueprints.structural_sections.steel.steel_cross_sections.i_profile import ISteelProfile
+    from blueprints.structural_sections.steel.steel_cross_sections.i_profile import IProfile
 
     steel_material = SteelMaterial(steel_class=SteelStrengthClass.S355)
-    heb_profile = ISteelProfile.from_standard_profile(
+    heb_profile = IProfile.from_standard_profile(
         profile=HEB.HEB300,
         steel_material=steel_material,
         corrosion=0,
@@ -64,7 +65,7 @@ class NormalForceClass123:
 
     """
 
-    profile: ISteelProfile
+    profile: IProfile
     properties: SectionProperties
     result_internal_force_1d: ResultInternalForce1D
     gamma_m0: DIMENSIONLESS = 1.0
@@ -103,8 +104,8 @@ class NormalForceClass123:
             "6.9": check_compression,
         }
 
-    def check(self) -> CheckResult:
-        """Check normal force resistance.
+    def result(self) -> CheckResult:
+        """Calculate result of normal force resistance.
 
         Returns
         -------
@@ -123,26 +124,19 @@ class NormalForceClass123:
         required = steps["6.10"]
         return CheckResult.from_comparison(provided=provided, required=required)
 
-    def latex(self, n: int = 1, latex_format: str = "long") -> str:
+    def report(self, n: int = 1) -> str:
         """Returns the LaTeX string representation for the normal force check.
 
         Parameters
         ----------
         n : int, optional
             Formula numbering for LaTeX output (default is 1).
-        latex_format : str, optional
-            Output format: 'long' or 'summary'.
-            'long' gives detailed output or 'summary' gives a summary.
 
         Returns
         -------
         str
             LaTeX representation of the normal force check.
         """
-        allowed_formats = {"long", "summary"}
-        if latex_format not in allowed_formats:
-            raise ValueError(f"latex_format must be one of {allowed_formats}, got '{latex_format}'")
-
         if self.result_internal_force_1d.n == 0:
             text = r"\text{Checking normal force not needed as no normal force applied.} \newline CHECK \to OK"
         elif self.result_internal_force_1d.n > 0:
@@ -151,9 +145,6 @@ class NormalForceClass123:
             text = r"\text{Checking normal force (compression) using chapter 6.2.4.}"
 
         if self.result_internal_force_1d.n != 0:
-            if latex_format == "summary":
-                text += rf"\newline {list(self.calculation_steps().values())[-1].latex(n=n)} "
-            else:  # long
-                for step in self.calculation_steps().values():
-                    text += rf"\newline \text{{With formula {step.label}:}} \newline {step.latex(n=n)} "
+            for step in self.calculation_steps().values():
+                text += rf"\newline \text{{With formula {step.label}:}} \newline {step.latex(n=n)} "
         return text
