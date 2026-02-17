5e34f1914e3531b5f1f2baeaa1cb66b0dfc14615
Gerjan Dorgelo <gerjan.dorgelo@anteagroup.nl>
2026-02-17 14:14:25 +0100

    fix: reorder CheckStrengthBendingClass3 and CheckStrengthBendingClass12 imports in __init__.py

--- blueprints/checks/__init__.py
+++ blueprints/checks/__init__.py
@@ -1,13 +1,13 @@
 """Collection of common civil engineering checks."""
 
 from blueprints.checks.eurocode.concrete.nominal_concrete_cover import NominalConcreteCover
+from blueprints.checks.eurocode.steel.strength_bending import CheckStrengthBendingClass3, CheckStrengthBendingClass12
 from blueprints.checks.eurocode.steel.strength_compression import CheckStrengthCompressionClass123
 from blueprints.checks.eurocode.steel.strength_tension import CheckStrengthTensionClass1234
-from blueprints.checks.eurocode.steel.strength_bending import CheckStrengthBendingClass12, CheckStrengthBendingClass3
 
 __all__ = [
-    "CheckStrengthBendingClass12",
     "CheckStrengthBendingClass3",
+    "CheckStrengthBendingClass12",
     "CheckStrengthCompressionClass123",
     "CheckStrengthTensionClass1234",
     "NominalConcreteCover",
