afb7a3b33dec63c4e951bbaaf0c4e4bef40f2c83
GerjanDorgelo <gerjan.dorgelo@anteagroup.nl>
2026-07-19 19:32:03 +0200

    feat: add GU, PAL, PAU, and PU profiles to standard profiles module

--- blueprints/structural_sections/steel/standard_profiles/__init__.py
+++ blueprints/structural_sections/steel/standard_profiles/__init__.py
@@ -1,13 +1,18 @@
 """Common steel cross sectional shapes."""
 
+from blueprints.structural_sections.steel.standard_profiles.au import AU
 from blueprints.structural_sections.steel.standard_profiles.az import AZ
 from blueprints.structural_sections.steel.standard_profiles.chs import CHS
+from blueprints.structural_sections.steel.standard_profiles.gu import GU
 from blueprints.structural_sections.steel.standard_profiles.hea import HEA
 from blueprints.structural_sections.steel.standard_profiles.heb import HEB
 from blueprints.structural_sections.steel.standard_profiles.hem import HEM
 from blueprints.structural_sections.steel.standard_profiles.ipe import IPE
 from blueprints.structural_sections.steel.standard_profiles.lnp import LNP
+from blueprints.structural_sections.steel.standard_profiles.pal import PAL
+from blueprints.structural_sections.steel.standard_profiles.pau import PAU
 from blueprints.structural_sections.steel.standard_profiles.paz import PAZ
+from blueprints.structural_sections.steel.standard_profiles.pu import PU
 from blueprints.structural_sections.steel.standard_profiles.rhs import RHS
 from blueprints.structural_sections.steel.standard_profiles.rhscf import RHSCF
 from blueprints.structural_sections.steel.standard_profiles.shs import SHS
@@ -17,14 +22,19 @@ from blueprints.structural_sections.steel.standard_profiles.unp import UNP
 from blueprints.structural_sections.steel.standard_profiles.usections import USections
 
 __all__ = [
+    "AU",
     "AZ",
     "CHS",
+    "GU",
     "HEA",
     "HEB",
     "HEM",
     "IPE",
     "LNP",
+    "PAL",
+    "PAU",
     "PAZ",
+    "PU",
     "RHS",
     "RHSCF",
     "SHS",
