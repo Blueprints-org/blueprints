871b31dacf9a198fa41483f96aad984001f35538
Katinka Verleg <katinka.verleg@movares.nl>
2026-07-27 09:29:56 +0200

    First updated version. Fixed complete_with_units for chapter 8 of EC2-1-1. Fixed equation formula prompt to have correct style.

--- tests/codes/eurocode/en_1992_1_1_2004/chapter_8_detailing_of_reinforcement_and_prestressing_tendons/test_formula_8_8n.py
+++ tests/codes/eurocode/en_1992_1_1_2004/chapter_8_detailing_of_reinforcement_and_prestressing_tendons/test_formula_8_8n.py
@@ -181,9 +181,9 @@ class TestForm8Dot8nAnchorageCapacityWeldedTransverseBar:
             (
                 "complete",
                 r"F_{btd} = \min\left( l_{td} \cdot Ø_t \cdot \sigma_{td}, F_{wd} \right)"
-                r" = \min\left( 80.00 \cdot 16.00 \cdot 36.00 / 1000, 150.00 \right) = 46.08",
+                r" = \min\left( 80.00 \cdot 16.00 \cdot 36.00 / 1000, 150.00 \right) = 46.08 \ kN",
             ),
-            ("short", r"F_{btd} = 46.08"),
+            ("short", r"F_{btd} = 46.08 \ kN"),
         ],
     )
     def test_latex(self, representation: str, expected: str) -> None:
